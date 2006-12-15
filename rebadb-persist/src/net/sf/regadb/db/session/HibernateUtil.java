bf79391a3cf187afe58011ab4ed9611368c872cb
kdforc0 <kdforc0@24bc6ac0-6624-0410-8c52-f92b4954a5f8>
2006-12-15 16:17:14 +0000

    initial import

---
+++ rebadb-persist/src/net/sf/regadb/db/session/HibernateUtil.java
@@ -0,0 +1,29 @@
+/*
+ * Created on Dec 14, 2006
+ *
+ * To change the template for this generated file go to
+ * Window>Preferences>Java>Code Generation>Code and Comments
+ */
+package net.sf.regadb.db.session;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtil {
+    private static final SessionFactory sessionFactory;
+
+    static {
+        try {
+            // Create the SessionFactory from hibernate.cfg.xml
+            sessionFactory = new Configuration().configure().buildSessionFactory();
+        } catch (Throwable ex) {
+            // Make sure you log the exception, as it might be swallowed
+            System.err.println("Initial SessionFactory creation failed." + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }    
+}
