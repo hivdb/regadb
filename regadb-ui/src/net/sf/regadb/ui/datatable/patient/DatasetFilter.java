f2a0a7018707166eb80b6cca21b9653f7ea7966f
plibin0 <plibin0@24bc6ac0-6624-0410-8c52-f92b4954a5f8>
2007-01-08 12:22:07 +0000

    rudimentary start of datatable impl dataset filter impl

---
+++ regadb-ui/src/net/sf/regadb/ui/datatable/patient/DatasetFilter.java
@@ -0,0 +1,18 @@
+package net.sf.regadb.ui.datatable.patient;
+
+import net.sf.regadb.ui.framework.widgets.datatable.ListFilter;
+import net.sf.witty.wt.widgets.WComboBox;
+
+public class DatasetFilter extends ListFilter
+{
+	public DatasetFilter()
+	{
+		
+	}
+	
+	@Override
+	public void setComboBox(WComboBox combo)
+	{
+
+	}
+}
