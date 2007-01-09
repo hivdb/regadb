5366d47ef5b7c8414a47357e402a11471091f115
plibin0 <plibin0@24bc6ac0-6624-0410-8c52-f92b4954a5f8>
2007-01-09 19:33:38 +0000

    patient datatable features

--- regadb-ui/src/net/sf/regadb/ui/datatable/patient/DatasetFilter.java
+++ regadb-ui/src/net/sf/regadb/ui/datatable/patient/DatasetFilter.java
@@ -1,18 +1,26 @@
 package net.sf.regadb.ui.datatable.patient;
 
+import java.util.List;
+
+import net.sf.regadb.db.Dataset;
+import net.sf.regadb.db.Transaction;
 import net.sf.regadb.ui.framework.widgets.datatable.ListFilter;
 import net.sf.witty.wt.widgets.WComboBox;
 
 public class DatasetFilter extends ListFilter
 {
-	public DatasetFilter()
+	public DatasetFilter(Transaction transaction)
 	{
-		
+		super(transaction);
 	}
 	
 	@Override
 	public void setComboBox(WComboBox combo)
 	{
-
+		List<Dataset> datasets = getTransaction().getCurrentUsersDatasets();
+		for(Dataset ds : datasets )
+		{
+			combo.addItem(lt(ds.getDescription()));
+		}
 	}
 }
