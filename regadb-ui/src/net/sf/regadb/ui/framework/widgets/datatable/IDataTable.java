ebc3fee248d08a9fe79fe38f71ccb2ac9d793ea7
plibin0 <plibin0@24bc6ac0-6624-0410-8c52-f92b4954a5f8>
2007-01-11 18:30:22 +0000

    changed the interface of the DataTable with this changes it becomes easier to implement (we now put more responsability towards the DataTable impl)

--- regadb-ui/src/net/sf/regadb/ui/framework/widgets/datatable/IDataTable.java
+++ regadb-ui/src/net/sf/regadb/ui/framework/widgets/datatable/IDataTable.java
@@ -9,10 +9,16 @@ public interface IDataTable <DataType>
 	
 	public int getAmountOfRows();
 	
-	public List<DataType> getPreviousDataBlock();
-	public List<DataType> getNextDataBlock();
 	public List<DataType> getFirstDataBlock();
 	public List<DataType> getLastDataBlock();
+	
+	/*
+	 * The start index needs to be provided
+	 * The size of the data block we know already,
+	 * we use the size of the returned List from the
+	 * getFirstDataBlock() method
+	 * */
+	public List<DataType> getDataBlock(int startIndex);
 	
 	public String[] getRowData(DataType type);
 	
