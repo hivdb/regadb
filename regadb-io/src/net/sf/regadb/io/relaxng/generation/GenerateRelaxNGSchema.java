ffc32ca69a1fce220e5c0ad906104c9168cdea2b
plibin0 <plibin0@24bc6ac0-6624-0410-8c52-f92b4954a5f8>
2007-01-19 19:34:54 +0000

    io scheme generation

--- regadb-io/src/net/sf/regadb/io/relaxng/generation/GenerateRelaxNGSchema.java
+++ regadb-io/src/net/sf/regadb/io/relaxng/generation/GenerateRelaxNGSchema.java
@@ -6,7 +6,7 @@ import java.util.List;
 import java.util.Set;
 
 import net.sf.regadb.db.Patient;
-import net.sf.regadb.io.hibernate.mapping.InterpreteHbm;
+import net.sf.regadb.util.hbm.InterpreteHbm;
 
 import org.jdom.Document;
 import org.jdom.Element;
@@ -21,7 +21,23 @@ public class GenerateRelaxNGSchema
 	private Class startclass_;
 	
 	private static Class[] regaClasses_;
+    private static ArrayList<String> classToBeIgnored_ = new ArrayList<String>();
+    private static String dbPackage = "net.sf.regadb.db.";
 	
+    static
+    {
+        try
+        {
+            regaClasses_ = net.sf.regadb.util.reflection.Package.getInstance().getClasses(Patient.class.getPackage().getName());
+        }
+        catch (ClassNotFoundException e)
+        {
+            e.printStackTrace();
+        }
+        
+        classToBeIgnored_.add(dbPackage + "DatasetAccess");
+    }
+    
 	public GenerateRelaxNGSchema(String strstartclass,String rootnodename)
 	{
 		rootE1_ = new Element("element");
@@ -39,34 +55,13 @@ public class GenerateRelaxNGSchema
 		}
 	}
 		
-	static
-	{
-		try
-		{
-			regaClasses_ = net.sf.regadb.util.reflection.Package.getInstance().getClasses(Patient.class.getPackage().getName());
-		}
-		catch (ClassNotFoundException e)
-		{
-			e.printStackTrace();
-		}
-	}
-
 	private void makeXmlSchema(Class c, Element parentnode)
 	{
 		InterpreteHbm interpreter = InterpreteHbm.getInstance();
 		Element child = new Element("element");
 		Field[] pfields = c.getDeclaredFields();
-		String currentfieldname = new String();
-
-		if (c.getName().equals(startclass_.getName())) //startclass.getname()
-		{
-			
-		}
-		else
-		{
-			parentnode.setAttribute("name", parentnode.getAttributeValue("name"));
-		}
-
+		String currentfieldname;
+        
 		child.setAttribute("name", c.getName().substring(c.getName().lastIndexOf(".") + 1));
 		parentnode.addContent(child);
 		
@@ -75,37 +70,75 @@ public class GenerateRelaxNGSchema
 			currentfieldname = pfields[m].getName();
 			if ((pfields[m].getType() == Set.class))
 			{
-				try
-				{
-					String setInfo = pfields[m].toGenericString();
-					int startfrom = setInfo.indexOf('<') + 1;
-					int endat = setInfo.indexOf('>');
-					String classstr = setInfo.substring(startfrom, endat);
-					if (isRegaClass(Class.forName(classstr))) 
-					{
-						makeXmlSchema(Class.forName(classstr), child);
-					}
-				}
-				catch (ClassNotFoundException e)
-				{
-					e.printStackTrace();
-				}
+                handleSet(child, pfields[m], c.getName());
 			}
-			else
+			else if (!interpreter.isId(c.getName(), currentfieldname)
+						&& !interpreter.isVersion(c.getName(), currentfieldname) 
+                        && !isClassToBeIgnored(c.getName()) 
+                        && !isClassListed(c))
 			{
-				if (!interpreter.isId(c.getName(), currentfieldname)
-						&& !interpreter.isVersion(c.getName(), currentfieldname))
-				{
-					Element childnode = new Element("element");
-					childnode.setAttribute("name", currentfieldname);
-					child.addContent(childnode);
-				}
-
+			    Element toAdd = handleOptionalFields(child, pfields[m], c.getName());
+				Element childnode = new Element("element");
+				childnode.setAttribute("name", currentfieldname);
+                toAdd.addContent(childnode);
 			}
 		}
 
 		classeslisted_.add(c.getClass());
 	}
+    
+    private boolean isClassToBeIgnored(String className)
+    {
+        for(String c : classToBeIgnored_)
+        {
+            if(c.equals(className) || c.equals(dbPackage+className))
+            {
+                return true;
+            }
+        }
+        
+        return false;
+    }
+    
+    private void handleSet(Element child, Field field, String parentClassStr)
+    {
+        try
+        {
+            String setInfo = field.toGenericString();
+            int startfrom = setInfo.indexOf('<') + 1;
+            int endat = setInfo.indexOf('>');
+            String classStr = setInfo.substring(startfrom, endat);
+            if (isRegaClass(Class.forName(classStr)) && !isClassToBeIgnored(classStr) && !isClassListed(Class.forName(classStr))) 
+            {
+                Element zeroOrMore = new Element("zeroOrMore");
+                child.addContent(zeroOrMore);
+                makeXmlSchema(Class.forName(classStr), zeroOrMore);
+            }
+        }
+        catch (ClassNotFoundException e)
+        {
+            e.printStackTrace();
+        }
+    }
+    
+    //the returned Element is the Element where you should put your stuff
+    private Element handleOptionalFields(Element child, Field field, String parentClassStr)
+    {
+        InterpreteHbm interpreter = InterpreteHbm.getInstance();
+        Element toAdd;
+        if(!interpreter.isNotNull(parentClassStr, field.getName()))
+        {
+            Element optional = new Element("optional");
+            child.addContent(optional);
+            toAdd = optional;
+        }
+        else
+        {
+            toAdd = child;
+        }
+        
+        return toAdd;
+    }
 
 	public void printXmlSchema()
 	{
