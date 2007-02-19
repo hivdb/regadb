569a63c2927eb0622434d4649b6e7e7f0e663a29
plibin0 <plibin0@24bc6ac0-6624-0410-8c52-f92b4954a5f8>
2007-02-19 16:56:46 +0000

    ValueTypes enum

---
+++ rebadb-persist/src/net/sf/regadb/db/ValueTypes.java
@@ -0,0 +1,26 @@
+package net.sf.regadb.db;
+
+public enum ValueTypes 
+{
+    NUMBER,
+    LIMITED_NUMBER,
+    STRING,
+    NOMINAL_VALUE;
+    
+    public static ValueTypes getValueType(int valueTypeKey)
+    {
+        switch(valueTypeKey)
+        {
+        case 1:
+            return NUMBER;
+        case 2:
+            return LIMITED_NUMBER;
+        case 3:
+            return STRING;
+        case 4:
+            return NOMINAL_VALUE;
+        }
+        
+        return null;
+    }
+}
