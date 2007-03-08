ed673736c06581b4ea742b9604d3a03bb8362dcf
plibin0 <plibin0@24bc6ac0-6624-0410-8c52-f92b4954a5f8>
2007-03-08 16:47:54 +0000

    analysis framework base

---
+++ regadb-analyses/src/net/sf/regadb/service/AnalysisPool.java
@@ -0,0 +1,76 @@
+package net.sf.regadb.service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class AnalysisPool 
+{
+    private static AnalysisPool instance_ = null;
+    
+    private List<AnalysisThread> threadList_ = new ArrayList<AnalysisThread>();
+    
+    private AnalysisPool()
+    {
+        TimerTask keepThreadListClean = new TimerTask()
+        {
+            @Override
+            public void run() 
+            {
+                cleanThreadList();
+            }
+        };
+        
+        Timer timer = new Timer();
+        timer.schedule(keepThreadListClean, 60*5*1000, 60*2*1000);
+    }
+    
+    private synchronized void cleanThreadList()
+    {
+        List<AnalysisThread> toBeRemoved = new ArrayList<AnalysisThread>();
+        long currentTime = System.currentTimeMillis();
+        long endTimePlusLogDelay;
+        for(AnalysisThread t : threadList_)
+        {
+            if(t.getAnalysis().getEndTime()!=null && t.getAnalysis().removeFromLogging()!=null && !t.isAlive())
+            {
+                endTimePlusLogDelay = t.getAnalysis().getEndTime().getTime() + t.getAnalysis().removeFromLogging();
+                if(currentTime>=endTimePlusLogDelay)
+                {
+                    toBeRemoved.add(t);
+                }
+            }
+        }
+        
+        threadList_.removeAll(toBeRemoved);
+    }
+    
+    public synchronized void launchAnalysis(final IAnalysis analysis)
+    {
+        AnalysisThread t = new AnalysisThread(analysis);
+        t.start();
+        threadList_.add(t);
+    }
+    
+    //The items returned by this list might be cleaned up by a job thread
+    //Be aware of this when using them
+    public synchronized List<IAnalysis> getAnalyses()
+    {
+        List<IAnalysis> analysesList = new ArrayList<IAnalysis>();
+        
+        for(AnalysisThread at : threadList_)
+        {
+            analysesList.add(at.getAnalysis());
+        }
+        
+        return analysesList;
+    }
+    
+    public static AnalysisPool getInstance()
+    {
+        if(instance_==null)
+            instance_ = new AnalysisPool();
+        return instance_;
+    }
+}
---
+++ regadb-analyses/src/net/sf/regadb/service/AnalysisThread.java
@@ -0,0 +1,25 @@
+package net.sf.regadb.service;
+
+import net.sf.regadb.db.Analysis;
+
+public class AnalysisThread extends Thread
+{
+    private IAnalysis analysis_;
+    
+    public AnalysisThread(final IAnalysis analysis)
+    {
+        super(new Runnable()
+        {
+            public void run() 
+            {
+                analysis.launch();
+            }
+        });
+        analysis_ = analysis;
+    }
+
+    public IAnalysis getAnalysis() 
+    {
+        return analysis_;
+    }
+}
