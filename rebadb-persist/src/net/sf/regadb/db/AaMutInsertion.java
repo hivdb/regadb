768d2a763ba958c0d4900773d7c01688bcd5bcf7
plibin0 <plibin0@24bc6ac0-6624-0410-8c52-f92b4954a5f8>
2007-03-13 19:37:29 +0000

    show synonymous and non-synonymous mutations

--- rebadb-persist/src/net/sf/regadb/db/AaMutInsertion.java
+++ rebadb-persist/src/net/sf/regadb/db/AaMutInsertion.java
@@ -3,6 +3,7 @@ package net.sf.regadb.db;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.SortedSet;
+import java.util.StringTokenizer;
 import java.util.TreeSet;
 
 public class AaMutInsertion implements Comparable<AaMutInsertion>
@@ -40,6 +41,14 @@ public class AaMutInsertion implements Comparable<AaMutInsertion>
         else
             return mutation_.getAaMutation();
     }
+    
+    public String getAaReferenceString()
+    {
+        if(mutation_!=null)
+            return mutation_.getAaReference();
+        else
+            return null;
+    }
 
     public int compareTo(AaMutInsertion a) 
     {
@@ -76,5 +85,26 @@ public class AaMutInsertion implements Comparable<AaMutInsertion>
             return mutation_.getAaReference().equals(mutation_.getAaMutation());
         else
             return false;
+    }
+    
+    public AaMutation getMutation()
+    {
+        return mutation_;
+    }
+    
+    public AaInsertion getInsertion()
+    {
+        return insertion_;
+    }
+    
+    public String filterCodon(String codon)
+    {
+        String temp = "";
+        StringTokenizer parser = new StringTokenizer(codon,"{},");
+        while(parser.hasMoreTokens())
+        {
+            temp = temp + parser.nextToken();
+        }
+        return temp;
     }
 }
