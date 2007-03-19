a348851a7322ec887f13a57c3dc3b4c0dd42997d
jvsant1 <jvsant1@24bc6ac0-6624-0410-8c52-f92b4954a5f8>
2007-03-19 12:37:59 +0000

    nt form field

--- regadb-ui/src/net/sf/regadb/ui/framework/forms/fields/NucleotideField.java
+++ regadb-ui/src/net/sf/regadb/ui/framework/forms/fields/NucleotideField.java
@@ -2,9 +2,11 @@ package net.sf.regadb.ui.framework.forms.fields;
 
 import net.sf.regadb.ui.framework.forms.IForm;
 import net.sf.regadb.ui.framework.forms.InteractionState;
+import net.sf.witty.wt.core.utils.WVerticalAlignment;
 import net.sf.witty.wt.widgets.WCheckBox;
 import net.sf.witty.wt.widgets.WFormWidget;
 import net.sf.witty.wt.widgets.WPushButton;
+import net.sf.witty.wt.widgets.WTable;
 import net.sf.witty.wt.widgets.WTextArea;
 
 public class NucleotideField extends FormField
@@ -19,10 +21,16 @@ public class NucleotideField extends FormField
         if(state == InteractionState.Adding || state == InteractionState.Editing)
         {
             _fieldEdit = new WTextArea();
-            autoFix_ = new WCheckBox(tr("formfield.ntfield.checkbox.autofixSequence"));
-            uploadFasta_ = new WPushButton(tr("formfield.ntfield.button.uploadFastaFile"));
+            _fieldEdit.setColumns(70);
+            _fieldEdit.setRows(15);
+            WTable ntFileTable = new WTable(this);
+            ntFileTable.putElementAt(0, 0, _fieldEdit);
+            WTable buttonTable = new WTable(ntFileTable.elementAt(0, 1));
+            autoFix_ = new WCheckBox(tr("formfield.ntfield.checkbox.autofixSequence"), buttonTable.elementAt(0, 0));
+            uploadFasta_ = new WPushButton(tr("formfield.ntfield.button.uploadFastaFile"), buttonTable.elementAt(1, 0));
+            ntFileTable.elementAt(0, 1).setVerticalAlignment(WVerticalAlignment.AlignBottom);
             
-            addWidget(_fieldEdit);
+            /*addWidget(_fieldEdit);*/
             flagValid();
         }
         else
@@ -53,13 +61,68 @@ public class NucleotideField extends FormField
         _fieldEdit.setStyleClass("form-field-ntfield-edit-valid");
     }
 
-    public String getFormText() 
-    {
-        return _fieldEdit.text();
-    }
-    
     public void setFormText(String text) 
     {
-        _fieldEdit.setText(text);
+       _fieldEdit.setText(createLinesFromText("\n", text));
+    }
+    
+    @Override
+    public String text()
+    {
+        return replaceAllPatterns(super.text(), "<br>", "");
+    }
+    
+    public static String replaceAllPatterns(String str, String pattern, String replace) 
+    {
+        int s = 0;
+        int e = 0;
+        StringBuffer result = new StringBuffer();
+    
+        while ((e = str.indexOf(pattern, s)) >= 0) {
+            result.append(str.substring(s, e));
+            result.append(replace);
+            s = e+pattern.length();
+        }
+        result.append(str.substring(s));
+        return result.toString();
+    }
+    
+    public String getFormText()
+    {
+        return replaceAllPatterns(_fieldEdit.text(), "\n", "");
+    }
+    
+    private String createLinesFromText(String endOfLine, String text)
+    {
+        final int charsInLine = 69;
+        
+        StringBuffer newsb = new StringBuffer(text.length()+(int)(text.length()/charsInLine));
+        int index = 0;
+        int untill;
+        while(index < text.length())
+        {
+            untill = index + charsInLine;
+            try
+            {
+                newsb.append(text.substring(index, untill));
+                newsb.append(endOfLine);
+                index = untill;
+            }
+            catch(StringIndexOutOfBoundsException sioobe)
+            {
+                if(index>=text.length())
+                {
+                    break;
+                }
+                else
+                {
+                    newsb.append(text.substring(index, text.length()));
+                    newsb.append(endOfLine);
+                    break;
+                }
+            }
+        }
+        
+        return newsb.toString();
     }
 }
