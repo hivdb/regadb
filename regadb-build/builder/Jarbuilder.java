002605c1b84ea05a95eb53e58bc16c13ae8fd333
plibin0 <plibin0@24bc6ac0-6624-0410-8c52-f92b4954a5f8>
2007-04-06 11:44:03 +0000

    ant simplification

--- regadb-build/builder/Jarbuilder.java
+++ regadb-build/builder/Jarbuilder.java
@@ -1,186 +1,54 @@
 package builder;
 
-import java.io.File;
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Iterator;
-import org.apache.tools.ant.BuildException;
-import org.apache.tools.ant.Project;
-import org.apache.tools.ant.ProjectHelper;
-import org.tmatesoft.svn.core.SVNDirEntry;
-import org.tmatesoft.svn.core.SVNException;
-import org.tmatesoft.svn.core.SVNNodeKind;
-import org.tmatesoft.svn.core.SVNURL;
-import org.tmatesoft.svn.core.auth.ISVNAuthenticationManager;
-import org.tmatesoft.svn.core.internal.io.dav.DAVRepositoryFactory;
-import org.tmatesoft.svn.core.internal.io.svn.SVNRepositoryFactoryImpl;
+
+import net.sf.regadb.build.ant.AntTools;
+import net.sf.regadb.build.cvs.CvsTools;
+import net.sf.regadb.build.svn.SvnTools;
+
 import org.tmatesoft.svn.core.io.SVNRepository;
-import org.tmatesoft.svn.core.io.SVNRepositoryFactory;
-import org.tmatesoft.svn.core.wc.SVNClientManager;
-import org.tmatesoft.svn.core.wc.SVNRevision;
-import org.tmatesoft.svn.core.wc.SVNWCUtil;
 
 public class Jarbuilder
 {
-    private static Project project;
-    private static String _baseDir = "C:/jvsant1/test_co/";
-    private static String _buildFile = "build.xml";
-    private static ArrayList<String> projects;
-    private static ArrayList<String> removeProjects;
-    private static ArrayList<String> dependendProjects;
-    private static ArrayList<String> queue;
-    private static String URL = "svn+ssh://zolder:3333/var/svn/repos";
+    private final static String buildDir_ = "/home/plibin0/regadb_build";
+    
+    private final static String regadb_svn_url_ = "svn+ssh://zolder:3333/var/svn/repos";
+    private final static String witty_cvs_url = ":pserver:anonymous@zolder:2401/cvsroot/witty";
 
     public static void main (String args[])
     {
-        removeProjects = new ArrayList<String>();
-        queue = new ArrayList<String>();
+        CvsTools.checkout(witty_cvs_url, buildDir_, "jwt/src", "jwt_src");
         
-        checkOut(URL);
+        AntTools.buildProject("jwt_src", buildDir_);
         
-        for(String s : removeProjects)
+        SVNRepository svnrepos = SvnTools.getSVNRepository(regadb_svn_url_, "jvsant1", "Kangoer1" );
+        ArrayList<String> modules = SvnTools.getModules(svnrepos);
+        
+        modules = filterRegaDBSvnModules(modules);
+    }
+    
+    private static ArrayList<String> filterRegaDBSvnModules(ArrayList<String> modules)
+    {
+        ArrayList<String> filteredModules = new ArrayList<String>(); 
+        
+        for(String m : modules)
         {
-            projects.remove(s);
-        }
-        
-        build("regadb-util");
-        build("regadb-persist");
-        
-        for(String s : projects)
-        {
-            getDependendProjects(s);
-            
-            Iterator itr3 = dependendProjects.iterator();
-            
-            if(dependendProjects.size() > 0)
+            if(m.startsWith("regadb-"))
             {
-                queue.add(s);
-            }
-            else
-            {
-                if(s.equals("regadb-io"))
+                if(!m.equals("regadb-sql") && !m.equals("regadb-build"))
                 {
-                    //
-                }
-                else
-                {
-                    build(s);
+                    filteredModules.add(m);
                 }
             }
         }
         
-        for(String s : queue)
-        {
-            //sort queue
-        }
-        
-        for(String s : queue)
-        {
-            build(s);
-        }
+        return filteredModules;
     }
     
-    public static void checkOut(String url)
-    {
-        DAVRepositoryFactory.setup();
-        SVNRepositoryFactoryImpl.setup();
-        
-        String name = "jvsant1";
-        String password = "Kangoer1";
-        SVNRepository repository = null;
-        long latestRevision;
-        
-        try
-        {
-            repository = SVNRepositoryFactory.create( SVNURL.parseURIDecoded( url ) );
-            ISVNAuthenticationManager authManager = SVNWCUtil.createDefaultAuthenticationManager( name , password );
-            repository.setAuthenticationManager( authManager );
-            System.out.println( "Getting repositories");
-            SVNNodeKind nodeKind = repository.checkPath( "" ,  -1 );
-            
-            projects = getModules( repository, "" );
-            for(String s : projects)
-            {
-                if(s.equals("regadb-sql") || s.equals("regadb-build") || s.equals("test_svn"))
-                {
-                    removeProjects.add(s);
-                }
-                else
-                {
-                    latestRevision = repository.getLatestRevision( );
-                    SVNClientManager.newInstance().getUpdateClient().doCheckout(SVNURL.parseURIDecoded( url + "/" +s), new File(_baseDir + s),
-                    SVNRevision.create(latestRevision), SVNRevision.create(latestRevision), true);
-                }
-                
-                
-            }          
-        }
-        catch(Exception e)
-        {
-            e.printStackTrace();
-        }
-    }
-    
-    public static ArrayList<String> getModules( SVNRepository repository, String path ) throws SVNException
-    {
-        ArrayList<String> modules = new ArrayList<String>(); 
-        Collection entries = repository.getDir( path, -1 , null , (Collection) null );
-        Iterator iterator = entries.iterator( );
-        while ( iterator.hasNext( ) )
-        {
-            SVNDirEntry entry = ( SVNDirEntry ) iterator.next( );
-            modules.add(entry.getName( ));
-        }
-        return modules;
-    }
-    
-    public static void getDependendProjects(String projectDir)
+    /*public static void getDependendProjects(String projectDir)
     {
         ProjectFileReader pfr = new ProjectFileReader();
         dependendProjects = pfr.getDependencies(_baseDir + projectDir +"/.project");
-    }
+    }*/
     
-    public static void build(String projectName)
-    {
-        project = new Project();
-        
-        try
-        { 
-            project.init();
-        }
-        catch (BuildException e)
-        {
-            System.err.println("The default task list could not be loaded.");
-        }
-        
-        
-        try
-        {
-            project.setBasedir(_baseDir);
-        }
-        catch (BuildException e)
-        {
-            System.err.println("The given basedir doesn't exist, or isn't a directory.");
-        }
-
-        try
-        {
-            ProjectHelper.getProjectHelper().parse(project, new File(_baseDir + projectName + "/" + _buildFile));
-        }
-        catch (BuildException e)
-        {
-            System.err.println("Configuration file " + _buildFile + " is invalid, or cannot be read.");
-        }
-        
-        if(projectName.equals("regadb-persist/hibernate"))
-        {
-            System.out.println("Compiling " + projectName);
-            project.executeTarget("compile");
-        }
-        else
-        {
-            System.out.println("Compiling " + projectName);
-            project.executeTarget("all");
-        }
-    }
 }
---
+++ regadb-build/net/sf/regadb/build/ant/AntTools.java
@@ -0,0 +1,47 @@
+package net.sf.regadb.build.ant;
+
+import java.io.File;
+
+import org.apache.tools.ant.BuildException;
+import org.apache.tools.ant.Project;
+import org.apache.tools.ant.ProjectHelper;
+
+public class AntTools 
+{
+    public static void buildProject(String projectName, String localBuildDir)
+    {
+        Project project = new Project();
+        
+        String buildFile = "build.xml";
+        
+        try
+        { 
+            project.init();
+        }
+        catch (BuildException e)
+        {
+            System.err.println("The default task list could not be loaded.");
+        }
+        
+        
+        try
+        {
+            project.setBasedir(localBuildDir);
+        }
+        catch (BuildException e)
+        {
+            System.err.println("The given basedir doesn't exist, or isn't a directory.");
+        }
+
+        try
+        {
+            ProjectHelper.getProjectHelper().parse(project, new File(localBuildDir + projectName + File.separatorChar + buildFile));
+        }
+        catch (BuildException e)
+        {
+            System.err.println("Configuration file " + buildFile + " is invalid, or cannot be read.");
+        }
+        
+        project.executeTarget("all");
+    }
+}
