ef6c211ddc519a22d12968e21776ddad7cb29809
plibin0 <plibin0@24bc6ac0-6624-0410-8c52-f92b4954a5f8>
2007-04-11 18:49:53 +0000

    deleted obsolete interface

--- regadb-ui/src/net/sf/regadb/ui/tree/items/singlePatient/IGetSinglePatient.java
+++
@@ -1,9 +0,0 @@
-package net.sf.regadb.ui.tree.items.singlePatient;
-
-import net.sf.regadb.db.Patient;
-
-public interface IGetSinglePatient
-{
-	public Patient getSelectedPatient();
-	public void setSelectedPatient(Patient selectedPatient);
-}
