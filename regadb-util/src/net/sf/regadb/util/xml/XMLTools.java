28a50377254e6b148f882484fe7d6caa00ad0985
plibin0 <plibin0@24bc6ac0-6624-0410-8c52-f92b4954a5f8>
2007-04-12 16:59:35 +0000

    relax ng date export function

--- regadb-util/src/net/sf/regadb/util/xml/XMLTools.java
+++ regadb-util/src/net/sf/regadb/util/xml/XMLTools.java
@@ -7,9 +7,15 @@ import java.util.Date;
 public class XMLTools 
 {
     private static DateFormat df_ = new SimpleDateFormat("yyyy-MM-dd-HH:mm:ss");
-
+    private static DateFormat dfRelaxNg_ = new SimpleDateFormat("yyyy-MM-dd");
+    
     public static String dateToString(Date date)
     {
         return df_.format(date);
+    }
+    
+    public static String dateToRelaxNgString(Date date)
+    {
+        return dfRelaxNg_.format(date);
     }
 }
