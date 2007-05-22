946131fe499c503f4975e809c53f86aba30df4e6
jvsant1 <jvsant1@24bc6ac0-6624-0410-8c52-f92b4954a5f8>
2007-05-22 10:36:43 +0000

    Create form to update from central server

---
+++ regadb-ui/src/net/sf/regadb/ui/framework/forms/administrator/UpdateForm.java
@@ -0,0 +1,31 @@
+package net.sf.regadb.ui.framework.forms.administrator;
+
+import net.sf.regadb.ui.framework.forms.FormWidget;
+import net.sf.regadb.ui.framework.forms.InteractionState;
+import net.sf.witty.wt.WGroupBox;
+
+public class UpdateForm extends FormWidget
+{
+    private WGroupBox testGroup_ = new WGroupBox(tr("form.update.central.server.test"));
+    private WGroupBox attributesGroup_ = new WGroupBox(tr("form.update.central.server.attribute"));
+    private WGroupBox drugsGroup_ = new WGroupBox(tr("form.update.central.server.drug"));
+    
+    public UpdateForm(InteractionState interactionState)
+    {
+        super(tr("form.update.central.server"), interactionState);
+        init();
+    }
+    
+    public void init()
+    {
+        addWidget(testGroup_);
+        addWidget(attributesGroup_);
+        addWidget(drugsGroup_);
+    }
+    
+    @Override
+    public void saveData()
+    {        
+        
+    }
+}
