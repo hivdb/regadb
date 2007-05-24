363fcd4a2b6e3b92fcf07d5d57fdf5d54d778c9e
jvsant1 <jvsant1@24bc6ac0-6624-0410-8c52-f92b4954a5f8>
2007-05-24 17:51:38 +0000

    dataset_access form

--- regadb-ui/src/net/sf/regadb/ui/form/datasetAccess/IDatasetAccessSelectionEditableTable.java
+++ regadb-ui/src/net/sf/regadb/ui/form/datasetAccess/IDatasetAccessSelectionEditableTable.java
@@ -2,7 +2,9 @@ package net.sf.regadb.ui.form.datasetAccess;
 
 import java.util.Set;
 
+import net.sf.regadb.db.Dataset;
 import net.sf.regadb.db.DatasetAccess;
+import net.sf.regadb.db.DatasetAccessId;
 import net.sf.regadb.db.Privileges;
 import net.sf.regadb.db.SettingsUser;
 import net.sf.regadb.db.Transaction;
@@ -12,7 +14,9 @@ import net.sf.regadb.ui.framework.forms.FormWidget;
 import net.sf.regadb.ui.framework.forms.InteractionState;
 import net.sf.regadb.ui.framework.forms.fields.TextField;
 import net.sf.regadb.ui.framework.widgets.editableTable.IEditableTable;
+import net.sf.witty.wt.SignalListener;
 import net.sf.witty.wt.WComboBox;
+import net.sf.witty.wt.WEmptyEvent;
 import net.sf.witty.wt.WWidget;
 
 public class IDatasetAccessSelectionEditableTable implements IEditableTable<DatasetAccess>
@@ -20,6 +24,7 @@ public class IDatasetAccessSelectionEditableTable implements IEditableTable<Data
     private FormWidget form_;
     private SettingsUser user_;
     private Set<DatasetAccess> currentUserDatasetAccess;
+    private SettingsUser currentUser_;
     
     private static final String [] headers_ = {"editableTable.datasetAccess.colName.dataset", "editableTable.datasetAccess.colName.rights", "editableTable.datasetAccess.colName.providerUser"};
     
@@ -29,7 +34,8 @@ public class IDatasetAccessSelectionEditableTable implements IEditableTable<Data
         user_ = user;
         
         Transaction t = RegaDBMain.getApp().createTransaction();
-        currentUserDatasetAccess = t.getSettingsUser(RegaDBMain.getApp().getLogin().getUid()).getDatasetAccesses();            
+        currentUser_ = t.getSettingsUser(RegaDBMain.getApp().getLogin().getUid());
+        currentUserDatasetAccess = currentUser_.getDatasetAccesses();            
         t.commit();
     }
     
@@ -124,11 +130,53 @@ public class IDatasetAccessSelectionEditableTable implements IEditableTable<Data
 
     public WWidget[] addRow() 
     {
-        return null;
+        WWidget[] widgets = new WWidget[2];
+        
+        final WComboBox datasetCombo = new WComboBox();
+        final WComboBox privilegesCombo = new WComboBox();
+        datasetCombo.changed.addListener(new SignalListener<WEmptyEvent>()
+        {
+            public void notify(WEmptyEvent a) 
+            {
+                setRights(datasetCombo, privilegesCombo);
+            }
+        });
+        
+        for(DatasetAccess dsa : currentUserDatasetAccess)
+        {
+            Dataset ds = dsa.getId().getDataset();
+            DataComboMessage<DatasetAccess> currentAccess = new DataComboMessage<DatasetAccess>(dsa, ds.getDescription());
+            datasetCombo.addItem(currentAccess);
+        }
+        
+        setRights(datasetCombo, privilegesCombo);
+        
+        widgets[0] = datasetCombo;
+        widgets[1] = privilegesCombo;
+        
+        return widgets;
+    }
+    
+    private void setRights(WComboBox datasetCombo, WComboBox privilegesCombo)
+    {
+        Privileges daPermissions = Privileges.getPrivilege(((DataComboMessage<DatasetAccess>)datasetCombo.currentText()).getValue().getPermissions());
+        privilegesCombo.clear();
+        for(int i = 0; i<daPermissions.getValue(); i++)
+        {
+            Privileges p = Privileges.getPrivilege(i+1);
+            privilegesCombo.addItem(new DataComboMessage<Privileges>(p,p.toString()));
+        }
     }
 
     public WWidget[] fixAddRow(WWidget[] widgets) 
     {
-        return null;
+        WComboBox ds = (WComboBox)widgets[0];
+        DatasetAccess dsaFromAddRow = ((DataComboMessage<DatasetAccess>)ds.currentText()).getValue();
+        WComboBox priv = (WComboBox)widgets[1];
+        Privileges privillege = ((DataComboMessage<Privileges>)priv.currentText()).getValue();
+    
+        DatasetAccess dsa = new DatasetAccess(new DatasetAccessId(currentUser_, dsaFromAddRow.getId().getDataset()),privillege.getValue());
+    
+        return getWidgets(dsa);
     }
 }
