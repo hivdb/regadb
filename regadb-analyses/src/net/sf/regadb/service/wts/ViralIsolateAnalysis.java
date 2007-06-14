d9d85d7a04b1b55a0f8287462703bf1705f61e2c
plibin0 <plibin0@24bc6ac0-6624-0410-8c52-f92b4954a5f8>
2007-06-14 11:42:27 +0000

    added ViralIsolateAnalysis

---
+++ regadb-analyses/src/net/sf/regadb/service/wts/ViralIsolateAnalysis.java
@@ -0,0 +1,130 @@
+package net.sf.regadb.service.wts;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
+
+import net.sf.regadb.db.AnalysisStatus;
+import net.sf.regadb.db.Test;
+import net.sf.regadb.db.TestResult;
+import net.sf.regadb.db.Transaction;
+import net.sf.regadb.db.ViralIsolate;
+import net.sf.regadb.db.session.Login;
+import net.sf.regadb.io.importXML.ResistanceInterpretationParser;
+import net.sf.regadb.service.AnalysisThread;
+import net.sf.regadb.service.IAnalysis;
+
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+
+public class ViralIsolateAnalysis implements IAnalysis
+{
+    private Integer vi_ii_;
+    private Integer test_ii_;
+
+    private Date startTime_;
+    private Date endTime_;
+    private String user_;
+    
+    public ViralIsolateAnalysis(ViralIsolate isolate, Test test, String uid)
+    {
+        vi_ii_ = isolate.getViralIsolateIi();
+        test_ii_ = test.getTestIi();
+    }
+    
+    public Date getEndTime() 
+    {
+        return endTime_;
+    }
+
+    public Date getStartTime() 
+    {
+        return startTime_;
+    }
+
+    public AnalysisStatus getStatus() 
+    {
+        return null;
+    }
+
+    public String getUser() 
+    {
+        return user_;
+    }
+
+    public void kill() 
+    {
+        
+    }
+
+    public void launch(Login sessionSafeLogin)
+    {
+        startTime_ = new Date(System.currentTimeMillis());
+        
+        Transaction t = sessionSafeLogin.createTransaction();
+        
+        ViralIsolate vi = t.getViralIsolate(vi_ii_);
+        Test test = t.getTest(test_ii_);
+        
+        t.commit();
+        
+        byte [] result = ViralIsolateAnalysisHelper.run(vi, test, 500);
+        
+        t = sessionSafeLogin.createTransaction();
+        t.clear();
+        
+        synchronized(AnalysisThread.mutex_)
+        {
+            vi = t.getViralIsolate(vi_ii_);
+            test = t.getTest(test_ii_);
+            
+            final ViralIsolate isolate = vi;
+            final Test test_final = test;
+            final Transaction t_final = t;
+            ResistanceInterpretationParser inp = new ResistanceInterpretationParser()
+            {
+                @Override
+                public void completeScore(String drug, int level, double gss, String description, char sir, ArrayList<String> mutations, String remarks) 
+                {
+                    TestResult resistanceInterpretation = new TestResult();
+                    resistanceInterpretation.setViralIsolate(isolate);
+                    resistanceInterpretation.setDrugGeneric(t_final.getDrugGeneric(drug));
+                    resistanceInterpretation.setValue(gss+"");
+                    resistanceInterpretation.setTestDate(new Date(System.currentTimeMillis()));
+                    resistanceInterpretation.setTest(test_final);
+                    
+                    isolate.getTestResults().add(resistanceInterpretation);
+                }
+            };
+            try 
+            {
+                inp.parse(new InputSource(new ByteArrayInputStream(result)));
+            } 
+            catch (SAXException e) 
+            {
+                e.printStackTrace();
+            } 
+            catch (IOException e) 
+            {
+                e.printStackTrace();
+            }
+            
+            t.update(vi);
+            t.commit();
+        }
+        
+        endTime_ = new Date(System.currentTimeMillis());
+    }
+    
+
+    public void pause()
+    {
+        
+    }
+
+    public Long removeFromLogging()
+    {
+        return 10000L;
+    }
+}
