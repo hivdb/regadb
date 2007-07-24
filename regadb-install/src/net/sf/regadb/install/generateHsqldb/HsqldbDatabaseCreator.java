67a2523bf49add5597f9bf4dd48e683ba8cee24c
plibin0 <plibin0@24bc6ac0-6624-0410-8c52-f92b4954a5f8>
2007-07-24 11:21:57 +0000

    parametrized hsqldb

--- regadb-install/src/net/sf/regadb/install/generateHsqldb/HsqldbDatabaseCreator.java
+++ regadb-install/src/net/sf/regadb/install/generateHsqldb/HsqldbDatabaseCreator.java
@@ -5,45 +5,57 @@ import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 
 import net.sf.regadb.install.initdb.InitRegaDB;
+import net.sf.regadb.util.pair.Pair;
 
 import org.apache.commons.io.FileUtils;
 import org.hsqldb.Server;
 import org.hsqldb.jdbc.jdbcDataSource;
 
 public class HsqldbDatabaseCreator {
-	private final static String databasePath = "src/net/sf/regadb/install/hsqldb/database/";
-	private final static String databaseName = "regadb";
+	private String databasePath_;
+	private String databaseName_;
+    private String hsqldbSchemaLocation_;
 	
-	private static String url;
-	private static String userName;
-	private static String password;
+	private String url_;
+	private String userName_;
+	private String password_;
 	
 	public static void main(String[] args) {
-		File database = new File(databasePath);
-		
-		url = "jdbc:hsqldb:file:" + database.getAbsolutePath() + File.separatorChar + databaseName;
-		userName = "regadb";
-		password = "regadb";
-		
-		startupDatabase();
-		createDatabase();
-		initDatabase();
-		shutdownDatabase();
-		changeUser();
-	}
+        HsqldbDatabaseCreator hsqldb = new HsqldbDatabaseCreator("src/net/sf/regadb/install/hsqldb/database/", "regadb", "regadb", "regadb", "src/net/sf/regadb/install/ddl/schema/hsqldbSchema.sql");
+        hsqldb.run();
+    }
+    
+    public HsqldbDatabaseCreator(String databasePath, String databaseName, String userName, String password, String hsqlSchemaLocation) {
+        databasePath_ = databasePath;
+        databaseName_ = databaseName;
+        userName_ = userName;
+        password_ = password;
+        hsqldbSchemaLocation_ = hsqlSchemaLocation;
+        
+        File database = new File(databasePath_);
+        url_ = "jdbc:hsqldb:file:" + database.getAbsolutePath() + File.separatorChar + databaseName_;
+    }
+    
+    public void run() {
+        startupDatabase();
+        createDatabase();
+        initDatabase();
+        shutdownDatabase();
+        changeUser();
+    }
 	
-	private static void startupDatabase() {
+	private void startupDatabase() {
 		Server server = new Server();
-		server.setDatabasePath(0, databasePath + File.separatorChar + databaseName);
-		server.setDatabaseName(0, databaseName);
+		server.setDatabasePath(0, databasePath_ + File.separatorChar + databaseName_);
+		server.setDatabaseName(0, databaseName_);
 		server.start();
 	}
 	
-	private static void createDatabase() {
-		String hsqldbSchemaLocation = "src/net/sf/regadb/install/ddl/schema/hsqldbSchema.sql";
-		File hsqldbSchemaFile = new File(hsqldbSchemaLocation);
+	private void createDatabase() {
+		File hsqldbSchemaFile = new File(hsqldbSchemaLocation_);
 		
 		String query = new String();
 		
@@ -57,16 +69,25 @@ public class HsqldbDatabaseCreator {
 		executeQuery(query);
 	}
 	
-	private static void initDatabase() {
-		InitRegaDB.main(null);
+	private void initDatabase() {
+        InitRegaDB init = new InitRegaDB();
+        
+        ArrayList<Pair<String, String>> conf = new ArrayList<Pair<String, String>>();
+        conf.add(new Pair<String, String>("hibernate.connection.driver_class", "org.hsqldb.jdbcDriver"));
+        conf.add(new Pair<String, String>("hibernate.connection.password", ""));
+        conf.add(new Pair<String, String>("hibernate.connection.username", "sa"));
+        conf.add(new Pair<String, String>("hibernate.connection.url", url_));
+        conf.add(new Pair<String, String>("hibernate.dialect", "org.hibernate.dialect.HSQLDialect"));
+
+        init.run(conf);
 	}
 	
-	private static void shutdownDatabase() {
+	private void shutdownDatabase() {
 		executeQuery("SHUTDOWN");
 	}
 	
-	private static void changeUser() {
-		File databaseScript = new File(databasePath + File.separatorChar + databaseName + ".script");
+	private void changeUser() {
+		File databaseScript = new File(databasePath_ + File.separatorChar + databaseName_ + ".script");
 		
 		String scriptFile = new String();
 		
@@ -77,8 +98,8 @@ public class HsqldbDatabaseCreator {
 			e.printStackTrace();
 		}
 		
-		scriptFile = scriptFile.replace("CREATE USER SA PASSWORD \"\"", "CREATE USER " + userName.toUpperCase() + " PASSWORD \"" + password.toUpperCase() + "\"");
-		scriptFile = scriptFile.replace("GRANT DBA TO SA", "GRANT DBA TO " + userName.toUpperCase());
+		scriptFile = scriptFile.replace("CREATE USER SA PASSWORD \"\"", "CREATE USER " + userName_.toUpperCase() + " PASSWORD \"" + password_.toUpperCase() + "\"");
+		scriptFile = scriptFile.replace("GRANT DBA TO SA", "GRANT DBA TO " + userName_.toUpperCase());
 		
 		try {
 			FileUtils.writeStringToFile(databaseScript, scriptFile);
@@ -88,16 +109,16 @@ public class HsqldbDatabaseCreator {
 		}
 	}
 	
-	private static jdbcDataSource setupDataSource() {
+	private jdbcDataSource setupDataSource() {
 		jdbcDataSource dataSource = new jdbcDataSource();
-		dataSource.setDatabase(url);
+		dataSource.setDatabase(url_);
 		dataSource.setUser("sa");
 		dataSource.setPassword("");
 		
 		return dataSource;
 	}
 	
-	private static void executeQuery(String query) {
+	private void executeQuery(String query) {
 		jdbcDataSource dataSource = setupDataSource();
 		
 		try {
