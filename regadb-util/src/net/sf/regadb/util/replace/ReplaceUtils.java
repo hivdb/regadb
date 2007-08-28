cd2e6d9cccd4ccbcb99a4669408c17ea4096808b
plibin0 <plibin0@24bc6ac0-6624-0410-8c52-f92b4954a5f8>
2007-08-28 11:18:21 +0000

    replace all in file

---
+++ regadb-util/src/net/sf/regadb/util/replace/ReplaceUtils.java
@@ -0,0 +1,43 @@
+package net.sf.regadb.util.replace;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.nio.CharBuffer;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.charset.Charset;
+import java.nio.charset.CharsetDecoder;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ReplaceUtils {
+    public static void replaceAllInFile(File inputFile, File outputFile, String substitute, String substituteReplacement) throws IOException {
+        Pattern pattern = Pattern.compile(substitute);
+
+        // Open the file and then get a channel from the stream
+        FileInputStream fis = new FileInputStream(inputFile);
+        FileChannel fc = fis.getChannel();
+
+        // Get the file's size and then map it into memory
+        int sz = (int)fc.size();
+        MappedByteBuffer bb = fc.map(FileChannel.MapMode.READ_ONLY, 0, sz);
+
+        // Decode the file into a char buffer
+        // Charset and decoder for ISO-8859-15
+        Charset charset = Charset.forName("ISO-8859-15");
+        CharsetDecoder decoder = charset.newDecoder();
+        CharBuffer cb = decoder.decode(bb);
+
+        Matcher matcher = pattern.matcher(cb);
+        String outString = matcher.replaceAll(substituteReplacement);
+
+        FileOutputStream fos = new FileOutputStream(outputFile.getAbsolutePath());
+        PrintStream ps =new PrintStream(fos);
+        ps.print(outString);
+        ps.close();
+        fos.close();
+    }
+}
