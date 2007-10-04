b80e8ab0935c1f0db731b43eadd55c4d6b391dfb
plibin0 <plibin0@24bc6ac0-6624-0410-8c52-f92b4954a5f8>
2007-10-04 16:07:57 +0000

    extra connection settings for hibernate + jdbc

--- regadb-persist/src/net/sf/regadb/db/session/HibernateUtil.java
+++ regadb-persist/src/net/sf/regadb/db/session/HibernateUtil.java
@@ -6,8 +6,14 @@
  */
 package net.sf.regadb.db.session;
 
+import java.io.File;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 import net.sf.regadb.util.settings.RegaDBSettings;
 
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
@@ -17,18 +23,31 @@ public class HibernateUtil {
     static {
         try {
             // Create the SessionFactory from hibernate.cfg.xml + from regadb conf xml file
-            Configuration conf = new Configuration().configure();
-            setProperty("hibernate.connection.driver_class", conf);
-            setProperty("hibernate.connection.password", conf);
-            setProperty("hibernate.connection.url", conf);
-            setProperty("hibernate.connection.username", conf);
-            setProperty("hibernate.dialect", conf);
+            Configuration conf = getConfiguration();
             sessionFactory = conf.buildSessionFactory(); 
         } catch (Throwable ex) {
             // Make sure you log the exception, as it might be swallowed
             System.err.println("Initial SessionFactory creation failed." + ex);
             throw new ExceptionInInitializerError(ex);
         }
+    }
+    
+    public static Configuration getConfiguration() {
+        Configuration conf = new Configuration().configure();
+        
+        setProperty("hibernate.connection.driver_class", conf);
+        setProperty("hibernate.connection.password", conf);
+        setProperty("hibernate.connection.url", conf);
+        setProperty("hibernate.connection.username", conf);
+        setProperty("hibernate.dialect", conf);
+        
+        return conf;
+    }
+    
+    public static Session getEditedSession(File tempTableMappings) {
+        Configuration conf = getConfiguration();
+        conf.addFile(tempTableMappings);
+        return conf.buildSessionFactory().openSession();
     }
     
     public static void setProperty(String name, Configuration conf)
@@ -42,5 +61,20 @@ public class HibernateUtil {
 
     public static SessionFactory getSessionFactory() {
         return sessionFactory;
-    }    
+    }
+    
+    public static Connection getJDBCConnection() {
+        try {
+            Class.forName(RegaDBSettings.getInstance().getPropertyValue("hibernate.connection.driver_class"));
+            String url = RegaDBSettings.getInstance().getPropertyValue("hibernate.connection.url");
+            String userName = RegaDBSettings.getInstance().getPropertyValue("hibernate.connection.username");
+            String password = RegaDBSettings.getInstance().getPropertyValue("hibernate.connection.password");
+            Connection conn = DriverManager.getConnection(url, userName, password);
+            return conn;
+        } catch (ClassNotFoundException e) {
+            return null;
+        } catch (SQLException e) {
+            return null;
+        }
+    }
 }
