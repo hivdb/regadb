390aec0b38da0f731ac606fc7f1a6b55563b2931
simbre1 <simbre1@24bc6ac0-6624-0410-8c52-f92b4954a5f8>
2008-03-13 11:36:48 +0000

    added drug placebo, blind and frequency

--- regadb-ui/src/net/sf/regadb/ui/form/singlePatient/ICommercialDrugSelectionEditableTable.java
+++ regadb-ui/src/net/sf/regadb/ui/form/singlePatient/ICommercialDrugSelectionEditableTable.java
@@ -10,10 +10,12 @@ import net.sf.regadb.db.Transaction;
 import net.sf.regadb.ui.framework.RegaDBMain;
 import net.sf.regadb.ui.framework.forms.FormWidget;
 import net.sf.regadb.ui.framework.forms.InteractionState;
+import net.sf.regadb.ui.framework.forms.fields.CheckBox;
 import net.sf.regadb.ui.framework.forms.fields.ComboBox;
 import net.sf.regadb.ui.framework.forms.fields.FieldType;
 import net.sf.regadb.ui.framework.forms.fields.TextField;
 import net.sf.regadb.ui.framework.widgets.editableTable.IEditableTable;
+import net.sf.regadb.util.frequency.Frequency;
 import net.sf.witty.wt.WWidget;
 
 public class ICommercialDrugSelectionEditableTable implements IEditableTable<TherapyCommercial>
@@ -21,7 +23,12 @@ public class ICommercialDrugSelectionEditableTable implements IEditableTable<The
     private FormWidget form_;
     private Therapy therapy_;
     private Transaction transaction_;
-    private static final String [] headers_ = {"editableTable.drug.colName.drug", "editableTable.drugCommercial.colName.dosage"};
+    private static final String [] headers_ = { "editableTable.drug.colName.drug",
+                                                "editableTable.drugCommercial.colName.dosage",
+                                                "editableTable.drugCommercial.colName.frequency",
+                                                "editableTable.drugCommercial.colName.frequency_type",
+                                                "editableTable.drugCommercial.colName.placebo",
+                                                "editableTable.drugCommercial.colName.blind"};
     
     public ICommercialDrugSelectionEditableTable(FormWidget form, Therapy therapy)
     {
@@ -32,15 +39,23 @@ public class ICommercialDrugSelectionEditableTable implements IEditableTable<The
     public void addData(WWidget[] widgets)
     {
         DrugCommercial dc = ((ComboBox<DrugCommercial>)widgets[0]).currentValue();
-        TherapyCommercial tc = new TherapyCommercial(new TherapyCommercialId(therapy_, dc), getDosage((TextField)widgets[1]));
+        TherapyCommercial tc = new TherapyCommercial(   new TherapyCommercialId(therapy_, dc),
+                                                        getDosage((TextField)widgets[1]),
+                                                        ((CheckBox)widgets[4]).isChecked(),
+                                                        ((CheckBox)widgets[5]).isChecked(),
+                                                        getFrequency(widgets[2],widgets[3]));
         therapy_.getTherapyCommercials().add(tc);
     }
 
     public void changeData(TherapyCommercial tc, WWidget[] widgets)
     {
         DrugCommercial dc = ((ComboBox<DrugCommercial>)widgets[0]).currentValue();
-        tc.setDayDosageUnits(getDosage(((TextField)widgets[1])));
+        tc.setDayDosageUnits(getDosage(((TextField)widgets[1])));        
         tc.getId().setDrugCommercial(dc);
+
+        tc.setFrequency(getFrequency(widgets[2],widgets[3]));
+        tc.setPlacebo(((CheckBox)widgets[4]).isChecked());
+        tc.setBlind(((CheckBox)widgets[5]).isChecked());
     }
     
     private Double getDosage(TextField tf)
@@ -75,6 +90,10 @@ public class ICommercialDrugSelectionEditableTable implements IEditableTable<The
     {
         ComboBox<DrugCommercial> combo = new ComboBox<DrugCommercial>(InteractionState.Viewing, form_);
         TextField tf = new TextField(form_.getInteractionState(), form_, FieldType.DOUBLE);
+        CheckBox cb_placebo = new CheckBox(form_.getInteractionState(), form_);
+        CheckBox cb_blind = new CheckBox(form_.getInteractionState(), form_);
+        TextField tf_freq = new TextField(form_.getInteractionState(), form_, FieldType.DOUBLE);
+        ComboBox<Frequency> combo_freq = createFrequencyComboBox();
         
         Transaction t = RegaDBMain.getApp().createTransaction();
         List<DrugCommercial> commercialDrugs = t.getCommercialDrugs();
@@ -86,9 +105,13 @@ public class ICommercialDrugSelectionEditableTable implements IEditableTable<The
         
         t.commit();
         
-        WWidget[] widgets = new WWidget[2];
+        WWidget[] widgets = new WWidget[6];
         widgets[0] = combo;
         widgets[1] = tf;
+        widgets[2] = tf_freq;
+        widgets[3] = combo_freq;
+        widgets[4] = cb_placebo;
+        widgets[5] = cb_blind;
         
         if(tc!=null)
         {        
@@ -97,6 +120,11 @@ public class ICommercialDrugSelectionEditableTable implements IEditableTable<The
                 tf.setText(tc.getDayDosageUnits()+"");
             }
             combo.selectItem(tc.getId().getDrugCommercial().getName());
+            
+            cb_placebo.setChecked(tc.isPlacebo());
+            cb_blind.setChecked(tc.isBlind());
+            
+            setFrequency(tc.getFrequency(), tf_freq, combo_freq);
         }
         
         return widgets;
@@ -111,6 +139,10 @@ public class ICommercialDrugSelectionEditableTable implements IEditableTable<The
     {
         ComboBox<DrugCommercial> combo = new ComboBox<DrugCommercial>(form_.getInteractionState(), form_);
         TextField tf = new TextField(form_.getInteractionState(), form_, FieldType.DOUBLE);
+        CheckBox cb_placebo = new CheckBox(form_.getInteractionState(), form_);
+        CheckBox cb_blind = new CheckBox(form_.getInteractionState(), form_);
+        TextField tf_freq = new TextField(form_.getInteractionState(), form_, FieldType.DOUBLE);
+        ComboBox<Frequency> combo_freq = createFrequencyComboBox();
         
         Transaction t = RegaDBMain.getApp().createTransaction();
         List<DrugCommercial> commercialDrugs = t.getCommercialDrugs();
@@ -121,9 +153,13 @@ public class ICommercialDrugSelectionEditableTable implements IEditableTable<The
         combo.sort();
         t.commit();
         
-        WWidget[] widgets = new WWidget[2];
+        WWidget[] widgets = new WWidget[6];
         widgets[0] = combo;
         widgets[1] = tf;
+        widgets[2] = tf_freq;
+        widgets[3] = combo_freq;
+        widgets[4] = cb_placebo;
+        widgets[5] = cb_blind;
         
         return widgets;
     }
@@ -140,7 +176,11 @@ public class ICommercialDrugSelectionEditableTable implements IEditableTable<The
         
         Double units = getDosage((TextField)widgets[1]);
         
-        TherapyCommercial tc = new TherapyCommercial(new TherapyCommercialId(null, dc), units);
+        TherapyCommercial tc = new TherapyCommercial(   new TherapyCommercialId(null, dc),
+                                                        units,
+                                                        ((CheckBox)widgets[4]).isChecked(),
+                                                        ((CheckBox)widgets[5]).isChecked(),
+                                                        getFrequency(widgets[2],widgets[3]));
         
         return getWidgets(tc);
     }
@@ -148,5 +188,46 @@ public class ICommercialDrugSelectionEditableTable implements IEditableTable<The
     public void flush() 
     {
         transaction_.flush();
+    }
+    
+    private ComboBox<Frequency> createFrequencyComboBox()
+    {
+        ComboBox<Frequency> cmbb = new ComboBox<Frequency>(form_.getInteractionState(),form_);
+        
+        cmbb.addItem(new DataComboMessage<Frequency>(Frequency.DAYS,    Frequency.DAYS.toString()));
+        cmbb.addItem(new DataComboMessage<Frequency>(Frequency.WEEKS,   Frequency.WEEKS.toString()));
+        cmbb.addItem(new DataComboMessage<Frequency>(Frequency.MONTHS,  Frequency.MONTHS.toString()));
+        cmbb.addItem(new DataComboMessage<Frequency>(Frequency.YEARS,   Frequency.YEARS.toString()));
+        cmbb.addItem(new DataComboMessage<Frequency>(Frequency.PERDAYS, Frequency.PERDAYS.toString()));
+        cmbb.addItem(new DataComboMessage<Frequency>(Frequency.PERWEEKS,Frequency.PERWEEKS.toString()));
+        cmbb.addItem(new DataComboMessage<Frequency>(Frequency.PERMONTHS,Frequency.PERMONTHS.toString()));
+        cmbb.addItem(new DataComboMessage<Frequency>(Frequency.PERYEARS, Frequency.PERYEARS.toString()));
+        
+        return cmbb;
+    }
+    
+    private Long getFrequency(WWidget value, WWidget combo_freq){
+        TextField tf = (TextField)value;
+        if(tf.text().equals(""))
+        {
+            return null;
+        }
+        else
+        {
+            double fval =  Double.parseDouble(tf.text());
+            Frequency freq = ((ComboBox<Frequency>)combo_freq).currentValue();
+            return (long)freq.timesToInterval(fval);
+        }
+    }
+    
+    private void setFrequency(Long value, TextField tf, ComboBox<Frequency> combo_freq)
+    {
+        if(value != null){
+            Frequency f = Frequency.getFrequency((double)value);
+            if(f != null){
+                tf.setText(java.lang.Math.round(f.getX()) +"");
+                combo_freq.selectItem(f.toString());
+            }
+        }
     }
 }
--- regadb-ui/src/net/sf/regadb/ui/form/singlePatient/IGenericDrugSelectionEditableTable.java
+++ regadb-ui/src/net/sf/regadb/ui/form/singlePatient/IGenericDrugSelectionEditableTable.java
@@ -10,11 +10,12 @@ import net.sf.regadb.db.Transaction;
 import net.sf.regadb.ui.framework.RegaDBMain;
 import net.sf.regadb.ui.framework.forms.FormWidget;
 import net.sf.regadb.ui.framework.forms.InteractionState;
+import net.sf.regadb.ui.framework.forms.fields.CheckBox;
 import net.sf.regadb.ui.framework.forms.fields.ComboBox;
 import net.sf.regadb.ui.framework.forms.fields.FieldType;
 import net.sf.regadb.ui.framework.forms.fields.TextField;
-import net.sf.regadb.ui.framework.widgets.editableTable.EditableTable;
 import net.sf.regadb.ui.framework.widgets.editableTable.IEditableTable;
+import net.sf.regadb.util.frequency.Frequency;
 import net.sf.witty.wt.WWidget;
 
 public class IGenericDrugSelectionEditableTable implements IEditableTable<TherapyGeneric>
@@ -22,7 +23,12 @@ public class IGenericDrugSelectionEditableTable implements IEditableTable<Therap
     private FormWidget form_;
     private Therapy therapy_;
     private Transaction transaction_;
-    private static final String [] headers_ = {"editableTable.drug.colName.drug", "editableTable.drugGeneric.colName.dosage"};
+    private static final String [] headers_ = { "editableTable.drug.colName.drug",
+                                                "editableTable.drugGeneric.colName.dosage",
+                                                "editableTable.drugGeneric.colName.frequency",
+                                                "editableTable.drugGeneric.colName.frequency_type",
+                                                "editableTable.drugGeneric.colName.placebo",
+                                                "editableTable.drugGeneric.colName.blind"};
 
     public IGenericDrugSelectionEditableTable(FormWidget form, Therapy therapy)
     {
@@ -33,7 +39,11 @@ public class IGenericDrugSelectionEditableTable implements IEditableTable<Therap
     public void addData(WWidget[] widgets)
     {
         DrugGeneric dg = ((ComboBox<DrugGeneric>)widgets[0]).currentValue();
-        TherapyGeneric tg = new TherapyGeneric(new TherapyGenericId(therapy_, dg), getDosage(((TextField)widgets[1])));
+        TherapyGeneric tg = new TherapyGeneric( new TherapyGenericId(therapy_, dg),
+                                                getDosage(((TextField)widgets[1])),
+                                                ((CheckBox)widgets[4]).isChecked(),
+                                                ((CheckBox)widgets[5]).isChecked(),
+                                                getFrequency(widgets[2],widgets[3]));
         therapy_.getTherapyGenerics().add(tg);
     }
 
@@ -42,6 +52,10 @@ public class IGenericDrugSelectionEditableTable implements IEditableTable<Therap
         DrugGeneric dg = ((ComboBox<DrugGeneric>)widgets[0]).currentValue();
         tg.setDayDosageMg(getDosage(((TextField)widgets[1])));
         tg.getId().setDrugGeneric(dg);
+
+        tg.setFrequency(getFrequency(widgets[2],widgets[3]));
+        tg.setPlacebo(((CheckBox)widgets[4]).isChecked());
+        tg.setBlind(((CheckBox)widgets[5]).isChecked());
     }
 
     private Double getDosage(TextField tf)
@@ -76,6 +90,10 @@ public class IGenericDrugSelectionEditableTable implements IEditableTable<Therap
     {
         ComboBox<DrugGeneric> combo = new ComboBox<DrugGeneric>(InteractionState.Viewing, form_);
         TextField tf = new TextField(form_.getInteractionState(), form_, FieldType.DOUBLE);
+        CheckBox cb_placebo = new CheckBox(form_.getInteractionState(), form_);
+        CheckBox cb_blind = new CheckBox(form_.getInteractionState(), form_);
+        TextField tf_freq = new TextField(form_.getInteractionState(), form_, FieldType.DOUBLE);
+        ComboBox<Frequency> combo_freq = createFrequencyComboBox();
         
         Transaction t = RegaDBMain.getApp().createTransaction();
         List<DrugGeneric> genericDrugs = t.getGenericDrugs();
@@ -87,15 +105,24 @@ public class IGenericDrugSelectionEditableTable implements IEditableTable<Therap
         
         t.commit();
         
-        WWidget[] widgets = new WWidget[2];
+        WWidget[] widgets = new WWidget[6];
         widgets[0] = combo;
         widgets[1] = tf;
+        widgets[2] = tf_freq;
+        widgets[3] = combo_freq;
+        widgets[4] = cb_placebo;
+        widgets[5] = cb_blind;
         
         if(tg.getDayDosageMg()!=null)
         {
             tf.setText(tg.getDayDosageMg()+"");
         }
         combo.selectItem(tg.getId().getDrugGeneric().getGenericName());
+        
+        cb_placebo.setChecked(tg.isPlacebo());
+        cb_blind.setChecked(tg.isBlind());
+
+        setFrequency(tg.getFrequency(), tf_freq, combo_freq);
         
         return widgets;
     }
@@ -109,6 +136,10 @@ public class IGenericDrugSelectionEditableTable implements IEditableTable<Therap
     {
         ComboBox<DrugGeneric> combo = new ComboBox<DrugGeneric>(form_.getInteractionState(), form_);
         TextField tf = new TextField(form_.getInteractionState(), form_, FieldType.DOUBLE);
+        CheckBox cb_placebo = new CheckBox(form_.getInteractionState(), form_);
+        CheckBox cb_blind = new CheckBox(form_.getInteractionState(), form_);
+        TextField tf_freq = new TextField(form_.getInteractionState(), form_, FieldType.DOUBLE);
+        ComboBox<Frequency> combo_freq = createFrequencyComboBox();
         
         Transaction t = RegaDBMain.getApp().createTransaction();
         List<DrugGeneric> genericDrugs = t.getGenericDrugs();
@@ -119,9 +150,13 @@ public class IGenericDrugSelectionEditableTable implements IEditableTable<Therap
         combo.sort();
         t.commit();
         
-        WWidget[] widgets = new WWidget[2];
+        WWidget[] widgets = new WWidget[6];
         widgets[0] = combo;
         widgets[1] = tf;
+        widgets[2] = tf_freq;
+        widgets[3] = combo_freq;
+        widgets[4] = cb_placebo;
+        widgets[5] = cb_blind;
         
         return widgets;
     }
@@ -138,7 +173,11 @@ public class IGenericDrugSelectionEditableTable implements IEditableTable<Therap
         
         Double units = getDosage((TextField)widgets[1]);
         
-        TherapyGeneric tg = new TherapyGeneric(new TherapyGenericId(null, dg), units);
+        TherapyGeneric tg = new TherapyGeneric( new TherapyGenericId(null, dg),
+                                                units,
+                                                ((CheckBox)widgets[4]).isChecked(),
+                                                ((CheckBox)widgets[5]).isChecked(),
+                                                getFrequency(widgets[2],widgets[3]));
         
         return getWidgets(tg);
     }
@@ -146,5 +185,46 @@ public class IGenericDrugSelectionEditableTable implements IEditableTable<Therap
     public void flush() 
     {
         transaction_.flush();
+    }
+    
+    private ComboBox<Frequency> createFrequencyComboBox()
+    {
+        ComboBox<Frequency> cmbb = new ComboBox<Frequency>(form_.getInteractionState(),form_);
+        
+        cmbb.addItem(new DataComboMessage<Frequency>(Frequency.DAYS,    Frequency.DAYS.toString()));
+        cmbb.addItem(new DataComboMessage<Frequency>(Frequency.WEEKS,   Frequency.WEEKS.toString()));
+        cmbb.addItem(new DataComboMessage<Frequency>(Frequency.MONTHS,  Frequency.MONTHS.toString()));
+        cmbb.addItem(new DataComboMessage<Frequency>(Frequency.YEARS,   Frequency.YEARS.toString()));
+        cmbb.addItem(new DataComboMessage<Frequency>(Frequency.PERDAYS, Frequency.PERDAYS.toString()));
+        cmbb.addItem(new DataComboMessage<Frequency>(Frequency.PERWEEKS,Frequency.PERWEEKS.toString()));
+        cmbb.addItem(new DataComboMessage<Frequency>(Frequency.PERMONTHS,Frequency.PERMONTHS.toString()));
+        cmbb.addItem(new DataComboMessage<Frequency>(Frequency.PERYEARS, Frequency.PERYEARS.toString()));
+        
+        return cmbb;
+    }
+    
+    private Long getFrequency(WWidget value, WWidget combo_freq){
+        TextField tf = (TextField)value;
+        if(tf.text().equals(""))
+        {
+            return null;
+        }
+        else
+        {
+            double fval =  Double.parseDouble(tf.text());
+            Frequency freq = ((ComboBox<Frequency>)combo_freq).currentValue();
+            return (long)freq.timesToInterval(fval);
+        }
+    }
+    
+    private void setFrequency(Long value, TextField tf, ComboBox<Frequency> combo_freq)
+    {
+        if(value != null){
+            Frequency f = Frequency.getFrequency((double)value);
+            if(f != null){
+                tf.setText(java.lang.Math.round(f.getX()) +"");
+                combo_freq.selectItem(f.toString());
+            }
+        }
     }
 }
