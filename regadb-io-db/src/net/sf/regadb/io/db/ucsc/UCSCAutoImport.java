1b5a9e4a68dcfe5a6093c206796038e61303169f
massel0 <massel0@24bc6ac0-6624-0410-8c52-f92b4954a5f8>
2008-04-16 12:07:05 +0000

    modified: cleans now the complete directory first

--- regadb-io-db/src/net/sf/regadb/io/db/ucsc/UCSCAutoImport.java
+++ regadb-io-db/src/net/sf/regadb/io/db/ucsc/UCSCAutoImport.java
@@ -3,17 +3,43 @@ package net.sf.regadb.io.db.ucsc;
 import java.io.File;
 
 public class UCSCAutoImport {
-    public static void main(String [] args) {
-        if(args.length!=2) {
-            System.err.println("Usage: UCSCAutoImport database.mdb mappingBasePath");
+    public static void main(String [] args) 
+    {
+        if(args.length!=3) 
+        {
+            System.err.println("Usage: UCSCAutoImport outputDirectory database.mdb mappingBasePath");
             System.exit(0);
         }
 
-        File tempDir = null;
-        tempDir = new File("C:\\temp\\");
-        tempDir.mkdir();
+        File tempDir = new File(args[0]);
+        
+        if(tempDir.exists())
+        {
+        	System.out.println("Removing all files from directory...");
+        	
+        	File[] files = tempDir.listFiles();
+        	
+        	if(files.length != 0)
+        	{
+        		for(int i = 0; i < files.length; i++)
+        		{
+        			File file = files[i];
+        			
+        			if(file.isFile())
+        			{
+        				System.out.println("Deleting file " +file.getName());
+        				file.delete();
+        			}
+        		}
+        	}
+        }
+        else
+        {
+	        System.out.println("Creating directory...");
+	        tempDir.mkdir();
+        }
         
         ImportUcsc imp = new  ImportUcsc();
-        imp.getData(tempDir, args[0], args[1]);
+        imp.getData(tempDir, args[1], args[2]);
     }
 }
