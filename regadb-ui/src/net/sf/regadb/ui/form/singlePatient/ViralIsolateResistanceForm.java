41ed46e9eb295aee3bc31dbd6caa219557e124dc
plibin0 <plibin0@24bc6ac0-6624-0410-8c52-f92b4954a5f8>
2008-05-09 17:22:45 +0000

    trac:#68

--- regadb-ui/src/net/sf/regadb/ui/form/singlePatient/ViralIsolateResistanceForm.java
+++ regadb-ui/src/net/sf/regadb/ui/form/singlePatient/ViralIsolateResistanceForm.java
@@ -29,7 +29,7 @@ public class ViralIsolateResistanceForm extends WContainerWidget
     private ViralIsolateForm viralIsolateForm_;
     
     private WGroupBox resistanceGroup_;
-    private WTable resistanceTable_;
+    private ViralIsolateResistanceTable resistanceTable_;
     private WPushButton refreshButton_;
     private WCheckBox showMutations_;
     
@@ -47,7 +47,7 @@ public class ViralIsolateResistanceForm extends WContainerWidget
         
         WTable wrapper = new WTable(resistanceGroup_);
         
-        resistanceTable_ = new WTable(wrapper.elementAt(0, 0));
+        resistanceTable_ = new ViralIsolateResistanceTable(wrapper.elementAt(0, 0));
         
         refreshButton_ = new WPushButton(tr("form.viralIsolate.editView.resistance.refreshButton"), wrapper.elementAt(0, 1));
         refreshButton_.setMargin(new WLength(15), WSide.Left);
@@ -69,7 +69,7 @@ public class ViralIsolateResistanceForm extends WContainerWidget
                     }
                 });
         
-        loadTable();
+        resistanceTable_.loadTable(showMutations_.isChecked(), viralIsolateForm_.getViralIsolate().getTestResults());
     }
     
     private void refreshTable() {
@@ -77,82 +77,6 @@ public class ViralIsolateResistanceForm extends WContainerWidget
         t.refresh(viralIsolateForm_.getViralIsolate());
         t.commit();
         
-        loadTable();
-    }
-    
-    private void loadTable()
-    {
-        resistanceTable_.clear();
-        
-        Transaction t = RegaDBMain.getApp().createTransaction();
-        List<DrugClass> sortedDrugClasses_  = t.getDrugClassesSortedOnResistanceRanking();
-        
-        //drug names - column position
-        HashMap<String, Integer> algoColumn = new HashMap<String, Integer>();
-        int col = 0;
-        resistanceTable_.putElementAt(0, col, new WText());
-        col = resistanceTable_.numColumns();
-        resistanceTable_.putElementAt(0, col, new WText());
-        int maxWidth = 0;
-        for(Test test : t.getTests())
-        {
-            if(StandardObjects.getGssId().equals(test.getTestType().getDescription()) && test.getAnalysis()!=null)
-            {
-                col = resistanceTable_.numColumns();
-                resistanceTable_.putElementAt(0, col, new TableHeader(lt(test.getDescription())));
-                algoColumn.put(test.getDescription(), col);
-                maxWidth += test.getDescription().length();
-            }
-        }
-        
-        //drug names - row position
-        HashMap<String, Integer> drugColumn = new HashMap<String, Integer>();
-        
-        List<DrugGeneric> genericDrugs;
-        int row;
-        boolean firstGenericDrugInThisClass;
-        for(DrugClass dc : sortedDrugClasses_)
-        {
-            genericDrugs = t.getDrugGenericSortedOnResistanceRanking(dc);
-            firstGenericDrugInThisClass = true;
-            for(DrugGeneric dg : genericDrugs)
-            {
-                row = resistanceTable_.numRows();
-                if(firstGenericDrugInThisClass)
-                {
-                    resistanceTable_.putElementAt(row, 0, new TableHeader(lt(dc.getClassId()+ ":")));
-                    firstGenericDrugInThisClass = false;
-                }
-                resistanceTable_.putElementAt(row, 1, new TableHeader(lt(dg.getGenericId())));
-                drugColumn.put(dg.getGenericId(), row);
-            }
-        }
-        
-        //clear table
-        for(int i = 1; i < resistanceTable_.numRows(); i++)
-        {
-            for(int j = 2; j< resistanceTable_.numColumns(); j++)
-            {
-                ViralIsolateFormUtils.putResistanceTableResult(null, resistanceTable_.elementAt(i, j), false, showMutations_.isChecked());
-            }
-        }
-        
-        Integer colN;
-        Integer rowN;
-        for(TestResult tr : viralIsolateForm_.getViralIsolate().getTestResults())
-        {            
-            colN = algoColumn.get(tr.getTest().getDescription());
-            rowN = drugColumn.get(ViralIsolateFormUtils.getFixedGenericId(tr));
-            if(colN!=null && rowN!=null) {
-                ViralIsolateFormUtils.putResistanceTableResult(tr, resistanceTable_.elementAt(rowN, colN), false, showMutations_.isChecked());
-            }
-            rowN = drugColumn.get(ViralIsolateFormUtils.getFixedGenericId(tr)+"/r");
-            if(colN!=null && rowN!=null) {
-                ViralIsolateFormUtils.putResistanceTableResult(tr, resistanceTable_.elementAt(rowN, colN), true, showMutations_.isChecked());
-            }
-        }
-        
-        resistanceTable_.resize(new WLength(maxWidth+maxWidth/2, WLengthUnit.FontEx), new WLength());
-        resistanceTable_.setCellPadding(4);
+        resistanceTable_.loadTable(showMutations_.isChecked(), viralIsolateForm_.getViralIsolate().getTestResults());
     }
 }
