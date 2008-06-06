4f18e7ab94fb3445006514557e3621af1fbd10ae
fromba0 <fromba0@24bc6ac0-6624-0410-8c52-f92b4954a5f8>
2008-06-06 14:51:56 +0000

    fix bug selection IE

--- regadb-ui/src/net/sf/regadb/ui/form/query/querytool/select/SimpleSelectionContainer.java
+++ regadb-ui/src/net/sf/regadb/ui/form/query/querytool/select/SimpleSelectionContainer.java
@@ -7,7 +7,7 @@ import com.pharmadm.custom.rega.queryeditor.SimpleSelection;
 import net.sf.witty.wt.SignalListener;
 import net.sf.witty.wt.WCheckBox;
 import net.sf.witty.wt.WContainerWidget;
-import net.sf.witty.wt.WEmptyEvent;
+import net.sf.witty.wt.WMouseEvent;
 import net.sf.witty.wt.i8n.WMessage;
 
 public class SimpleSelectionContainer extends WContainerWidget {
@@ -27,8 +27,8 @@ public class SimpleSelectionContainer extends WContainerWidget {
 		OutputVariable ovar = (OutputVariable) selection.getObject();
 		checkBox = new WCheckBox(new WMessage(ovar.getUniqueName(), true), this);
 		checkBox.setChecked(selection.isSelected());
-		checkBox.changed.addListener(new SignalListener<WEmptyEvent>() {
-			public void notify(WEmptyEvent a) {
+		checkBox.clicked.addListener(new SignalListener<WMouseEvent>() {
+			public void notify(WMouseEvent a) {
 				toggleFieldChecked();
 			}
 		});		
