dfd1f4805b302695a7027a9559dcbdaf242034c9
simbre1 <simbre1@24bc6ac0-6624-0410-8c52-f92b4954a5f8>
2008-06-09 18:10:26 +0000

    removed hard coded ccparse path

--- cpp2java/src/cpp2java/scripts/CreatePrettyBatchScript.java
+++ cpp2java/src/cpp2java/scripts/CreatePrettyBatchScript.java
@@ -9,6 +9,7 @@ public class CreatePrettyBatchScript {
 	public static void main(String [] args ) {
 		String workDirectory = args[0];
 		String scriptDirectory = args[1];
+		String ccparse = args[2];
 		
 		StringTokenizer st = new StringTokenizer(workDirectory, File.separatorChar+"");
 		String lastToken = "";
@@ -16,20 +17,30 @@ public class CreatePrettyBatchScript {
 			lastToken = st.nextToken();
 		}
 		
+		String options="";
+		boolean first = true;
+		
 		try {
 			FileWriter script = new FileWriter(scriptDirectory + File.separatorChar + lastToken + "_no_override_batch.sh");
 			
 			File dirF = new File(workDirectory);
+			File dirS = new File(scriptDirectory);
 	    	for(File f : dirF.listFiles()) {
 	    	    if(f.isFile() && f.getAbsolutePath().endsWith(".C")) {
-	    	    	String preproc = "./preprocess.sh " + f.getAbsolutePath() 
+	    	    	String preproc = dirS.getAbsolutePath() +"/preprocess.sh "+ options +" " + f.getAbsolutePath() 
 	    	    		+ " -c cat > " + f.getAbsolutePath() + ".preproc";
 	    	    	script.write(preproc+"\n");
 	    	    	
-	    	    	String pretty = "./pretty.sh /home/plibin0/projects/oink/oink-stack/elsa/ccparse " 
+	    	    	String pretty = dirS.getAbsolutePath() +"/pretty.sh "
+	    	    	    + ccparse +" "
 	    	    		+ f.getAbsolutePath() + ".ii 2> " 
 	    	    		+ f.getAbsolutePath() + ".ii.pretty.err";
 	    	    	script.write(pretty+"\n");
+	    	    	
+	    	    	if(first){
+	    	    	    options = "-n";
+	    	    	    first = false;
+	    	    	}
 	    	    }
 	    	}
 	    	
