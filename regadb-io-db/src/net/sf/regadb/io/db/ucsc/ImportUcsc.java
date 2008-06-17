49ee194f8db98bb4b2ef59255090e04b766a3617
massel0 <massel0@24bc6ac0-6624-0410-8c52-f92b4954a5f8>
2008-06-17 12:06:16 +0000

    updated therapy motivation mapping

--- regadb-io-db/src/net/sf/regadb/io/db/ucsc/ImportUcsc.java
+++ regadb-io-db/src/net/sf/regadb/io/db/ucsc/ImportUcsc.java
@@ -803,11 +803,20 @@ public class ImportUcsc
     	
     	if(motivation != null && !motivation.equals(""))
     	{
-    		//TODO
-    	    //Needs improvement, requires the mapping of motivation
-    		TherapyMotivation therapyMotivation = new TherapyMotivation("Toxicity");
+    	    //Still needs improvement, requires the mapping of motivation
+    		TherapyMotivation therapyMotivation = null;
+    		
+    		if(motivation.equals("toxicity"))
+    			therapyMotivation = new TherapyMotivation("Toxicity");
+    		else if(motivation.equals("unknown"))
+    			therapyMotivation = new TherapyMotivation("Unknown");
+    		else if(motivation.equals("patient's choice"))
+    			therapyMotivation = new TherapyMotivation("Patient's choice");
+    		else
+    			therapyMotivation = new TherapyMotivation("Other");
     	
-    		t.setTherapyMotivation(therapyMotivation);
+    		if(therapyMotivation != null)
+    			t.setTherapyMotivation(therapyMotivation);
     	}
     }
     
