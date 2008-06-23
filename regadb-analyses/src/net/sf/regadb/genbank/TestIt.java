de158c363981708e5b5f2ac0c4fdadcd436eeaf3
plibin0 <plibin0@24bc6ac0-6624-0410-8c52-f92b4954a5f8>
2008-06-23 11:28:23 +0000

    various scripts

---
+++ regadb-analyses/src/net/sf/regadb/genbank/TestIt.java
@@ -0,0 +1,19 @@
+package net.sf.regadb.genbank;
+
+import java.io.File;
+
+import net.sf.regadb.analysis.functions.FastaHelper;
+import net.sf.regadb.analysis.functions.FastaRead;
+import net.sf.regadb.genome.HivGenome;
+
+public class TestIt {
+	public static void main(String [] args) {
+        FastaRead read1 = FastaHelper.readFastaFile(new File("/home/plibin0/projects/genbank/hiv1/k03455.fasta"), false);
+        HivGenome genome = HivGenome.getHxb2();
+        String koenrt = genome.getPol().getSequence().seqString();
+        String k03455rt = read1.xna_.substring(2252,2549);
+        
+        System.err.println(koenrt);
+        //System.err.println(k03455rt);
+	}
+}
