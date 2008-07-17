4fe4de31fa6625569d6eacedfb7375db45acd8da
simbre1 <simbre1@24bc6ac0-6624-0410-8c52-f92b4954a5f8>
2008-07-17 17:52:08 +0000

    Add therapy with genome shortcut (forgot to commit this)

--- multi-genome-branch/regadb-persist/src/net/sf/regadb/db/Patient.java
+++ multi-genome-branch/regadb-persist/src/net/sf/regadb/db/Patient.java
@@ -191,8 +191,12 @@ public class Patient {
         eventValue.setPatient(patient);
     }
     
-    public Therapy createTherapy(Date startDate) {
+    public Therapy createTherapy(Date startDate){
+        return createTherapy(startDate, null);
+    }
+    public Therapy createTherapy(Date startDate, Genome genome) {
         Therapy result = new Therapy(patient, startDate);
+        result.setGenome(genome);
         getTherapies().add(result);
         return result;
     }
