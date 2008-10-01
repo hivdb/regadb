7ed7a160a82f25d74be56ccc99e92a4eb7f1dc7a
gbehey0 <gbehey0@24bc6ac0-6624-0410-8c52-f92b4954a5f8>
2008-10-01 10:56:02 +0000

    output method change hard-coded arguments to cmdline args

--- jhivgensim/src/org/sf/hivgensim/queries/GetNaiveSequences.java
+++ jhivgensim/src/org/sf/hivgensim/queries/GetNaiveSequences.java
@@ -1,40 +1,54 @@
 package org.sf.hivgensim.queries;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.util.HashSet;
+import java.util.Set;
 
 import net.sf.regadb.db.DrugGeneric;
+import net.sf.regadb.db.NtSequence;
 import net.sf.regadb.db.Patient;
 import net.sf.regadb.db.Therapy;
 import net.sf.regadb.db.TherapyCommercial;
 import net.sf.regadb.db.TherapyGeneric;
+import net.sf.regadb.db.ViralIsolate;
 
 public class GetNaiveSequences extends QueryInfra {
-	List<Patient> naivePatients = new ArrayList<Patient>();
-	
+	Set<Patient> naivePatients = new HashSet<Patient>(0);
+
 	public GetNaiveSequences() {
-		
+
 	}
 
 	@Override
 	protected void performQuery(Patient p) {
-		List<Therapy> proteaseTherapies = new ArrayList<Therapy>();
 		for(Therapy t : p.getTherapies()) {
 			if(!hasClassExperience("PI", t)) {
 				naivePatients.add(p);
 			}
-		}
+		}		
 	}
 	
+	/*
+	 * args[0] xmlfile
+	 * args[1] loginname
+	 * args[2] passwd
+	 * args[3] snapshotfile
+	 * args[4] outputfile
+	 */
+
 	public static void main(String [] args) {
 		GetNaiveSequences gns = new GetNaiveSequences();
 		long start = System.currentTimeMillis();
-		gns.run(new File("/home/plibin0/Desktop/patients-export.xml"));
+		gns.runOnSnapshot(new File(args[0]),args[1],args[2]);
+		gns.createSnapshot(args[3], gns.naivePatients);
+		gns.createOutput(args[4]);
 		long stop = System.currentTimeMillis();
 		System.err.println("done" + (stop - start));
 	}
-	
+
 	public boolean hasClassExperience(String drugClass, Therapy t) {
 		for(TherapyCommercial tc : t.getTherapyCommercials()) {
 			for(DrugGeneric dg : tc.getId().getDrugCommercial().getDrugGenerics()) {
@@ -43,13 +57,28 @@ public class GetNaiveSequences extends QueryInfra {
 				}
 			}
 		}
-		
+
 		for(TherapyGeneric tg : t.getTherapyGenerics()) {
 			if(tg.getId().getDrugGeneric().getDrugClass().getClassName().equals(drugClass)) {
 				return true;
 			}
 		}
-		
+
 		return false;
+	}
+
+	public void createOutput(String filename){
+		try {
+			PrintStream out = new PrintStream(new FileOutputStream(new File(filename)));
+			for(Patient p : naivePatients){
+				for(ViralIsolate vi : p.getViralIsolates()){
+					for(NtSequence seq : vi.getNtSequences()){
+						out.println(p.getPatientId()+","+seq.getNucleotides());
+					}
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
