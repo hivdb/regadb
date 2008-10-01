50dd3bf8125cf8ebad003908aebd99ca9009f442
gbehey0 <gbehey0@24bc6ac0-6624-0410-8c52-f92b4954a5f8>
2008-10-01 10:58:04 +0000

    snapshot reading/writing

--- jhivgensim/src/org/sf/hivgensim/queries/QueryInfra.java
+++ jhivgensim/src/org/sf/hivgensim/queries/QueryInfra.java
@@ -1,9 +1,16 @@
 package org.sf.hivgensim.queries;
 
+import java.io.EOFException;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.List;
+import java.util.Set;
 
 import net.sf.regadb.db.Patient;
 import net.sf.regadb.db.Transaction;
@@ -18,14 +25,43 @@ import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 
 public abstract class QueryInfra {
+
 	public QueryInfra() {
-		
+
 	}
-	public void run(File file) {
+
+	public void runOnSnapshot(File file, String loginname, String passwd){
+		try
+		{
+			FileInputStream fis = new FileInputStream(file);
+			ObjectInputStream in = new ObjectInputStream(fis);
+			Patient p;
+			while((p = (Patient)in.readObject()) != null){				
+				performQuery(p);
+			}
+			in.close();
+		}
+		catch (EOFException e) 
+		{
+			//end of file reached but unexpected?
+			return;
+		}
+		catch(IOException ex)
+		{
+			ex.printStackTrace();
+		} 
+		catch (ClassNotFoundException e)
+		{
+			e.printStackTrace();
+		} 
+
+	}
+
+	public void runOnXml(File file, String loginname, String passwd) {
 		Login login = null;
 		try
 		{
-			login = Login.authenticate("admin", "admin");
+			login = Login.authenticate(loginname, passwd);
 		}
 		catch (WrongUidException e)
 		{
@@ -35,40 +71,71 @@ public abstract class QueryInfra {
 		{
 			e.printStackTrace();
 		} 
-        catch (DisabledUserException e) 
-        {
-            e.printStackTrace();
-        }
-        
-        Transaction t = login.createTransaction();
-        
-        ImportFromXML imp = new ImportFromXML();
-        imp.loadDatabaseObjects(t);
-        FileReader r;
-        try 
-        {
-            r = new FileReader(file);
-            imp.readPatients(new InputSource(r), new ImportHandler<Patient>() {
+		catch (DisabledUserException e) 
+		{
+			e.printStackTrace();
+		}
+
+		Transaction t = login.createTransaction();
+
+		ImportFromXML imp = new ImportFromXML();
+		imp.loadDatabaseObjects(t);
+		FileReader r;
+		try 
+		{
+			r = new FileReader(file);
+			imp.readPatients(new InputSource(r), new ImportHandler<Patient>() {
 				@Override
 				public void importObject(Patient p) {
 					performQuery(p);
 				}
-            });
-        } 
-        catch (FileNotFoundException e) 
-        {
-            e.printStackTrace();
-        } 
-        catch (SAXException e) 
-        {
-            e.printStackTrace();
-        } 
-        catch (IOException e) 
-        {
-            e.printStackTrace();
-        }
+			});
+		} 
+		catch (FileNotFoundException e) 
+		{
+			e.printStackTrace();
+		} 
+		catch (SAXException e) 
+		{
+			e.printStackTrace();
+		} 
+		catch (IOException e) 
+		{
+			e.printStackTrace();
+		}
 	}
 	
+
 	protected abstract void performQuery(Patient p);
+
+	public <T> void createSnapshot(String filename, Set<T> set){
+		try 
+		{
+			ObjectOutputStream snapshotstream = new ObjectOutputStream(new FileOutputStream(filename));
+			for(T t : set){
+				snapshotstream.writeObject(t);
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}		
+	}
+
+	public <T> void createSnapshot(String filename, List<T> list){
+		try 
+		{
+			ObjectOutputStream snapshotstream = new ObjectOutputStream(new FileOutputStream(filename));
+			for(T t : list){
+				snapshotstream.writeObject(t);
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}		
+	}
+
+
 }
 
