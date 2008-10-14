2d2a56f8b0e200c516565db41cf0e599a3d644aa
simbre1 <simbre1@24bc6ac0-6624-0410-8c52-f92b4954a5f8>
2008-10-14 17:23:32 +0000

    mdb2csv script

---
+++ regadb-io-db/src/net/sf/regadb/io/db/util/db2csv/mdb2csv.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+db=$1
+tables=$2
+
+if [ $# -eq 1 ];
+then
+	for table in `mdb-tables $1`; do mdb-export $db $table > $table.csv; done;
+else
+	while read table; do mdb-export $db $table > $table.csv; done < $tables;
+fi
