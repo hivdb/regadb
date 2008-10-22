ab55843b20dcd4a32a66f1c71746b99eac031341
plibin0 <plibin0@24bc6ac0-6624-0410-8c52-f92b4954a5f8>
2008-10-22 15:24:47 +0000

    analysis in progress

--- jwt2/rega-genotype/src/main/java/rega/genotype/ui/forms/AbstractJobOverview.java
+++ jwt2/rega-genotype/src/main/java/rega/genotype/ui/forms/AbstractJobOverview.java
@@ -1,7 +1,6 @@
 package rega.genotype.ui.forms;
 
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
@@ -30,6 +29,7 @@ public abstract class AbstractJobOverview extends IForm {
 	protected File jobDir;
 
 	private WText analysisInProgress;
+	private WText willBeUpdated;
 	private WTable jobTable;
 	
 	private WTimer updater;
@@ -42,10 +42,15 @@ public abstract class AbstractJobOverview extends IForm {
 	public AbstractJobOverview(GenotypeWindow main) {
 		super(main, "monitor-form");
 	
-		WString aipm = new WString("monitorForm.analysisInProgress");
-		aipm.arg(getMain().getOrganismDefinition().getUpdateInterval()/1000);
-		analysisInProgress = new WText(aipm, this);
+		
+		analysisInProgress = new WText(tr("monitorForm.analysisInProgress"), this);
 		analysisInProgress.setStyleClass("analysisProgress");
+		new WBreak(this);
+		WString aipm = tr("monitorForm.willBeUpdated");
+		aipm.arg(getMain().getOrganismDefinition().getUpdateInterval()/1000);
+		willBeUpdated = new WText(aipm, this);
+		willBeUpdated.setStyleClass("analysisProgress");
+		new WBreak(this);
 		
 		new WBreak(this);
 		
@@ -76,10 +81,12 @@ public abstract class AbstractJobOverview extends IForm {
 		downloadContainer.clear();
 
 		analysisInProgress.setHidden(true);
+		willBeUpdated.setHidden(true);
 		
 		File jobDone = new File(jobDir.getAbsolutePath() + File.separatorChar + "DONE");
 		if (!jobDone.exists()) {
 			analysisInProgress.setHidden(false);
+			willBeUpdated.setHidden(false);
 
 			updater = new WTimer();
 			updater.setInterval(getMain().getOrganismDefinition().getUpdateInterval());
@@ -113,6 +120,7 @@ public abstract class AbstractJobOverview extends IForm {
 			if(updater!=null)
 				updater.stop();
 			analysisInProgress.setHidden(true);
+			willBeUpdated.setHidden(true);
 
 			WText downloadResult = new WText(tr("monitorForm.downloadResults"), downloadContainer);
 			WAnchor xmlFileDownload = new WAnchor("", tr("monitorForm.xmlFile"), downloadContainer);
