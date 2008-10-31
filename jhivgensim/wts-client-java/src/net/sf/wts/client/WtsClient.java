f1eb73e9945453a7880ff40249645a5f7d4a4549
gbehey0 <gbehey0@24bc6ac0-6624-0410-8c52-f92b4954a5f8>
2008-10-31 12:03:42 +0000

    added encryption functionality to client

--- jhivgensim/wts-client-java/src/net/sf/wts/client/WtsClient.java
+++ jhivgensim/wts-client-java/src/net/sf/wts/client/WtsClient.java
@@ -4,247 +4,285 @@ import java.io.File;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.rmi.RemoteException;
+import java.security.InvalidKeyException;
+import java.security.Key;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.Signature;
+import java.security.SignatureException;
+
+import javax.crypto.spec.SecretKeySpec;
 
 import net.sf.wts.client.util.AxisClient;
 import net.sf.wts.client.util.Encrypt;
 
 import org.apache.commons.io.FileUtils;
 
+import sun.misc.BASE64Decoder;
+
 public class WtsClient 
 {
-    private String url_;
-    private AxisClient axisService = new AxisClient();
-    
-    public WtsClient(String url)
-    {
-        url_ = url;
-    }
-    
-    public String getChallenge(String userName) throws RemoteException, MalformedURLException
-    {
-        axisService.removeParameters();
-        axisService.setServiceUrl(url_, "GetChallenge");
-        
-        axisService.addParameter(userName);
-        
-        String challenge = "";
-        
-        challenge = axisService.callAndGetStringResult();
-        
-        return challenge;
-    }
-    
-    public String login(String userName, String challenge, String password, String serviceName) throws RemoteException, MalformedURLException
-    {
-        axisService.removeParameters();
-        axisService.setServiceUrl(url_, "Login");
-        
-        axisService.addParameter(userName);
-        axisService.addParameter(challenge);
-        axisService.addParameter(Encrypt.encryptMD5(Encrypt.encryptMD5(password)+challenge));
-        axisService.addParameter(serviceName);
-        
-        String sessionTicket = "";
-        
-        sessionTicket = axisService.callAndGetStringResult();
-        
-        return sessionTicket;
-    }
-    
-    public void upload(String sessionTicket, String serviceName, String fileName, byte[] file) throws RemoteException, MalformedURLException
-    {
-        axisService.removeParameters();
-        axisService.setServiceUrl(url_, "Upload");
-        
-        axisService.addParameter(sessionTicket);
-        axisService.addParameter(serviceName);
-        axisService.addParameter(fileName);
-        axisService.addParameter(file);
-        
-        try 
-        {
-            axisService.call();
-        } 
-        catch (RemoteException e) 
-        {
-            e.printStackTrace();
-        }
-    }
-    
-    public byte[] download(String sessionTicket, String serviceName, String fileName) throws RemoteException, MalformedURLException
-    {
-        axisService.removeParameters();
-        axisService.setServiceUrl(url_, "Download");
-        
-        axisService.addParameter(sessionTicket);
-        axisService.addParameter(serviceName);
-        axisService.addParameter(fileName);
-        
-        byte[] array = null;
-        try 
-        {
-            array = axisService.callAndGetByteArrayResult();
-        } 
-        catch (RemoteException e) 
-        {
-            e.printStackTrace();
-        }
-        
-        return array;
-    }
-    
-    public void download(String sessionTicket, String serviceName, String fileName, File toWrite) throws RemoteException, MalformedURLException
-    {
-        byte[] array = download(sessionTicket, serviceName, fileName);
-        if(array!=null)
-        {
-            try 
-            {
-                FileUtils.writeByteArrayToFile(toWrite, array);
-            } 
-            catch (IOException e) 
-            {
-                e.printStackTrace();
-            }
-        }
-    }
-    
-    public void upload(String sessionTicket, String serviceName, String fileName, File localLocation) throws RemoteException, MalformedURLException
-    {
-        byte[] array = null;
-        try 
-        {
-            array = FileUtils.readFileToByteArray(localLocation);;
-        } 
-        catch (IOException e) 
-        {
-            e.printStackTrace();
-        }
-        
-        upload(sessionTicket, serviceName, fileName, array);
-    }
-    
-    public byte[] monitorLogFile(String sessionTicket, String serviceName) throws RemoteException, MalformedURLException
-    {
-        axisService.removeParameters();
-        axisService.setServiceUrl(url_, "MonitorLogFile");
-        
-        axisService.addParameter(sessionTicket);
-        axisService.addParameter(serviceName);
+	private String url_;
+	private AxisClient axisService = new AxisClient();
+	private Key sessionKey;
+	private PrivateKey privateKey; 
 
-        byte[] array = null;
-        try 
-        {
-            array = axisService.callAndGetByteArrayResult();
-        } 
-        catch (RemoteException e) 
-        {
-            e.printStackTrace();
-        }
-        
-        return array;
-    }
-    
-    public byte[] monitorLogTail(String sessionTicket, String serviceName, int numberOfLines) throws RemoteException, MalformedURLException
-    {
-        axisService.removeParameters();
-        axisService.setServiceUrl(url_, "MonitorLogTail");
-        
-        axisService.addParameter(sessionTicket);
-        axisService.addParameter(serviceName);
-        axisService.addParameter(numberOfLines);
+	public WtsClient(String url)
+	{
+		url_ = url;
+	}
 
-        byte[] array = null;
-        try 
-        {
-            array = axisService.callAndGetByteArrayResult();
-        } 
-        catch (RemoteException e) 
-        {
-            e.printStackTrace();
-        }
-        
-        return array;
-    }
-    
-    public String monitorStatus(String sessionTicket, String serviceName) throws RemoteException, MalformedURLException
-    {
-        axisService.removeParameters();
-        axisService.setServiceUrl(url_, "MonitorStatus");
-        
-        axisService.addParameter(sessionTicket);
-        axisService.addParameter(serviceName);
-        
-        String status = "";
-        
-        try 
-        {
-            status = axisService.callAndGetStringResult();
-        } 
-        catch (RemoteException e) 
-        {
-            e.printStackTrace();
-        }
+	public String getChallenge(String userName) throws RemoteException, MalformedURLException
+	{
+		axisService.removeParameters();
+		axisService.setServiceUrl(url_, "GetChallenge");
 
-        return status;
-    }
-    
-    public void start(String sessionTicket, String serviceName) throws RemoteException, MalformedURLException
-    {
-        axisService.removeParameters();
-        axisService.setServiceUrl(url_, "Start");
-        
-        axisService.addParameter(sessionTicket);
-        axisService.addParameter(serviceName);
-        
-        try 
-        {
-            axisService.call();
-        } 
-        catch (RemoteException e)
-        {
-            e.printStackTrace();
-        }
-    }
-    
-    public void closeSession(String sessionTicket, String serviceName) throws RemoteException, MalformedURLException
-    {
-        axisService.removeParameters();
-        axisService.setServiceUrl(url_, "CloseSession");
-        
-        axisService.addParameter(sessionTicket);
-        axisService.addParameter(serviceName);
-        
-        try 
-        {
-            axisService.call();
-        } 
-        catch (RemoteException e)
-        {
-            e.printStackTrace();
-        }
-    }
-    
-    public void stop(String sessionTicket, String serviceName) throws RemoteException, MalformedURLException
-    {
-        axisService.removeParameters();
-        axisService.setServiceUrl(url_, "Stop");
-        
-        axisService.addParameter(sessionTicket);
-        axisService.addParameter(serviceName);
-        
-        try 
-        {
-            axisService.call();
-        } 
-        catch (RemoteException e)
-        {
-            e.printStackTrace();
-        }
-    }
+		axisService.addParameter(userName);
 
-    public String getUrl() 
-    {
-        return url_;
-    }
+		String challenge = "";
+
+		challenge = axisService.callAndGetStringResult();
+
+		return challenge;
+	}
+
+	public String login(String userName, String challenge, String password, String serviceName) throws RemoteException, MalformedURLException
+	{
+		//sign challenge
+		try {
+			Signature sign = Signature.getInstance("SHA1withRSA");
+			sign.initSign(privateKey);
+			sign.update(challenge.getBytes());
+			byte[] signedChallenge = sign.sign();
+			
+			axisService.removeParameters();
+			axisService.setServiceUrl(url_, "Login");
+
+			axisService.addParameter(userName);
+			axisService.addParameter(challenge);
+			axisService.addParameter(new String(signedChallenge));
+			axisService.addParameter(serviceName);
+		} catch (SignatureException e) {
+			e.printStackTrace();
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		} catch (InvalidKeyException e) {
+			e.printStackTrace();
+		}		
+		
+		//send signedChallenge
+		byte[] answer = axisService.callAndGetByteArrayResult();
+
+		//decrypt answer
+		String decryptedAnswer = Encrypt.decrypt(answer,privateKey);
+		
+		//retrieve sessionkey and store it
+		String encodedSessionKey = decryptedAnswer.substring(decryptedAnswer.lastIndexOf("_")+1, decryptedAnswer.length());
+		try {
+			sessionKey = new SecretKeySpec((new BASE64Decoder()).decodeBuffer(encodedSessionKey),"AES");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		//return sessionTicket
+		return decryptedAnswer.substring(0, decryptedAnswer.lastIndexOf("_"));
+	}
+
+	public void upload(String sessionTicket, String serviceName, String fileName, byte[] file) throws RemoteException, MalformedURLException
+	{
+		axisService.removeParameters();
+		axisService.setServiceUrl(url_, "Upload");
+
+		axisService.addParameter(sessionTicket);
+		axisService.addParameter(serviceName);
+		axisService.addParameter(fileName);
+		axisService.addParameter(Encrypt.encrypt(sessionKey, file));
+
+		try 
+		{
+			axisService.call();
+		} 
+		catch (RemoteException e) 
+		{
+			e.printStackTrace();
+		}
+	}
+
+	public byte[] download(String sessionTicket, String serviceName, String fileName) throws RemoteException, MalformedURLException
+	{
+		axisService.removeParameters();
+		axisService.setServiceUrl(url_, "Download");
+
+		axisService.addParameter(sessionTicket);
+		axisService.addParameter(serviceName);
+		axisService.addParameter(fileName);
+
+		byte[] array = null;
+		try 
+		{
+			array = axisService.callAndGetByteArrayResult();
+		} 
+		catch (RemoteException e) 
+		{
+			e.printStackTrace();
+		}
+
+		return Encrypt.decrypt(sessionKey, array);
+	}
+
+	public void download(String sessionTicket, String serviceName, String fileName, File toWrite) throws RemoteException, MalformedURLException
+	{
+		byte[] array = download(sessionTicket, serviceName, fileName);
+		
+		if(array!=null)
+		{
+			try 
+			{
+				FileUtils.writeByteArrayToFile(toWrite, Encrypt.decrypt(sessionKey, array));
+			} 
+			catch (IOException e) 
+			{
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public void upload(String sessionTicket, String serviceName, String fileName, File localLocation) throws RemoteException, MalformedURLException
+	{
+		byte[] array = null;
+		try 
+		{
+			array = FileUtils.readFileToByteArray(localLocation);;
+		} 
+		catch (IOException e) 
+		{
+			e.printStackTrace();
+		}
+
+		upload(sessionTicket, serviceName, fileName, Encrypt.encrypt(sessionKey, array));
+	}
+
+	public byte[] monitorLogFile(String sessionTicket, String serviceName) throws RemoteException, MalformedURLException
+	{
+		axisService.removeParameters();
+		axisService.setServiceUrl(url_, "MonitorLogFile");
+
+		axisService.addParameter(sessionTicket);
+		axisService.addParameter(serviceName);
+
+		byte[] array = null;
+		try 
+		{
+			array = axisService.callAndGetByteArrayResult();
+		} 
+		catch (RemoteException e) 
+		{
+			e.printStackTrace();
+		}
+
+		return Encrypt.decrypt(sessionKey, array);
+	}
+
+	public byte[] monitorLogTail(String sessionTicket, String serviceName, int numberOfLines) throws RemoteException, MalformedURLException
+	{
+		axisService.removeParameters();
+		axisService.setServiceUrl(url_, "MonitorLogTail");
+
+		axisService.addParameter(sessionTicket);
+		axisService.addParameter(serviceName);
+		axisService.addParameter(numberOfLines);
+
+		byte[] array = null;
+		try 
+		{
+			array = axisService.callAndGetByteArrayResult();
+		} 
+		catch (RemoteException e) 
+		{
+			e.printStackTrace();
+		}
+
+		return Encrypt.decrypt(sessionKey, array);
+	}
+
+	public String monitorStatus(String sessionTicket, String serviceName) throws RemoteException, MalformedURLException
+	{
+		axisService.removeParameters();
+		axisService.setServiceUrl(url_, "MonitorStatus");
+
+		axisService.addParameter(sessionTicket);
+		axisService.addParameter(serviceName);
+
+		String status = "";
+
+		try 
+		{
+			status = axisService.callAndGetStringResult();
+		} 
+		catch (RemoteException e) 
+		{
+			e.printStackTrace();
+		}
+
+		return status;
+	}
+
+	public void start(String sessionTicket, String serviceName) throws RemoteException, MalformedURLException
+	{
+		axisService.removeParameters();
+		axisService.setServiceUrl(url_, "Start");
+
+		axisService.addParameter(sessionTicket);
+		axisService.addParameter(serviceName);
+
+		try 
+		{
+			axisService.call();
+		} 
+		catch (RemoteException e)
+		{
+			e.printStackTrace();
+		}
+	}
+
+	public void closeSession(String sessionTicket, String serviceName) throws RemoteException, MalformedURLException
+	{
+		axisService.removeParameters();
+		axisService.setServiceUrl(url_, "CloseSession");
+
+		axisService.addParameter(sessionTicket);
+		axisService.addParameter(serviceName);
+
+		try 
+		{
+			axisService.call();
+		} 
+		catch (RemoteException e)
+		{
+			e.printStackTrace();
+		}
+	}
+
+	public void stop(String sessionTicket, String serviceName) throws RemoteException, MalformedURLException
+	{
+		axisService.removeParameters();
+		axisService.setServiceUrl(url_, "Stop");
+
+		axisService.addParameter(sessionTicket);
+		axisService.addParameter(serviceName);
+
+		try 
+		{
+			axisService.call();
+		} 
+		catch (RemoteException e)
+		{
+			e.printStackTrace();
+		}
+	}
+
+	public String getUrl() 
+	{
+		return url_;
+	}
 }
--- jhivgensim/wts-client-java/src/net/sf/wts/client/util/Encrypt.java
+++ jhivgensim/wts-client-java/src/net/sf/wts/client/util/Encrypt.java
@@ -1,41 +1,170 @@
 package net.sf.wts.client.util;
 
+import java.io.IOException;
 import java.io.UnsupportedEncodingException;
+import java.security.InvalidKeyException;
+import java.security.Key;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.Security;
 
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.KeyGenerator;
+import javax.crypto.NoSuchPaddingException;
+
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+
+import sun.misc.BASE64Decoder;
 import sun.misc.BASE64Encoder;
 
-public class Encrypt
-{
-	public static synchronized String encryptMD5(String plaintext)
-	{
-		return encrypt(plaintext, "MD5");
+public class Encrypt {
+
+	static {
+		Security.addProvider(new BouncyCastleProvider());
+	}
+
+	public static final String SECURITY_PROVIDER_STRING = "BC";
+	public static final String ASYMMETRIC_ALGORITHM_NAME = "RSA";
+	public static final String SYMMETRIC_ALGORITHM_NAME = "AES";
+	public static final String ASYMMETRIC_CIPHER_TRANSFORMATION = "RSA/ECB/PKCS1Padding";
+	public static final String SYMMETRIC_CIPHER_TRANSFORMATION = "AES";
+	public static final int LENGTH_OF_GENERATED_ASYMMETRIC_KEYS = 2048;
+	public static final int LENGTH_OF_GENERATED_SYMMETRIC_KEYS = 128;
+
+	public static final Key getNewSessionKey(){
+		try {
+			KeyGenerator keyGen = KeyGenerator.getInstance(SYMMETRIC_ALGORITHM_NAME,SECURITY_PROVIDER_STRING);
+			keyGen.init(LENGTH_OF_GENERATED_SYMMETRIC_KEYS);
+			return keyGen.generateKey();
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		} catch (NoSuchProviderException e) {
+			e.printStackTrace();
+		}
+		//no arguments:
+		//so exception will not occur for this code
+		return null;	
+	}
+
+	private static final Cipher getEncryptCipher(Key k){
+		Cipher c = null;
+		try {
+			if(k.getAlgorithm().equals(ASYMMETRIC_ALGORITHM_NAME)){
+				c = Cipher.getInstance(ASYMMETRIC_ALGORITHM_NAME,SECURITY_PROVIDER_STRING);
+				c.init(Cipher.ENCRYPT_MODE, k);
+			}else if(k.getAlgorithm().equals(SYMMETRIC_ALGORITHM_NAME)){
+				c = Cipher.getInstance(SYMMETRIC_CIPHER_TRANSFORMATION,SECURITY_PROVIDER_STRING);
+				c.init(Cipher.ENCRYPT_MODE, k);
+			}
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		} catch (NoSuchPaddingException e) {
+			e.printStackTrace();
+		} catch (InvalidKeyException e) {
+			e.printStackTrace();
+		} catch (NoSuchProviderException e) {
+			e.printStackTrace();
+		}
+		return c;
+	}
+
+	private static final Cipher getDecryptCipher(Key k){
+		Cipher c = null;
+		try {
+			if(k.getAlgorithm().equals(ASYMMETRIC_ALGORITHM_NAME)){
+				c = Cipher.getInstance(ASYMMETRIC_ALGORITHM_NAME,SECURITY_PROVIDER_STRING);
+				c.init(Cipher.DECRYPT_MODE, k);
+			}else if(k.getAlgorithm().equals(SYMMETRIC_ALGORITHM_NAME)){
+				c = Cipher.getInstance(SYMMETRIC_CIPHER_TRANSFORMATION,SECURITY_PROVIDER_STRING);
+				c.init(Cipher.DECRYPT_MODE, k);
+			}
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		} catch (NoSuchPaddingException e) {
+			e.printStackTrace();
+		} catch (InvalidKeyException e) {
+			e.printStackTrace();
+		} catch (NoSuchProviderException e) {
+			e.printStackTrace();
+		}
+		return c;
+	}
+
+	public static final byte[] encrypt(Key sessionKey, byte[] input){
+		try {
+			return getEncryptCipher(sessionKey).doFinal(input);
+		} catch (IllegalBlockSizeException e) {
+			e.printStackTrace();
+		} catch (BadPaddingException e) {
+			e.printStackTrace();
+		}
+		throw new Error();
+	}
+
+	public static final byte[] decrypt(Key sessionKey, byte[] input){
+		try {
+			return getDecryptCipher(sessionKey).doFinal(input);
+		} catch (IllegalBlockSizeException e) {
+			e.printStackTrace();
+		} catch (BadPaddingException e) {
+			e.printStackTrace();
+		}
+		throw new Error();
+	}
+
+	public static String decrypt(byte[] message, PrivateKey key){
+		try {
+			Cipher decryptCipher = getDecryptCipher(key);
+			byte[] b = decryptCipher.doFinal(message);
+			return new String(b);
+		} catch (IllegalBlockSizeException e) {
+			e.printStackTrace();
+			System.err.println("message too long");
+		} catch (BadPaddingException e) {
+			e.printStackTrace();
+		} 
+		throw new Error();
 	}
 	
-	  private static synchronized String encrypt(String plaintext, String algo)
-	  {
-	    MessageDigest md = null;
-	    try
-	    {
-	      md = MessageDigest.getInstance(algo);
-	    }
-	    catch(NoSuchAlgorithmException e)
-	    {
-	      e.printStackTrace();
-	      return null;
-	    }
-	    try
-	    {
-	      md.update(plaintext.getBytes("UTF-8"));
-	    }
-	    catch(UnsupportedEncodingException e)
-	    {
-	    	e.printStackTrace();
-	    	return null;
-	    }
-	    byte raw[] = md.digest();
-	    String hash = (new BASE64Encoder()).encode(raw);
-	    return hash;
-	  }
+	public static byte[] encrypt(String message, PublicKey key){
+		try {
+			Cipher encryptCipher = getEncryptCipher(key);
+			byte[] b = encryptCipher.doFinal(message.getBytes());
+			return b;
+		} catch (IllegalBlockSizeException e) {
+			e.printStackTrace();
+			System.err.println("message too long");
+		} catch (BadPaddingException e) {
+			e.printStackTrace();
+		} 
+		throw new Error();
+	}
+	
+	public static synchronized String encryptMD5(String plaintext) {
+		return encrypt(plaintext, "MD5");
+	}
+
+	private static synchronized String encrypt(String plaintext, String algo) {
+		MessageDigest md = null;
+		try {
+			md = MessageDigest.getInstance(algo);
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+			return null;
+		}
+		try {
+			md.update(plaintext.getBytes("UTF-8"));
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+			return null;
+		}
+		byte raw[] = md.digest();
+		String hash = (new BASE64Encoder()).encode(raw);
+		return hash;
+	}	
 }
