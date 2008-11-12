e8234aa77515681e32b6cdae34623aac408c510c
simbre1 <simbre1@24bc6ac0-6624-0410-8c52-f92b4954a5f8>
2008-11-12 17:01:53 +0000

    name <-> lastname

--- regadb-ui/src/net/sf/regadb/ui/datatable/patient/IPatientDataTable.java
+++ regadb-ui/src/net/sf/regadb/ui/datatable/patient/IPatientDataTable.java
@@ -48,8 +48,8 @@ public class IPatientDataTable implements IDataTable<Pair<Patient,PatientAttribu
 	    
 	    addColumn("dataTable.patient.colName.dataSet", new DatasetFilter(t), "dataset.description", true,20);
 	    addColumn("dataTable.patient.colName.patientId", new StringFilter(), "patient.patientId", true,20);
-	    addColumn("dataTable.patient.colName.name", new StringFilter(), "patient.lastName", true,20);
-	    addColumn("dataTable.patient.colName.surName", new StringFilter(), "patient.firstName", true,20);
+	    addColumn("dataTable.patient.colName.name", new StringFilter(), "patient.firstName", true,20);
+	    addColumn("dataTable.patient.colName.surName", new StringFilter(), "patient.lastName", true,20);
 	    addColumn("dataTable.patient.colName.attribute", getAttributeFilter(), "attributeValue.value", true,20);
 	}
 	
