d4350760263d2a45c0372e455e2bf893c6428f7c
simbre1 <simbre1@24bc6ac0-6624-0410-8c52-f92b4954a5f8>
2008-12-12 11:46:06 +0000

    access mdb to csv script, uses mdb-tables program which handles unicode chars correctly

---
+++ multi-genome-branch/regadb-io-db/src/net/sf/regadb/io/db/util/db2csv/mdb2csv.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+db="$1"
+tables="$2"
+
+if [ $# -eq 1 ];
+then
+	for table in `mdb-tables "$1" -1 | sed -e 's/ /---/g' `;
+	do
+		tablename=`echo $table | sed -e 's/---/ /g'`;
+		tablefile=`echo $table | sed -e 's/---/_/g'`;
+		mdb-export "$db" "$tablename" > "$tablefile.csv";
+	done;
+else
+	while read table; do mdb-export "$db" "$table" > "$table.csv"; done < "$tables";
+fi
