036537f35678a7877b969af7ee92b31cae1188d2
plibin0 <plibin0@24bc6ac0-6624-0410-8c52-f92b4954a5f8>
2009-01-12 17:22:59 +0000

    test fasta align

---
+++ regadb-analyses/src/net/sf/regadb/align/test/TestFastaAlign.java
@@ -0,0 +1,104 @@
+package net.sf.regadb.align.test;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.NoSuchElementException;
+
+import net.sf.regadb.align.Aligner;
+import net.sf.regadb.align.local.LocalAlignmentService;
+import net.sf.regadb.db.AaInsertion;
+import net.sf.regadb.db.AaMutation;
+import net.sf.regadb.db.AaSequence;
+import net.sf.regadb.db.NtSequence;
+import net.sf.regadb.db.Protein;
+import net.sf.regadb.io.util.StandardObjects;
+
+import org.biojava.bio.BioException;
+import org.biojava.bio.seq.Sequence;
+import org.biojavax.bio.seq.RichSequenceIterator;
+
+public class TestFastaAlign {
+	public static void main(String [] args) {
+        RichSequenceIterator xna = null;
+        FileReader uploadedStream = null;
+        BufferedReader br = null;
+        
+        Map<String, Protein> proteinMap = new HashMap<String, Protein>();
+        
+        for(Protein p : StandardObjects.getProteins()) {
+            proteinMap.put(p.getAbbreviation(), p);
+        }
+        
+        Aligner aligner = new Aligner(new LocalAlignmentService(), proteinMap);
+        
+        try 
+        {
+            uploadedStream = new FileReader(args[0]);
+            br = new BufferedReader(uploadedStream);
+            xna = org.biojavax.bio.seq.RichSequence.IOTools.readFastaDNA(br, null);
+        }
+        catch (NoSuchElementException ex) 
+        {
+        	ex.printStackTrace();
+        }
+        catch (FileNotFoundException ex) 
+        {
+        	ex.printStackTrace();
+        }
+        catch(IOException ioe)
+        {
+        	ioe.printStackTrace();
+        }
+        
+        if(xna!=null)
+        { 
+            while(xna.hasNext())
+            {
+                try {
+                    Sequence seq = xna.nextRichSequence();
+                    NtSequence ntseq = new NtSequence();
+                    ntseq.setNucleotides(seq.seqString());
+                    		
+                    List<AaSequence> result = aligner.alignHiv(ntseq);
+                    for(AaSequence aaseq : result) {
+                    	for(AaMutation aamut : aaseq.getAaMutations()) {
+                    		System.out.print(aaseq.getProtein().getAbbreviation() + 
+                    				aamut.getId().getMutationPosition() + 
+                    				aamut.getAaMutation()+ " ");
+                    	}
+                    	
+                    	for(AaInsertion aains : aaseq.getAaInsertions()) {
+                    		System.out.print(aaseq.getProtein().getAbbreviation()+
+                    				aains.getId().getInsertionPosition()+aains.getId().getInsertionOrder()+
+                    				aains.getAaInsertion()+" ");
+                    	}
+                    }
+                    System.out.print("\n");
+                }
+                catch (NoSuchElementException e) 
+                {
+                    break;
+                } 
+                catch (BioException e) 
+                {
+                    break;
+                }
+            }
+        }
+        
+        try 
+        {
+            uploadedStream.close();
+            br.close();
+        } 
+        catch (IOException e) 
+        {
+            e.printStackTrace();
+        }
+	}
+}
