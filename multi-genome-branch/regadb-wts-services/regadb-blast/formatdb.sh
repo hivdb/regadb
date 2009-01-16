cc19ff2012afb9a09ef8ba188070698cb7d04545
plibin0 <plibin0@24bc6ac0-6624-0410-8c52-f92b4954a5f8>
2009-01-16 18:03:15 +0000

    fixes + blastdb creation script

--- multi-genome-branch/regadb-wts-services/regadb-blast/formatdb.sh
+++ multi-genome-branch/regadb-wts-services/regadb-blast/formatdb.sh
@@ -7,7 +7,7 @@ speciesdir=species
 rm -rf "$tmpdir"
 mkdir "$tmpdir"
 touch "$tmpdir/all.fasta"
-cat "$speciesdir/*.fasta" > "$tmpdir/all.fasta"
+cat "$speciesdir"/*.fasta > "$tmpdir/all.fasta"
 formatdb -i "$tmpdir/all.fasta" -p F -n all
 
 mv all.* formatdb.log "$tmpdir/"
