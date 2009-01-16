0d7eb57180ef926f12fe0d8529c90b92ce23a2a8
simbre1 <simbre1@24bc6ac0-6624-0410-8c52-f92b4954a5f8>
2009-01-16 17:33:01 +0000

    blast db creation script

---
+++ multi-genome-branch/regadb-wts-services/regadb-blast/formatdb.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+tmpdir=tmp
+dbbasedir=db
+speciesdir=species
+
+rm -rf "$tmpdir"
+mkdir "$tmpdir"
+touch "$tmpdir/all.fasta"
+cat "$speciesdir/*.fasta" > "$tmpdir/all.fasta"
+formatdb -i "$tmpdir/all.fasta" -p F -n all
+
+mv all.* formatdb.log "$tmpdir/"
+dbdir="$dbbasedir/"`date +'%Y%m%d'`
+
+mv "$tmpdir" "$dbdir"
