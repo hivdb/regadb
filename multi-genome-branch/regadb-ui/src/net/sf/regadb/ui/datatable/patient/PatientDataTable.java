598f669cd6fa6172c675b0db486f7167ade70c96
simbre1 <simbre1@24bc6ac0-6624-0410-8c52-f92b4954a5f8>
2009-01-27 16:45:33 +0000

    hibernate problem in patient datatable

--- multi-genome-branch/regadb-ui/src/net/sf/regadb/ui/datatable/patient/PatientDataTable.java
+++ multi-genome-branch/regadb-ui/src/net/sf/regadb/ui/datatable/patient/PatientDataTable.java
@@ -2,8 +2,6 @@ package net.sf.regadb.ui.datatable.patient;
 
 import net.sf.regadb.db.Patient;
 import net.sf.regadb.db.PatientAttributeValue;
-import net.sf.regadb.db.Transaction;
-import net.sf.regadb.ui.framework.RegaDBMain;
 import net.sf.regadb.ui.framework.widgets.datatable.DataTable;
 import net.sf.regadb.ui.framework.widgets.datatable.IDataTable;
 import net.sf.regadb.util.pair.Pair;
@@ -15,10 +13,12 @@ public class PatientDataTable extends DataTable<Pair<Patient,PatientAttributeVal
 
     @SuppressWarnings("unchecked")
     public boolean stillExists(Object obj) {
-        Pair<Patient,PatientAttributeValue> pair = (Pair<Patient,PatientAttributeValue>)obj;
-        Transaction trans = RegaDBMain.getApp().createTransaction();
-        boolean state = trans.stillExists(pair.getKey());
-        trans.commit();
-        return state;
+// disabled because somehow hibernate 3.3.1.GA doesn't always like this    	
+//        Pair<Patient,PatientAttributeValue> pair = (Pair<Patient,PatientAttributeValue>)obj;
+//        Transaction trans = RegaDBMain.getApp().createTransaction();
+//        boolean state = trans.stillExists(pair.getKey());
+//        trans.commit();
+//        return state;
+    	return true;
     }
 }
