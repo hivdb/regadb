0d5cd612f79b64e04ce2c31e33414712f08620a2
gbehey0 <gbehey0@24bc6ac0-6624-0410-8c52-f92b4954a5f8>
2009-03-06 14:08:00 +0000

    snapshot creation fix

--- multi-genome-branch/regadb-io/src/net/sf/regadb/io/generation/PersistenceWriteCodeGen.java
+++ multi-genome-branch/regadb-io/src/net/sf/regadb/io/generation/PersistenceWriteCodeGen.java
@@ -11,46 +11,31 @@ import net.sf.regadb.util.hbm.InterpreteHbm;
 public class PersistenceWriteCodeGen {
 
 	public void writeClassToFile() {
-		String total = "";
-
-		//package declaration
-//		total = "package net.sf.regadb.io.persistence;\n\n";        
-
 		//imports
 		InterpreteHbm interpreter = InterpreteHbm.getInstance();
 		String imports = "import net.sf.regadb.db.Patient;\n";
 		Package p = Package.getPackage("net.sf.regadb.db");
-		
-//		for(String className : interpreter.getClassNames())
-//		{
-//			imports += "import "+className.replace("PatientImpl", "Patient") +";\n";
-//		}
 		imports += "import org.hibernate.Hibernate;\n";
-//		total+=imports+"\n";       
 
+		//rest of class
+		String total = "";
 		total+="public class ExportToPersistentObjects {\n";
 		total+="public void initialize(Patient p){\n";
 		total+="write(p);\n";
 		total+="}\n\n";
-	
-		
+
 		ArrayList<String> classes = new ArrayList<String>();
 		for(String className : interpreter.getClassNames())
 		{
 			classes.add(className.replace("net.sf.regadb.db.", "").replace("PatientImpl", "Patient"));
 		}
-		
+
 		ArrayList<String> queue = new ArrayList<String>();
 		ArrayList<String> done = new ArrayList<String>();
 		queue.add("Patient"); //starting class = patient
 		while(!queue.isEmpty()){
 			System.out.println("queue size: "+queue.size()+" done size: "+done.size());
-			String shortClassName = null;
-			if(queue.get(0).equals("TestResult") && queue.size() > 1){
-				 shortClassName = queue.get(1);
-			}else{
-				shortClassName = queue.get(0);
-			}
+			String shortClassName = queue.get(0);
 			total+= "private void write(" + shortClassName + " " + toObjectName(shortClassName) + "){\n";
 			Class c = null;
 			try {
@@ -61,22 +46,24 @@ public class PersistenceWriteCodeGen {
 			for(Method m : c.getMethods()){
 				String methodName = m.getName().replace("PatientDatasets", "Datasets");
 				if(!ignoreMethod(m)){
+					total+="\tif(!Hibernate.isInitialized("+ toObjectName(shortClassName) +"."+methodName+"())){\n";
 					total+="\tHibernate.initialize("+ toObjectName(shortClassName) +"."+methodName+"());\n";
-					if(m.getReturnType().equals(java.util.Set.class) && !done.contains(toClassName(m)) && !queue.contains(toClassName(m))){
+
+					if(m.getReturnType().equals(java.util.Set.class)){
 						total+="\tfor("+toClassName(m)+ " " + toObjectName(toClassName(m)) + ":" + toObjectName(shortClassName) + "." + methodName + "()){\n";
 						total+="\twrite("+toObjectName(toClassName(m)) + ");\n";
-						total+="\t}\n";
-						imports+= "import net.sf.regadb.db."+toClassName(m)+";\n";
-						queue.add(toClassName(m));
-					}else if(!done.contains(toClassName(m)) && !queue.contains(toClassName(m))){
+						total+="\t}\n";						
+					}else{
 						total+="\tif("+toObjectName(shortClassName) +"."+methodName+"() != null"+")write("+toObjectName(shortClassName) +"."+methodName+"());\n";
+
+					}
+					total+="\t}\n";
+					if(!done.contains(toClassName(m)) && !queue.contains(toClassName(m))){
 						imports+= "import net.sf.regadb.db."+toClassName(m)+";\n";
 						queue.add(toClassName(m));
 					}
-//				}else if(m.getName().startsWith("get") && m.getParameterTypes().length ==0 ){
-//					total+="\tHibernate.initialize("+toObjectName(shortClassName) +"."+methodName+"());\n";
-				}
 
+				}
 			}
 			total+= "}\n\n";
 			done.add(shortClassName);
@@ -102,13 +89,13 @@ public class PersistenceWriteCodeGen {
 	private String toObjectName(String className){
 		return className.substring(0,1).toLowerCase() + className.substring(1);
 	}
-	
+
 	private String toClassName(Method m){
 		return m.getGenericReturnType().toString().replace("java.util.Set<", "").replace("net.sf.regadb.db.","").replace(">", "").replace("class ", "");
 	}
-	
-	
-	
+
+
+
 	private boolean ignoreMethod(Method m){
 		return (!m.getName().startsWith("get") || // method must be a getter
 				m.getName().equals("getClass") ||
@@ -125,7 +112,7 @@ public class PersistenceWriteCodeGen {
 				m.getReturnType().isArray() ||
 				m.getReturnType().isPrimitive() ||
 				m.getParameterTypes().length !=0
-				);		
+		);		
 	}
 
 	public static void main(String[] args){
