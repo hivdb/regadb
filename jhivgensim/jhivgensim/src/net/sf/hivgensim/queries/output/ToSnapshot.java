0eb1fed6930ac07975e04bc3b79ea7ac5493c491
gbehey0 <gbehey0@24bc6ac0-6624-0410-8c52-f92b4954a5f8>
2009-03-16 16:52:16 +0000

    fix snapshot

--- jhivgensim/jhivgensim/src/net/sf/hivgensim/queries/output/ToSnapshot.java
+++ jhivgensim/jhivgensim/src/net/sf/hivgensim/queries/output/ToSnapshot.java
@@ -5,16 +5,24 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
+import java.util.Collection;
+import java.util.List;
 
-import net.sf.hivgensim.queries.framework.Query;
+import net.sf.regadb.db.Dataset;
 import net.sf.regadb.db.Patient;
+import net.sf.regadb.db.Transaction;
+import net.sf.regadb.db.login.DisabledUserException;
+import net.sf.regadb.db.login.WrongPasswordException;
+import net.sf.regadb.db.login.WrongUidException;
+import net.sf.regadb.db.session.Login;
 import net.sf.regadb.io.persistence.ExportToPersistentObjects;
+import net.sf.regadb.util.hibernate.HibernateFilterConstraint;
 
 public class ToSnapshot {
-	
+
 	private ObjectOutputStream snapshotstream;
-	
-	public ToSnapshot(File file){
+
+	public ToSnapshot(File file) {
 		try {
 			snapshotstream = new ObjectOutputStream(new FileOutputStream(file));
 		} catch (FileNotFoundException e) {
@@ -24,27 +32,46 @@ public class ToSnapshot {
 		}
 	}
 
-	
-	public void generateOutput(Query<Patient> query) {
-		try 
-		{
+	public void generateOutput() {
+		try {
+			Login login = Login.authenticate("gbehey0", "bla123");
 			ExportToPersistentObjects export = new ExportToPersistentObjects();
-			
-			int patientCounter = 0;
-			for(Patient p : query.getOutputList()){
-				export.initialize(p);
-				snapshotstream.writeObject(p);
-				patientCounter++;
-				if(patientCounter==100) {
-					System.out.print(".");
-					patientCounter = 0;
+			Transaction t = login.createTransaction();
+			List<Dataset> datasets = t.getDatasets();
+			String dataset = null;
+			for (Dataset ds : datasets) {
+				dataset = ds.getDescription();
+				System.err.println("now starting with: "+dataset);
+				HibernateFilterConstraint hfc = new HibernateFilterConstraint();
+				hfc.setClause(" dataset.description = :description ");
+				hfc.addArgument("description", dataset);
+				long n = t.getPatientCount(hfc);
+				int maxResults = 10;
+
+				for (int i = 0; i < n; i += maxResults) {
+					t.commit();
+					t.clearCache();
+					t = login.createTransaction();
+					Collection<Patient> patients = t.getPatients(t.getDataset(dataset),i,maxResults);
+					for (Patient p : patients) {
+						export.initialize(p);
+						snapshotstream.writeObject(p);
+						if(i % 100 == 0){
+							System.err.print("."); 
+						}
+					}
 				}
 			}
-			snapshotstream.close();
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
-		}		
+		} catch (WrongUidException e) {
+			e.printStackTrace();
+		} catch (WrongPasswordException e) {
+			e.printStackTrace();
+		} catch (DisabledUserException e) {
+			e.printStackTrace();
+		}
 	}
 }
