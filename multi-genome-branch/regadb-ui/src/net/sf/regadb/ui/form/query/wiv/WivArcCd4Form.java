2f71160960aa6afa0e3851ae2ce6290b0e10b3f9
simbre1 <simbre1@24bc6ac0-6624-0410-8c52-f92b4954a5f8>
2009-04-22 11:00:51 +0000

    birth date and death date are attributes now

--- multi-genome-branch/regadb-ui/src/net/sf/regadb/ui/form/query/wiv/WivArcCd4Form.java
+++ multi-genome-branch/regadb-ui/src/net/sf/regadb/ui/form/query/wiv/WivArcCd4Form.java
@@ -21,12 +21,12 @@ public class WivArcCd4Form extends WivIntervalQueryForm {
     public WivArcCd4Form(){
         super(tr("menu.query.wiv.arc.cd4"),tr("form.query.wiv.label.arc.cd4"),tr("file.query.wiv.arc.cd4"));
         
-        String query =  "select p.birthDate, tr, pav "+
-            "from TestResult tr join tr.patient p, PatientAttributeValue pav " +
-            "where pav.patient = p and pav.attribute.name = 'PatCode' "+
+        String query =  "select tr, pc, bd "+
+            "from TestResult tr join tr.patient p join p.patientAttributeValues bd join p.patientAttributeValues pc " +
+            "where pc.attribute.name = 'PatCode' and bd.attribute.name = 'Birth date' "+
             "and (tr.test.testType.description = '"+ StandardObjects.getCd4TestType().getDescription() +"' "+
             "or tr.test.testType.description = '"+ StandardObjects.getCd4PercentageTestType().getDescription() +"') "+ 
-            "and tr.testDate >= :var_start_date and tr.testDate <= :var_end_date and pav.patient.patientIi in ("+ getArcPatientQuery() +") ";
+            "and tr.testDate >= :var_start_date and tr.testDate <= :var_end_date and p.id in ("+ getArcPatientQuery() +") ";
 
         setQuery(query);
         
@@ -48,14 +48,15 @@ public class WivArcCd4Form extends WivIntervalQueryForm {
         Table out = new Table();
         
         for(Object[] o : list){
-        	Date bDate = (Date)o[0];
-        	TestResult tr = (TestResult)o[1];
-        	PatientAttributeValue pav = (PatientAttributeValue)o[2];
+        	TestResult tr = (TestResult)o[0];
+        	PatientAttributeValue pc = (PatientAttributeValue)o[1];
+            PatientAttributeValue bdt = (PatientAttributeValue)o[2];
+            Date bd = DateUtils.parseDate(bdt.getValue());
         	
         	Date tDate = tr.getTestDate();
         	int testCode = TestCode.T4.getCode();
 
-        	if(bDate != null && tDate != null && DateUtils.getDateOffset(bDate, Calendar.YEAR, 15).after(tDate)){
+        	if(bd != null && tDate != null && DateUtils.getDateOffset(bd, Calendar.YEAR, 15).after(tDate)){
         		// < 15 years old at time of test
         		testCode = TestCode.T4PERCENT.getCode();
         		if(!tr.getTest().getTestType().getDescription().equals(StandardObjects.getCd4PercentageTestType().getDescription()))
@@ -72,7 +73,7 @@ public class WivArcCd4Form extends WivIntervalQueryForm {
             
             row.add(getCentreName());
             row.add(OriginCode.ARC.getCode()+"");
-            row.add(pav.getValue());
+            row.add(pc.getValue());
             row.add(getFormattedDate(tDate));
             row.add(TypeOfInformationCode.LAB_RESULT.getCode()+"");
             row.add(testCode +"");
--- multi-genome-branch/regadb-ui/src/net/sf/regadb/ui/form/query/wiv/WivArcDeathsForm.java
+++ multi-genome-branch/regadb-ui/src/net/sf/regadb/ui/form/query/wiv/WivArcDeathsForm.java
@@ -4,52 +4,59 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
 
 import net.sf.regadb.csv.Table;
+import net.sf.regadb.db.PatientAttributeValue;
+import net.sf.regadb.db.Transaction;
 import net.sf.regadb.util.date.DateUtils;
+
+import org.hibernate.Query;
 
 public class WivArcDeathsForm extends WivIntervalQueryForm {
     
     public WivArcDeathsForm(){
         super(tr("menu.query.wiv.arc.deaths"),tr("form.query.wiv.label.arc.deaths"),tr("file.query.wiv.arc.deaths"));
-        setQuery("select p, pav from PatientImpl as p inner join p.patientAttributeValues pav " +
-        		"where p.deathDate >= :var_start_date and p.deathDate <= :var_end_date " +
-        		"and pav.attribute.name = 'PatCode' and p.patientIi in (" + getArcPatientQuery() +") "+
-        		"order by p.deathDate desc");
+        setQuery("select pc, dd from PatientImpl as p inner join p.patientAttributeValues pc inner join p.patientAttributeValues dd " +
+        		"where p.patientIi in (" + getArcPatientQuery() +") "+
+        		"and pc.attribute.name = 'PatCode' and dd.attribute.name = 'Death date' "+
+        		"order by dd.value desc");
         
         setStartDate(DateUtils.getDateOffset(getEndDate(), Calendar.YEAR, -1));
     }
 
+    @SuppressWarnings("unchecked")
     @Override
-    protected File postProcess(File csvFile) throws Exception{
-
-        Table in = readTable(csvFile);
-        if(in.numRows() < 2)
+    protected void process(File csvFile) throws Exception{
+        Transaction t = createTransaction();
+        Query q = t.createQuery(getQuery());
+        
+        List<Object[]> list = (List<Object[]>)q.list();
+        if(list.size() < 1)
             throw new EmptyResultException();
-
-        File outFile = new File(csvFile.getAbsolutePath()+".processed.csv");
-        Table out = new Table();
+        
         ArrayList<String> row;
+        Table out = new Table();
         
-        int CDeathDate = in.findColumn("PatientImpl.deathDate");
-        int CPatCode = in.findColumn("PatientAttributeValue.value");
-        
-        for(int i=1; i<in.numRows(); ++i){
+        for(Object[] o : list){
+            PatientAttributeValue pc = (PatientAttributeValue)o[0];
+            PatientAttributeValue ddt = (PatientAttributeValue)o[1];
+            Date dd = DateUtils.parseDate(ddt.getValue());
+            
             row = new ArrayList<String>();
-            
-            
             row.add(getCentreName());
             row.add(OriginCode.ARC.getCode()+"");
-            row.add(in.valueAt(CPatCode, i));
-            row.add(getFormattedDate(getDate(in.valueAt(CDeathDate, i))));
+            row.add(pc.getValue());
+            row.add(getFormattedDate(dd));
             row.add(TypeOfInformationCode.DEATH.getCode()+"");
             row.add(CauseOfDeathCode.UNKNOWN.getCode()+"");
             row.add("");
 
             out.addRow(row);
         }
+        t.commit();
         
-        out.exportAsCsv(new FileOutputStream(outFile),';',false);
-        return outFile;
+        out.exportAsCsv(new FileOutputStream(csvFile),';',false);
     }
 }
