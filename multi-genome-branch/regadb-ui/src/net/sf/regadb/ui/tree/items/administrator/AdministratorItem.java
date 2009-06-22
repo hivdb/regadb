065f5740b660e54844e24d31c35b16cf9f030f38
simbre1 <simbre1@24bc6ac0-6624-0410-8c52-f92b4954a5f8>
2009-06-22 14:45:18 +0000

    fix, used wrong string to fetch Role

--- multi-genome-branch/regadb-ui/src/net/sf/regadb/ui/tree/items/administrator/AdministratorItem.java
+++ multi-genome-branch/regadb-ui/src/net/sf/regadb/ui/tree/items/administrator/AdministratorItem.java
@@ -1,10 +1,8 @@
 package net.sf.regadb.ui.tree.items.administrator;
 
-import net.sf.regadb.db.Transaction;
 import net.sf.regadb.ui.framework.RegaDBMain;
 import net.sf.regadb.ui.framework.forms.action.ITreeAction;
 import net.sf.regadb.ui.framework.tree.TreeMenuNode;
-import net.sf.regadb.util.settings.RegaDBSettings;
 import eu.webtoolkit.jwt.WTreeNode;
 
 public class AdministratorItem extends TreeMenuNode
@@ -23,16 +21,7 @@ public class AdministratorItem extends TreeMenuNode
     @Override
     public boolean isEnabled()
     {
-        if(RegaDBMain.getApp().getLogin()!=null)
-        {
-            Transaction t = RegaDBMain.getApp().getLogin().createTransaction();
-
-            return RegaDBSettings.getInstance().getAccessPolicyConfig().getRole(
-            		t.getSettingsUser(RegaDBMain.getApp().getLogin().getUid()).getUid()).isAdmin();
-        }
-        else
-        {
-            return false;
-        }
+        return RegaDBMain.getApp().getLogin()!=null
+            && RegaDBMain.getApp().getRole().isAdmin();
     }
 }
