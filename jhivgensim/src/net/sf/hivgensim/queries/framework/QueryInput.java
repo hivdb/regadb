bb78d65612580c0c9969312029ae64ba1cbd737c
gbehey0 <gertjan.beheydt+git@gmail.com>
2009-10-08 10:38:44 +0200

    set next query for queryinput

--- jhivgensim/src/net/sf/hivgensim/queries/framework/QueryInput.java
+++ jhivgensim/src/net/sf/hivgensim/queries/framework/QueryInput.java
@@ -3,11 +3,18 @@ package net.sf.hivgensim.queries.framework;
 import net.sf.regadb.db.Patient;
 
 public abstract class QueryInput extends PreQuery<Patient> {
-
+	
+	public QueryInput() {
+		super(null);
+	}
+	
 	protected QueryInput(IQuery<Patient> nextQuery) {
 		super(nextQuery);
 	}
 	
 	public abstract void run();
-
+	
+	public void setNextQuery(IQuery<Patient> nextQuery){
+		super.setNextQuery(nextQuery);
+	}
 }
