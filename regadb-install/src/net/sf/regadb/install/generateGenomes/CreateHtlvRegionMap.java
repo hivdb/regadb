3c1a7099d3583ffecd3e974407fcefcb1672de59
pieter <pieter@smoog.(none)>
2009-11-18 15:41:00 +0100

    changes to create region map script

--- regadb-install/src/net/sf/regadb/install/generateGenomes/CreateHtlvRegionMap.java
+++ regadb-install/src/net/sf/regadb/install/generateGenomes/CreateHtlvRegionMap.java
@@ -7,8 +7,8 @@ import net.sf.regadb.install.generateGenomes.refSeqs.HTLV;
 public class CreateHtlvRegionMap {
 	public static void main(String [] args) {
 		CreateRegionMap htlvrm = new CreateRegionMap(HTLV.AB513134);
-        GenerateGenome htlvabgg = new GenerateGenome("HTLV AB513134","HTLV AB513134","",GenerateGenome.getReferenceSequence("AB513134.fasta"));
-        Genome ab = htlvabgg.generateFromFile("hiv2ben.genome");
-        htlvrm.run(HTLV.ATK1, htlvabgg, ab);
+        GenerateGenome htlvafgg = new GenerateGenome("HTLV AF033817","HTLV AF033817","",GenerateGenome.getReferenceSequence("AF033817.fasta"));
+        Genome af = htlvafgg.generateFromFile("htlv-af.genome");
+        htlvrm.run(HTLV.ATK1, htlvafgg, af);
 	}
 }
