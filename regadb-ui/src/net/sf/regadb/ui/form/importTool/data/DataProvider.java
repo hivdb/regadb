2d83404115fb719187992d492032241d5fe7e9c2
pieter <pieter@smoog.(none)>
2009-11-25 01:34:02 +0100

    implement DataProvider

--- regadb-ui/src/net/sf/regadb/ui/form/importTool/data/DataProvider.java
+++ regadb-ui/src/net/sf/regadb/ui/form/importTool/data/DataProvider.java
@@ -1,13 +1,39 @@
 package net.sf.regadb.ui.form.importTool.data;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import jxl.Sheet;
+
 public class DataProvider {
+	private Sheet sheet;
+	public DataProvider(Sheet sheet) {
+		this.sheet = sheet;
+	}
+ 	
 	public List<String> getHeaders() {
+		List<String> headers = new ArrayList<String>();
+		for (int i = 0; i < sheet.getColumns(); i++) {
+			headers.add(sheet.getCell(0, i).getContents().trim());
+		}
+		return headers;
+	}
+	
+	public String getValue(int row, String header) {
+		//TODO add support for the script
+		for (int i = 0; i < sheet.getColumns(); i++) {
+			if (sheet.getCell(0, i).getContents().trim().equals(header)) {
+				for (int j = 0; j < sheet.getRows(); j++) {
+					String value = sheet.getCell(j, i).getContents().trim();
+					if (row == j)
+						return value;
+				}
+			}
+		}
 		return null;
 	}
 	
-	public List<String> getValues(String header) {
-		return null;
+	public int getNumberRows() {
+		return sheet.getRows() - 1;
 	}
 }
