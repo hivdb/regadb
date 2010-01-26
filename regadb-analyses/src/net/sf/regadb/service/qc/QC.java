3a608b433f632c74af8bc56207aa79c373f45b79
Pieter Libin <pieter@dozer.leuven.emweb.be>
2010-01-26 10:20:15 +0100

    create string interface

--- regadb-analyses/src/net/sf/regadb/service/qc/QC.java
+++ regadb-analyses/src/net/sf/regadb/service/qc/QC.java
@@ -1,35 +1,32 @@
 package net.sf.regadb.service.qc;
 
-import java.util.Date;
 import java.util.HashSet;
 import java.util.Set;
-
-import net.sf.regadb.db.NtSequence;
 
 public class QC {
 	private static final int MINIMUM_TRUGENE_FRAGMENT_LENGTH = 20;
 
-	public static Set<NtSequence> trugeneQC(NtSequence sequence) {
-		Set<NtSequence> toReturn = new HashSet<NtSequence>();
+	public static Set<String> trugeneQC(String sequence) {
+		Set<String> toReturn = new HashSet<String>();
 		
 		String nNucleotides = 
 			"nnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn";
 
-		int endPro = sequence.getNucleotides().toLowerCase().indexOf(nNucleotides);
+		int endPro = sequence.toLowerCase().indexOf(nNucleotides);
 
 		if (endPro != -1) {
 			int startRt = endPro + nNucleotides.length();
 
-			String pro = trimNs(sequence.getNucleotides().substring(0, endPro));
-			String rt = trimNs(sequence.getNucleotides().substring(startRt));
+			String pro = trimNs(sequence.substring(0, endPro));
+			String rt = trimNs(sequence.substring(startRt));
 			
 			if (pro.length() < MINIMUM_TRUGENE_FRAGMENT_LENGTH || rt.length() < MINIMUM_TRUGENE_FRAGMENT_LENGTH) {
 				toReturn.add(sequence);
 				return toReturn;
 			}
 			
-			toReturn.add(copy(sequence, " (a)", pro));
-			toReturn.add(copy(sequence, " (b)", rt));
+			toReturn.add(pro);
+			toReturn.add(rt);
 		} else 
 			toReturn.add(sequence);
 		
@@ -55,31 +52,12 @@ public class QC {
 		return seq.substring(firstIndex, lastIndex);
 	}
 	
-	private static NtSequence copy(NtSequence seq, String labelSuffix, String sequence) {
-		NtSequence copy = new NtSequence();
-		copy.setAligned(false);
-		copy.setSequenceDate(seq.getSequenceDate());
-		copy.setLabel(seq.getLabel() + labelSuffix);
-		copy.setNucleotides(sequence);
-		return copy;
-	}
-	
 	public static void main(String [] args) {
-		NtSequence test = new NtSequence();
-		test.setSequenceDate(new Date());
-		test.setLabel("Sequence 1");
-		test.setNucleotides("NNNNNNNNNACTCTTTGGCAGCGACCCCTTGTYACAATAAAAGTAGGGGGCCARATAAAgGagGCTCTATTAGATACAGGAGCAGATGATACAGTATTAGAAGAAATAGAATTRCCAGGAAAATGGAAACCAAAAATGATAGGRGGAATTGGAGGTTTTATCAAAGTAAGACAGTATGAGCAAATTCTTATAGAAATCTGTGGAAAAAAGGCTATAGGTACAGTRTTAGTAGGMCCCACACCTrTCAACATAATTGGAAGAAATATGTTGACTCAGCTTGGATGCACRCTAAATTTTNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNTGTGAwGAAATGGAGAAGGAAGGAAAAATTTCAAAAATTGGRCCTGAAAATCCATATAACACTCCAGTATTTGCCATAAAAAAGAAGGACAGTACTAAGTGGAGAAAATTAGTAGATTTCAGGGAACTTAATAAAAGAACYCAAGACTTTTGTGAAGTTCAATTAGGAATACCACACCCAGCAGGGTTARAAAAGAARAAATCAGYGACAGTGCTRGATGTGGGGGATGCATATTTTTCAATCCCTTTAGATGAAAGCTTCAGGAAATATACTGCATTCACCATACCYAGYMYAAACAATGCAGCACCAGGGATTAGATATCAATATAATGTGCTTCCACAGGGATGGAAAGGATCACCAGCAATATTCCAGTGTAGCATGACAAAAATCTTAGAGCCCTTTAGGACAAAAAACCCAGACATAGTTATCTATCAATACGTGGATGACTTGTATGTAGSATCTGAYYTAGAAATAGGGCAACATAGAGCAAAAATAGAGGAGTTAAGAGAACATTTATTGAAATGGGGACTTACCACACCAGACAAGAAACATCAGAAAGAACCCCCAYTTCTTTGGATGGGGTATGARCTTCATCCTGACAAATGGACAGTACAGCCTATACAGCTACCA");
-		print (test);
-		Set<NtSequence> processed = trugeneQC(test);
-		for (NtSequence ntseq : processed) {
-			print (ntseq);
+		String test = "NNNNNNNNNACTCTTTGGCAGCGACCCCTTGTYACAATAAAAGTAGGGGGCCARATAAAgGagGCTCTATTAGATACAGGAGCAGATGATACAGTATTAGAAGAAATAGAATTRCCAGGAAAATGGAAACCAAAAATGATAGGRGGAATTGGAGGTTTTATCAAAGTAAGACAGTATGAGCAAATTCTTATAGAAATCTGTGGAAAAAAGGCTATAGGTACAGTRTTAGTAGGMCCCACACCTrTCAACATAATTGGAAGAAATATGTTGACTCAGCTTGGATGCACRCTAAATTTTNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNTGTGAwGAAATGGAGAAGGAAGGAAAAATTTCAAAAATTGGRCCTGAAAATCCATATAACACTCCAGTATTTGCCATAAAAAAGAAGGACAGTACTAAGTGGAGAAAATTAGTAGATTTCAGGGAACTTAATAAAAGAACYCAAGACTTTTGTGAAGTTCAATTAGGAATACCACACCCAGCAGGGTTARAAAAGAARAAATCAGYGACAGTGCTRGATGTGGGGGATGCATATTTTTCAATCCCTTTAGATGAAAGCTTCAGGAAATATACTGCATTCACCATACCYAGYMYAAACAATGCAGCACCAGGGATTAGATATCAATATAATGTGCTTCCACAGGGATGGAAAGGATCACCAGCAATATTCCAGTGTAGCATGACAAAAATCTTAGAGCCCTTTAGGACAAAAAACCCAGACATAGTTATCTATCAATACGTGGATGACTTGTATGTAGSATCTGAYYTAGAAATAGGGCAACATAGAGCAAAAATAGAGGAGTTAAGAGAACATTTATTGAAATGGGGACTTACCACACCAGACAAGAAACATCAGAAAGAACCCCCAYTTCTTTGGATGGGGTATGARCTTCATCCTGACAAATGGACAGTACAGCCTATACAGCTACCA";
+		System.err.println(test);
+		Set<String> processed = trugeneQC(test);
+		for (String s: processed) {
+			System.err.println(s);
 		}
-	}
-	
-	private static void print(NtSequence ntseq) {
-		System.err.println(ntseq.getLabel());
-		System.err.println(ntseq.getSequenceDate().toString());
-		System.err.println(ntseq.getNucleotides());
-		System.err.println("----");
 	}
 }
