bee0cf3a26794d724d9e20d6cb9a2afd64bfaf2f
Stijn Imbrechts <simbre1@pol.(none)>
2010-04-12 18:34:03 +0200

    better command line arguments

--- regadb-analyses/src/net/sf/regadb/align/test/TestFastaAlign.java
+++ regadb-analyses/src/net/sf/regadb/align/test/TestFastaAlign.java
@@ -3,10 +3,11 @@ package net.sf.regadb.align.test;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.PrintStream;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
@@ -30,6 +31,9 @@ import net.sf.regadb.io.importXML.ImportGenomes;
 import net.sf.regadb.service.wts.BlastAnalysis;
 import net.sf.regadb.service.wts.RegaDBWtsServer;
 import net.sf.regadb.service.wts.ServiceException;
+import net.sf.regadb.util.args.Arguments;
+import net.sf.regadb.util.args.PositionalArgument;
+import net.sf.regadb.util.args.ValueArgument;
 import net.sf.regadb.util.settings.RegaDBSettings;
 
 import org.biojava.bio.BioException;
@@ -42,28 +46,35 @@ public class TestFastaAlign {
 		Nt,
 		Ui;
 	}
-	public static void main(String [] args) {
+	public static void main(String [] args) throws FileNotFoundException {
+		Arguments as = new Arguments();
+		ValueArgument outputType = as.addValueArgument("type", "aa|nt|ui", false);
+		ValueArgument genome = as.addValueArgument("genome", "HIV-1|HIV-2A|HIV-2B|HCV|HTLV-1", false);
+		PositionalArgument inputFile = as.addPositionalArgument("fasta", true);
+		PositionalArgument outputFile = as.addPositionalArgument("output-file", false);
+		
+		if(!as.handle(args))
+			return;
+		
 		long start = System.currentTimeMillis();
 
 		RegaDBSettings.createInstance();
 
-		final String output = "--output=";
 		OutputType ot = OutputType.Nt;
-		for (String a : args) {
-			if (a.startsWith(output)) {
-				String outputFormat = a.substring(a.indexOf(output) + output.length());
-				if (outputFormat.equals("aa"))
-					ot = OutputType.Aa;
-				else if (outputFormat.equals("nt"))
-					ot = OutputType.Nt;
-				else if (outputFormat.equals("ui"))
-					ot = OutputType.Ui;
-				else {
-					System.err.println("Wrong output format! Exiting!");
-					System.exit(0);
-				}
+		if(outputType.isSet()){
+			if (outputType.getValue().equals("aa"))
+				ot = OutputType.Aa;
+			else if (outputType.getValue().equals("nt"))
+				ot = OutputType.Nt;
+			else if (outputType.getValue().equals("ui"))
+				ot = OutputType.Ui;
+			else {
+				System.err.println("Wrong output format! Exiting!");
+				return;
 			}
 		}
+		
+		PrintStream out = outputFile.isSet() ? new PrintStream(new FileOutputStream(new File(outputFile.getValue()))) : System.out;
 		
         RichSequenceIterator xna = null;
         FileReader uploadedStream = null;
@@ -73,7 +84,7 @@ public class TestFastaAlign {
         
         try 
         {
-            uploadedStream = new FileReader(args[0]);
+            uploadedStream = new FileReader(inputFile.getValue());
             br = new BufferedReader(uploadedStream);
             xna = org.biojavax.bio.seq.RichSequence.IOTools.readFastaDNA(br, null);
         }
@@ -85,14 +96,10 @@ public class TestFastaAlign {
         {
         	ex.printStackTrace();
         }
-        catch(IOException ioe)
-        {
-        	ioe.printStackTrace();
-        }
         
         Genome g = null;
-        if (args.length > 1) 
-        	g = getGenome(args[1]);
+        if (genome.isSet()) 
+        	g = getGenome(genome.getValue());
         
         if(xna!=null)
         { 
@@ -107,7 +114,8 @@ public class TestFastaAlign {
                     	g = getGenome(ntseq);
                     }
                     
-                    System.out.println("sequence=" + seq.getName());
+                    out.println("sequence=" + seq.getName());
+                    System.err.println(">"+ seq.getName());
                     List<AaSequence> tmpResult = aligner.align(ntseq, g);
 
                     //remove the AaSequences where startpos == endpos
@@ -126,15 +134,15 @@ public class TestFastaAlign {
                     
                 	for (AaSequence aaseq : result) {
                 		if (ot == OutputType.Ui) {
-                			System.out.println(new UIVisualizeAaSequence().getAlignmentView(aaseq));
-                			System.out.println(MutationHelper.getSynonymousMutations(aaseq));
-                			System.out.println(MutationHelper.getNonSynonymousMutations(aaseq));
+                			out.println(new UIVisualizeAaSequence().getAlignmentView(aaseq));
+                			out.println(MutationHelper.getSynonymousMutations(aaseq));
+                			out.println(MutationHelper.getNonSynonymousMutations(aaseq));
                 		} else {
 	                    	Map<Short, String> aaMutations = new HashMap<Short, String>();
-	                    	System.out.print("protein=" + aaseq.getProtein().getAbbreviation() + ",");
-	                    	System.out.print("start=" + aaseq.getFirstAaPos() + ",");
-	                    	System.out.print("end=" + aaseq.getLastAaPos() + ",");
-	                    	System.out.print("mutations=");
+	                    	out.print("protein=" + aaseq.getProtein().getAbbreviation() + ",");
+	                    	out.print("start=" + aaseq.getFirstAaPos() + ",");
+	                    	out.print("end=" + aaseq.getLastAaPos() + ",");
+	                    	out.print("mutations=");
 	                    	for(AaMutation aamut : aaseq.getAaMutations()) {
 	                    		String mut;
 	                    		if (ot == OutputType.Nt)
@@ -173,26 +181,26 @@ public class TestFastaAlign {
 	                    		}
 	                    	}
 	                    	if (toPrint.length() > 0) {
-		                    	System.out.print(toPrint.substring(0,toPrint.length() - 1));	                    	
+		                    	out.print(toPrint.substring(0,toPrint.length() - 1));	                    	
 	                    	}
-	                    	System.out.print("\n");
+	                    	out.print("\n");
                 		}
                     }
                 }
                 catch (NoSuchElementException e) 
                 {
                 	e.printStackTrace();
-                    System.out.println("ERROR");
+                    out.println("ERROR");
                 } 
                 catch (BioException e) 
                 {
                 	e.printStackTrace();
-                	System.out.println("ERROR");
+                	out.println("ERROR");
                 }
                 catch (Exception e) 
                 {
                 	e.printStackTrace();
-                	System.out.println("ERROR");
+                	out.println("ERROR");
                 }
             }
         }
