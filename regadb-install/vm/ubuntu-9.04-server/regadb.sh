65649943054cde53509dd1171b5fd4ff0492d21c
Stijn Imbrechts <simbre1@pol.(none)>
2010-04-22 15:58:02 +0200

    vm use custom global-conf.xml

--- regadb-install/vm/ubuntu-9.04-server/regadb.sh
+++ regadb-install/vm/ubuntu-9.04-server/regadb.sh
@@ -1,11 +1,4 @@
 #! /bin/sh
-cd `echo $0 | sed 's/\/[^\/]*$/\//g'`
-
-cd ../../../
-
-apt-get -y install ant
-ant clean
-ant 
 
 TOMCAT="/var/lib/tomcat6"
 
@@ -14,15 +7,20 @@ LOGDIR="$REGADBDIR/log"
 QUERYDIR="$REGADBDIR/query"
 IMPORTDIR="$REGADBDIR/import"
 
+cd `echo $0 | sed 's/\/[^\/]*$/\//g'`
+
 mkdir -p "$REGADBDIR"
 mkdir "$LOGDIR"
 mkdir "$QUERYDIR"
 mkdir "$IMPORTDIR"
-
-cp regadb-doc/xml/global-conf.xml "$REGADBDIR"
-#sed -i -e 's///' "$REGADBDIR/global-conf.xml"
-
+cp global-conf.xml "$REGADBDIR"
 chown -R tomcat6:tomcat6 "$REGADBDIR"
+
+cd ../../../
+
+apt-get -y install ant
+ant clean
+ant 
 
 cp regadb-ui/dist/regadb-ui.war "$TOMCAT/webapps/regadb.war"
 
