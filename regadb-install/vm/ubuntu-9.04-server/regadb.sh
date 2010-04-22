979459e26405cf3f237fb5fd82bf0b59403490fd
Stijn Imbrechts <simbre1@pol.(none)>
2010-04-22 14:55:45 +0200

    vm set global-conf.xml file and create dirs

--- regadb-install/vm/ubuntu-9.04-server/regadb.sh
+++ regadb-install/vm/ubuntu-9.04-server/regadb.sh
@@ -9,5 +9,20 @@ ant build
 
 TOMCAT="/var/lib/tomcat6"
 
+REGADBDIR="/etc/rega_institute/regadb"
+LOGDIR="$REGADBDIR/log"
+QUERYDIR="$REGADBDIR/query"
+IMPORTDIR="$REGADBDIR/import"
+
+mkdir -p "$REGADBDIR"
+mkdir "$LOGDIR"
+mkdir "$QUERYDIR"
+mkdir "$IMPORTDIR"
+
+cp regadb-doc/xml/global-conf.xml "$REGADBDIR"
+#sed -i -e 's///' "$REGADBDIR/global-conf.xml"
+
+chown -R tomcat6:tomcat6 "$REGADBDIR"
+
 cp regadb-ui/dist/regadb-ui.war "$TOMCAT/webapps/regadb.war"
 
