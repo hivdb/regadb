3bf4098ba081ca6a607aad6c89adfd7e99f8e1e4
pieter <pieter@ubuntuvm.router.home>
2010-04-22 14:42:42 +0200

    /var -> /etc

--- regadb-install/vm/ubuntu-9.04-server/tomcat.sh
+++ regadb-install/vm/ubuntu-9.04-server/tomcat.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 apt-get -y install tomcat6
-sed -i 's/#TOMCAT_SECURITY=yes/TOMCAT_SECURITY=no/' /var/default/tomcat6
+
+sed -i 's/#TOMCAT_SECURITY=yes/TOMCAT_SECURITY=no/' /etc/default/tomcat6
