1630bf3e3fcb1ec7a84721f63911682a2f2f895c
Stijn Imbrechts <stijn.imbrechts+rega@gmail.com>
2010-07-08 14:58:27 +0200

    fix alignment visualization for insertions

--- regadb-analyses/src/net/sf/regadb/align/view/VisualizeAaSequence.java
+++ regadb-analyses/src/net/sf/regadb/align/view/VisualizeAaSequence.java
@@ -1,14 +1,10 @@
 package net.sf.regadb.align.view;
 
-import java.util.Iterator;
-import java.util.TreeSet;
-
 import net.sf.regadb.analysis.functions.AaSequenceHelper;
 import net.sf.regadb.db.AaInsertion;
 import net.sf.regadb.db.AaMutation;
 import net.sf.regadb.db.AaSequence;
 import net.sf.regadb.db.Protein;
-import net.sf.regadb.db.SplicingPosition;
 
 public abstract class VisualizeAaSequence {
     public String getAlignmentView(AaSequence aaseq) {
@@ -25,25 +21,12 @@ public abstract class VisualizeAaSequence {
         String mutationCodon;
         for(int i = 0; i<proteinNt.length()/3; i++) {
         	codonIndex = i+1;
-        	if(codonIndex==77) {
-        		System.err.println("wait");
-        	}
-        	if(mutations.length != mutationIndex && mutations[mutationIndex].getId().getMutationPosition()==codonIndex) {
+    		if(mutations.length != mutationIndex && mutations[mutationIndex].getId().getMutationPosition()==codonIndex){
                 mutationCodon = mutations[mutationIndex].getNtMutationCodon();
                 addNt(proteinNt.charAt(i*3), mutationCodon.charAt(0), codonIndex);
                 addNt(proteinNt.charAt(i*3+1), mutationCodon.charAt(1), codonIndex);
                 addNt(proteinNt.charAt(i*3+2), mutationCodon.charAt(2), codonIndex);
                 mutationIndex++;
-            } else if(insertions.length != insertionIndex && insertions[insertionIndex].getId().getInsertionPosition()==codonIndex) {
-                short pos = insertions[insertionIndex].getId().getInsertionPosition();
-                while(insertionIndex!=insertions.length && insertions[insertionIndex].getId().getInsertionPosition()==pos) {
-                    mutationCodon = insertions[insertionIndex].getNtInsertionCodon();
-                    addNt('-', mutationCodon.charAt(0), codonIndex);
-                    addNt('-', mutationCodon.charAt(1), codonIndex);
-                    addNt('-', mutationCodon.charAt(2), codonIndex);
-                    
-                    insertionIndex++;
-                }
             } else {
             	if(codonIndex<aaseq.getFirstAaPos() || codonIndex>aaseq.getLastAaPos()) {
                     addNt(proteinNt.charAt(i*3), '-', codonIndex);
@@ -55,6 +38,17 @@ public abstract class VisualizeAaSequence {
                     addNt(proteinNt.charAt(i*3+2), proteinNt.charAt(i*3+2), codonIndex);
             	}
             }
+    		if(insertions.length != insertionIndex && insertions[insertionIndex].getId().getInsertionPosition()==codonIndex) {
+                short pos = insertions[insertionIndex].getId().getInsertionPosition();
+                while(insertionIndex!=insertions.length && insertions[insertionIndex].getId().getInsertionPosition()==pos) {
+                    mutationCodon = insertions[insertionIndex].getNtInsertionCodon();
+                    addNt('-', mutationCodon.charAt(0), codonIndex);
+                    addNt('-', mutationCodon.charAt(1), codonIndex);
+                    addNt('-', mutationCodon.charAt(2), codonIndex);
+                    
+                    insertionIndex++;
+                }
+    		}
         }
         
         end();
