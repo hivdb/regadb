3f361620df264b2df1d14b03cd72ebda7c84b770
pieter <pieter@smoog.(none)>
2010-11-16 15:17:12 +0100

    create SequenceUtils containing method to determine the distance between 2 alignments

---
+++ regadb-analyses/src/net/sf/regadb/sequencedb/SequenceUtils.java
@@ -0,0 +1,25 @@
+package net.sf.regadb.sequencedb;
+
+public class SequenceUtils {
+	public static class SequenceDistance {
+		public int numberOfDifferences;
+		public int denominator;
+	}
+	
+	public static SequenceDistance distance(String alignment1, String alignment2) {
+		if (alignment1.length() != alignment2.length())
+			throw new RuntimeException("Alignments differ in size");
+		
+		SequenceDistance f = new SequenceDistance();
+		
+		for (int i = 0; i < alignment1.length(); i++) {
+			if (alignment1.charAt(i) != '-' && alignment2.charAt(i) != '-') {
+				f.denominator++;
+				if (alignment1.charAt(i) != alignment2.charAt(i))
+					f.numberOfDifferences++;
+			}
+		}
+		
+		return f;
+	}
+}
