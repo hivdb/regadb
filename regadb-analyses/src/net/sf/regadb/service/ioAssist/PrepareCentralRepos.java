d6e30e6f96186982f1d9ac5039783fda97343c90
simbre1 <simbre1@pol.(none)>
2010-12-07 11:39:33 +0100

    add contamination tests to central repos

--- regadb-analyses/src/net/sf/regadb/service/ioAssist/PrepareCentralRepos.java
+++ regadb-analyses/src/net/sf/regadb/service/ioAssist/PrepareCentralRepos.java
@@ -115,6 +115,9 @@ public class PrepareCentralRepos
         export.writeTopTest(StandardObjects.getGenericHAVIgGTest(), tests);
         export.writeTopTest(StandardObjects.getGenericHAVIgMTest(), tests);
         
+        export.writeTopTest(StandardObjects.getContaminationTest(), tests);
+        export.writeTopTest(StandardObjects.getContaminationClusterFactorTest(), tests);
+        
         //Resistance tests
         Test resTest;
         resTest = createResistanceTest("ANRS-HIV1-2009.07.xml", "ANRS 2009.07", StandardObjects.getHiv1Genome());
