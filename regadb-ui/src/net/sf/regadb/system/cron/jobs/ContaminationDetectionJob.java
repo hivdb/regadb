b7be59583a5b80b9814ceb834233d558a9317d90
pieter <pieter@smoog.(none)>
2010-12-07 17:39:20 +0100

    add contamination detection job

---
+++ regadb-ui/src/net/sf/regadb/system/cron/jobs/ContaminationDetectionJob.java
@@ -0,0 +1,64 @@
+package net.sf.regadb.system.cron.jobs;
+
+import java.util.List;
+
+import net.sf.regadb.contamination.ContaminationDetection;
+import net.sf.regadb.db.NtSequence;
+import net.sf.regadb.db.Test;
+import net.sf.regadb.db.TestResult;
+import net.sf.regadb.db.Transaction;
+import net.sf.regadb.db.session.HibernateUtil;
+import net.sf.regadb.io.util.StandardObjects;
+import net.sf.regadb.sequencedb.SequenceDb;
+import net.sf.regadb.util.settings.RegaDBSettings;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+
+public class ContaminationDetectionJob implements Job {
+	@Override
+	public void execute(JobExecutionContext arg0) throws JobExecutionException {
+		RegaDBSettings.createInstance();
+		
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		Transaction t  = new Transaction(null, session);
+		Test ccfTest = t.getTest(StandardObjects.getContaminationClusterFactorTest().getDescription());
+		
+		Query q = t.createQuery("from NtSequence ntseq where ntseq.id not in " +
+			"(select tr.ntSequence.id from TestResult tr where tr.test = :test)");
+		q.setParameter("test", ccfTest);
+		
+		List<NtSequence> sequences = q.list();
+		
+		SequenceDb seqDb = SequenceDb.getInstance(RegaDBSettings.getInstance().getSequenceDatabaseConfig().getPath());
+		for (int i = 0; i < sequences.size(); i++) {
+			NtSequence ntSeq = sequences.get(i);
+			double cf = ContaminationDetection.clusterFactor(ntSeq, seqDb);
+			System.err.println("cf:" + cf);
+			/*TestResult tr = new TestResult();
+			tr.setNtSequence(ntSeq);
+			ntSeq.getTestResults().add(tr);
+			tr.setTest(ccfTest);
+			tr.setValue(cf+"");
+			
+			if (i % 50 == 0) {
+				t.commit();   
+	            t.clearCache();
+			} */
+		}
+		t.commit();
+	}
+	
+	public static void main(String [] args) {
+		ContaminationDetectionJob j = new ContaminationDetectionJob();
+		try {
+			j.execute(null);
+		} catch (JobExecutionException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
