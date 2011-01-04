a7a16072e87831c5a8cf6e7aa6a93f64ce9c1ae8
pieter <pieter@smoog.(none)>
2011-01-04 12:22:42 +0100

    add range support print type improve type parsing

--- regadb-analyses/src/net/sf/regadb/contamination/ApproximateDistributions.java
+++ regadb-analyses/src/net/sf/regadb/contamination/ApproximateDistributions.java
@@ -3,16 +3,12 @@ package net.sf.regadb.contamination;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.HashSet;
 import java.util.Map;
-import java.util.Set;
 
 import net.sf.regadb.contamination.SequenceDistancesQuery.OutputType;
+import net.sf.regadb.contamination.SequenceDistancesQuery.Range;
 import net.sf.regadb.db.NtSequence;
-import net.sf.regadb.db.Patient;
-import net.sf.regadb.db.Privileges;
 import net.sf.regadb.db.Transaction;
-import net.sf.regadb.db.ViralIsolate;
 import net.sf.regadb.db.login.DisabledUserException;
 import net.sf.regadb.db.login.WrongPasswordException;
 import net.sf.regadb.db.login.WrongUidException;
@@ -27,6 +23,9 @@ import org.hibernate.ScrollableResults;
 
 public class ApproximateDistributions {
 	public static void main(String [] args) throws WrongUidException, WrongPasswordException, DisabledUserException, IOException {
+		if (args.length != 6)
+			System.err.println("user password outputfile.csv I/O orf region");
+		
 		RegaDBSettings.createInstance();
 		
 		SequenceDb db = SequenceDb.getInstance(RegaDBSettings.getInstance().getSequenceDatabaseConfig().getPath());
@@ -39,18 +38,26 @@ public class ApproximateDistributions {
 		
 		FileWriter fw = new FileWriter(new File(args[2]));
 		
+		String type = args[3].trim();
 		OutputType outputType = null;
-		if (args[3].trim().equals("I"))
+		if (type.equals("I"))
 			outputType = OutputType.IntraPatient;
-		else if (args[3].trim().equals("O"))
+		else if (type.equals("O"))
 			outputType = OutputType.ExtraPatient;
+		
+		Range range = new Range();
+		range.orf = args[4];
+		
+		String region[] = args[5].split("-");
+		range.start = Integer.parseInt(region[0]);
+		range.end = Integer.parseInt(region[1]);
 		
 		int i = 0;
 		long start = System.currentTimeMillis();
 		while (r.next()) {
 			NtSequence seq = (NtSequence)r.get(0);
 			
-			SequenceDistancesQuery distances = new SequenceDistancesQuery(seq, outputType);
+			SequenceDistancesQuery distances = new SequenceDistancesQuery(seq, outputType, range);
 			db.query(seq.getViralIsolate().getGenome(), distances);
 			
 			for (Map.Entry<Integer, SequenceDistance> e : distances.getSequenceDistances().entrySet()) {
@@ -62,7 +69,7 @@ public class ApproximateDistributions {
 				
 				double diff = ((double)f.numberOfDifferences/f.numberOfPositions);
 				if (f.numberOfPositions != 0){
-					fw.write(diff + "\n");
+					fw.write(type + "," + diff + "\n");
 					fw.flush();
 				}
 			}
