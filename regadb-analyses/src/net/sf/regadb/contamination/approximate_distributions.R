a7230fce9454e7707f49d6f7fa23851ce3bb3cee
pieter <pieter@smoog.(none)>
2011-01-05 21:28:30 +0100

    estimate the log normal distribution parameters for a genomic region

---
+++ regadb-analyses/src/net/sf/regadb/contamination/approximate_distributions.R
@@ -0,0 +1,21 @@
+# inter.csv is the inter-patient file 
+# type;diversity
+inter <- read.csv("inter.csv", sep=";")
+inter <- subset(inter, inter$type == "O")
+rows <- min(100000, length(inter$type))
+inter <- inter[1:rows,]
+
+# intra.csv: intra-patient file
+# type;diversity
+intra <- read.csv("intra.csv");
+intra <- subset(intra, intra$type == "O")
+
+expintra <- exp(intra$diversity)
+expinter <- exp(inter$diversity)
+
+intrad <- intra$diversity
+interd <- inter$diversity
+
+fitdistr(intrad[intrad > 0], "log-normal")
+
+fitdistr(interd[interd > 0], "log-normal")
