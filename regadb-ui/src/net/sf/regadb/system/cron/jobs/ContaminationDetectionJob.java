793350b8aeb518efb3e21558443a714bc0165930
pieter <pieter@smoog.(none)>
2011-01-12 15:26:12 +0100

    add support for sending a mail when a contamination was detected

--- regadb-ui/src/net/sf/regadb/system/cron/jobs/ContaminationDetectionJob.java
+++ regadb-ui/src/net/sf/regadb/system/cron/jobs/ContaminationDetectionJob.java
@@ -1,16 +1,23 @@
 package net.sf.regadb.system.cron.jobs;
 
-import java.util.ArrayList;
 import java.util.List;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.AddressException;
 
 import net.sf.regadb.contamination.ContaminationDetection;
 import net.sf.regadb.db.NtSequence;
+import net.sf.regadb.db.Patient;
+import net.sf.regadb.db.Privileges;
 import net.sf.regadb.db.Test;
 import net.sf.regadb.db.TestResult;
 import net.sf.regadb.db.Transaction;
 import net.sf.regadb.db.session.HibernateUtil;
 import net.sf.regadb.io.util.StandardObjects;
 import net.sf.regadb.sequencedb.SequenceDb;
+import net.sf.regadb.util.mail.MailUtils;
+import net.sf.regadb.util.settings.ContaminationConfig;
+import net.sf.regadb.util.settings.EmailConfig;
 import net.sf.regadb.util.settings.RegaDBSettings;
 
 import org.hibernate.Query;
@@ -18,6 +25,8 @@ import org.hibernate.Session;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
+
+import eu.webtoolkit.jwt.WString;
 
 public class ContaminationDetectionJob implements Job {
 	@Override
@@ -44,6 +53,28 @@ public class ContaminationDetectionJob implements Job {
 			Double cf = ContaminationDetection.clusterFactor(ntSeq, seqDb);
 			if (cf == null)
 				continue;
+			
+			ContaminationConfig cc = RegaDBSettings.getInstance().getContaminationConfig();
+			if (cc.isSendMail() && cf > cc.getThreshold()) {
+				EmailConfig ec = RegaDBSettings.getInstance().getInstituteConfig().getEmailConfig();
+                if (ec != null) {
+	                try {
+	                	String patientId 
+	                		= new Patient(ntSeq.getViralIsolate().getPatient(), Privileges.READONLY.getValue()).getPatientId();
+						MailUtils.sendMail(ec.getHost(), ec.getFrom(), ec.getTo(), 
+									WString.tr("jobs.contamination.subject"), 
+									WString.tr("jobs.contamination.message")
+										.arg(patientId)
+										.arg(ntSeq.getViralIsolate().getSampleId())
+										.arg(ntSeq.getLabel())
+										.arg(cf));
+					} catch (AddressException e) {
+						e.printStackTrace();
+					} catch (MessagingException e) {
+						e.printStackTrace();
+					}
+                }
+			}
 			
 			System.err.println("cf:" + cf);
 			TestResult tr = new TestResult();
