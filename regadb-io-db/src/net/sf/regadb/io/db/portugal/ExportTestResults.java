c73ac357456cd3f3702ce7700b4c31af7fe930c4
simbre1 <simbre1@pol.(none)>
2011-05-03 19:44:34 +0200

    ExportTestResults: export to a file

--- regadb-io-db/src/net/sf/regadb/io/db/portugal/ExportTestResults.java
+++ regadb-io-db/src/net/sf/regadb/io/db/portugal/ExportTestResults.java
@@ -1,12 +1,15 @@
 package net.sf.regadb.io.db.portugal;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
 import java.util.List;
 
 import net.sf.regadb.db.DrugGeneric;
 import net.sf.regadb.db.NtSequence;
 import net.sf.regadb.db.Test;
 import net.sf.regadb.db.TestResult;
-import net.sf.regadb.db.TestType;
 import net.sf.regadb.db.Transaction;
 import net.sf.regadb.db.ViralIsolate;
 import net.sf.regadb.db.login.DisabledUserException;
@@ -22,7 +25,7 @@ import org.hibernate.Query;
 
 public class ExportTestResults {
 
-	public static void main(String[] args) throws WrongUidException, WrongPasswordException, DisabledUserException{
+	public static void main(String[] args) throws WrongUidException, WrongPasswordException, DisabledUserException, FileNotFoundException{
 		Arguments as = new Arguments();
 		PositionalArgument user = as.addPositionalArgument("user", true);
 		PositionalArgument pass = as.addPositionalArgument("pass", true);
@@ -30,6 +33,8 @@ public class ExportTestResults {
 		PositionalArgument aTest = as.addPositionalArgument("test", true);
 		PositionalArgument aTestType = as.addPositionalArgument("test-type", true);
 		PositionalArgument aGenome = as.addPositionalArgument("genome", false);
+		
+		ValueArgument outFile = as.addValueArgument("o", "output-file", false);
 		
 		ValueArgument confDir = as.addValueArgument("c", "conf-dir", false);
 		
@@ -51,8 +56,15 @@ public class ExportTestResults {
 			test = t.getTest(aTest.getValue(), aTestType.getValue(), aGenome.getValue());
 		
 		if(test != null){
+			PrintStream out = System.out;
+			if(outFile.isSet())
+				out = new PrintStream(new FileOutputStream(new File(outFile.getValue())));
+			
 			ExportTestResults exp = new ExportTestResults();
-			exp.export(t, test, aTest.getValue(), aTestType.getValue(), aGenome.getValue());
+			exp.export(out, t, test, aTest.getValue(), aTestType.getValue(), aGenome.getValue());
+			
+			if(outFile.isSet())
+				out.close();
 		} else
 			System.err.println("test not found");
 		
@@ -61,7 +73,9 @@ public class ExportTestResults {
 	
 	
 	@SuppressWarnings("unchecked")
-	public void export(Transaction t, Test test, String testDescr, String testTypeDescr, String genome){
+	public void export(PrintStream out, Transaction t, Test test, String testDescr, String testTypeDescr, String genome){
+		out.println("set search_path to regadbschema;");
+		
 		Query q = t.createQuery("select p.patientId, tr from TestResult tr left outer join tr.patient p where tr.test = :test");
 		q.setParameter("test", test);
 		
@@ -74,6 +88,7 @@ public class ExportTestResults {
 				v = n.getViralIsolate();
 			
 			export(
+					out,
 					(String)o[0],
 					testDescr,
 					testTypeDescr,
@@ -87,51 +102,46 @@ public class ExportTestResults {
 		}
 	}
 	
-	public void export(Transaction t, TestType testType){
-		Query q = t.createQuery("select tr from TestResult tr where tr.test.testType = :testType order by tr.test.testIi");
-		q.setParameter("testType", testType);
-	}
-	
-	public void export(String patientId, String test, String testType, String genome, String genericId, String sampleId, String label, String value, byte[] data){
-		System.out.print("insert into test_result ");
-		System.out.print("(version, test_ii, generic_ii, viral_isolate_ii, patient_ii, nt_sequence_ii, value, data, test_date)");
-		System.out.print(" values (0, ");
+	public void export(PrintStream out, String patientId, String test, String testType, String genome, String genericId, String sampleId, String label, String value, byte[] data){
+		out.print("insert into test_result ");
+		out.print("(version, test_ii, generic_ii, viral_isolate_ii, patient_ii, nt_sequence_ii, value, data, test_date)");
+		out.print(" values (0, ");
 
 		if(genome == null)
-			System.out.println("(select test_ii from test t join test_type tt using(test_type_ii) where t.description='"+ test +"' and tt.description='"+ testType +"' limit 1),");
+			out.println("(select test_ii from test t join test_type tt using(test_type_ii) where t.description='"+ test +"' and tt.description='"+ testType +"' limit 1),");
 		else
-			System.out.println("(select test_ii from test t join test_type tt using(test_type_ii) join genome g using(genome_ii) where t.description='"+ test +"' and tt.description='"+ testType +"' and g.organism_name='"+ genome +"' limit 1),");
+			out.println("(select test_ii from test t join test_type tt using(test_type_ii) join genome g using(genome_ii) where t.description='"+ test +"' and tt.description='"+ testType +"' and g.organism_name='"+ genome +"' limit 1),");
 		
 		if(genericId == null)
-			System.out.print("null,");
+			out.print("null,");
 		else
-			System.out.println("(select generic_ii from drug_generic where generic_id = '"+ genericId +"' limit 1),");
+			out.println("(select generic_ii from drug_generic where generic_id = '"+ genericId +"' limit 1),");
 
 		if(sampleId == null)
-			System.out.println("null,");
+			out.println("null,");
 		else
-			System.out.println("(select viral_isolate_ii from viral_isolate where sample_id='"+ sampleId +"' limit 1),");
+			out.println("(select viral_isolate_ii from viral_isolate where sample_id='"+ sampleId +"' limit 1),");
 			
 		if(patientId == null)
-			System.out.print("null,");
+			out.print("null,");
 		else
-			System.out.println("(select patient_ii from patient where patient_id = '"+ patientId +"' limit 1),");
+			out.println("(select patient_ii from patient where patient_id = '"+ patientId +"' limit 1),");
 		
 		if(label == null)
-			System.out.print("null,");
+			out.print("null,");
 		else
-			System.out.println("(select nt_sequence_ii from viral_isolate join nt_sequence using(viral_isolate_ii) where sample_id = '"+ sampleId +"' and label = '"+ label +"' limit 1),");
+			out.println("(select nt_sequence_ii from viral_isolate join nt_sequence using(viral_isolate_ii) where sample_id = '"+ sampleId +"' and label = '"+ label +"' limit 1),");
 		
 		if(value == null)
-			System.out.print("null,");
+			out.print("null,");
 		else
-			System.out.print("'"+ value +"',");
+			out.print("'"+ value +"',");
 		
 		if(data == null)
-			System.out.print("null,");
+			out.print("null,");
 		else
-			System.out.print("'"+ new String(data) +"',");
+			out.print("'"+ new String(data) +"',");
 		
-		System.out.println("'2011-05-03');");
+		out.println("'2011-05-03');");
 	}
 }
