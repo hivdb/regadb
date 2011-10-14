2ee26ad5c4d6ccc040145c6e957af51d7507037d
simbre1 <simbre1@pol.(none)>
2011-10-14 14:37:23 +0200

    FastaFile implemented using list and map also made clearNucleotides optional

--- regadb-analyses/src/net/sf/regadb/tools/FastaFile.java
+++ regadb-analyses/src/net/sf/regadb/tools/FastaFile.java
@@ -7,57 +7,81 @@ import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 
 import net.sf.regadb.db.NtSequence;
 
-@SuppressWarnings("serial")
-public class FastaFile extends TreeMap<String, NtSequence>{
-	private static String possibleNucleotides  = "ACGTMRWSYKVHDBN";
+public class FastaFile{
+	private static String possibleNucleotides  = "ACGTUMRWSYKVHDBN";
+	
+	private File file = null;
+	
+	private Map<String, NtSequence> map = new TreeMap<String, NtSequence>();
+	private List<NtSequence> list = new ArrayList<NtSequence>();
 	
 	public FastaFile(File input) throws IOException{
-		BufferedReader br = new BufferedReader(new FileReader(input));
-		
-		String line;
-		String label = null;
-		StringBuilder nucl = null;
-		
-		while((line = br.readLine()) != null){
-			if(line.startsWith(">")){
-				if(label != null)
-					add(label,nucl.toString());
-				
-				label = line.substring(1).trim();
-				nucl = new StringBuilder();
-			}
-			else{
-				if(label != null){
-					nucl.append(line.toLowerCase().trim());
-				}
-			}
-		}
-		if(label != null)
-			add(label,nucl.toString());
-		
-		br.close();
+		this(input, false);
 	}
 	
-	public void add(String label, String nucleotides){
+	public FastaFile(File input, boolean clearNucleotides) throws IOException{
+		this.file = input;
+		
+		BufferedReader br = null;
+		
+		try{
+			br = new BufferedReader(new FileReader(input));
+			
+			String line;
+			String label = null;
+			StringBuilder nucl = null;
+			
+			while((line = br.readLine()) != null){
+				if(line.startsWith(">")){
+					if(label != null)
+						add(label, nucl.toString(), clearNucleotides);
+					
+					label = line.substring(1).trim();
+					nucl = new StringBuilder();
+				}
+				else{
+					if(label != null){
+						nucl.append(line.toLowerCase().trim());
+					}
+				}
+			}
+			if(label != null){
+				add(label, nucl.toString(), clearNucleotides);
+			}
+		}finally{
+			if(br != null)
+				br.close();
+		}
+	}
+	
+	public File getFile(){
+		return file;
+	}
+	
+	public void add(String label, String nucleotides, boolean clearNucleotides){
 		NtSequence nt = new NtSequence();
 		nt.setLabel(label);
-		nt.setNucleotides(clearNucleotides(nucleotides));
-		put(label, nt);
+		nt.setNucleotides(
+				clearNucleotides ? clearNucleotides(nucleotides) : nucleotides);
+		add(nt);
 	}
 	
 	public void add(NtSequence nt){
-		put(nt.getLabel(), nt);
+		map.put(nt.getLabel(), nt);
+		list.add(nt);
 	}
 	
 	public void print(PrintStream out){
-		for(Map.Entry<String, NtSequence> me : entrySet()){
-			out.println('>'+ me.getValue().getLabel());
-			out.println(me.getValue().getNucleotides());
+		for(NtSequence nt : list){
+			out.println('>'+ nt.getLabel());
+			out.println(nt.getNucleotides());
 		}
 	}
 	
@@ -65,6 +89,18 @@ public class FastaFile extends TreeMap<String, NtSequence>{
 		PrintStream out = new PrintStream(new FileOutputStream(output));
 		print(out);
 		out.close();
+	}
+	
+	public NtSequence get(int index){
+		return list.get(index);
+	}
+	
+	public NtSequence get(String label){
+		return map.get(label);
+	}
+	
+	public int size(){
+		return list.size();
 	}
 	
 	public static String clearNucleotides(String nucleotides){
