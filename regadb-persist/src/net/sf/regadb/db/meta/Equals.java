b9f652fe184d2c1f870b9afb08545a3e559c294c
simbre1 <simbre1@pol.(none)>
2012-08-06 18:53:10 +0200

    added Equals.isSameTherapyMotivation and javadoc comments

--- regadb-persist/src/net/sf/regadb/db/meta/Equals.java
+++ regadb-persist/src/net/sf/regadb/db/meta/Equals.java
@@ -40,37 +40,79 @@ import net.sf.regadb.db.ViralIsolate;
 
 public class Equals {
 
+	/**
+	 * Compares Tests on description and testType
+	 * @param o1
+	 * @param o2
+	 * @return
+	 */
     public static boolean isSameTest(Test o1, Test o2) {
         return o1 == o2
         || (o1 != null && o2 != null && o1.getDescription().equals(o2.getDescription()) && isSameTestType(o1.getTestType(),o2.getTestType()));
     }
 
+    /**
+     * Compares TestNominalValues on value
+     * @param o1
+     * @param o2
+     * @return
+     */
     public static boolean isSameTestNominalValue(TestNominalValue o1, TestNominalValue o2) {
         return o1 == o2
         || (o1 != null && o2 != null && o1.getValue().equals(o2.getValue()));
     }
     
+    /**
+     * Compares EventNominalValues on value
+     * @param o1
+     * @param o2
+     * @return
+     */
     public static boolean isSameEventNominalValue(EventNominalValue o1, EventNominalValue o2) {
         return o1 == o2
         || (o1 != null && o2 != null && o1.getValue().equals(o2.getValue()));
     }
 
+    /**
+     * Compares TestTypes on description and genome
+     * @param o1
+     * @param o2
+     * @return
+     */
     public static boolean isSameTestType(TestType o1, TestType o2) {
         return o1 == o2
         || (o1 != null && o2 != null && o1.getDescription().equals(o2.getDescription())
                 && isSameGenome(o1.getGenome(), o2.getGenome()));
     }
     
+    /**
+     * Compares Genomes on organismName
+     * @param o1
+     * @param o2
+     * @return
+     */
     public static boolean isSameGenome(Genome o1, Genome o2){
         return o1 == o2
         || (o1 != null && o2 != null && o1.getOrganismName().equals(o2.getOrganismName()));
     }
 
+    /**
+     * Compares Datasets on description
+     * @param o1
+     * @param o2
+     * @return
+     */
     public static boolean isSameDataset(Dataset o1, Dataset o2) {
         return o1 == o2
         || (o1 != null && o2 != null && o1.getDescription().equals(o2.getDescription()));
     }
 
+    /**
+     * Compares TestResults on testDate, test, drugGeneric, viralIsolate and ntSequence, not on patient
+     * @param o1
+     * @param o2
+     * @return
+     */
     public static boolean isSameTestResult(TestResult o1, TestResult o2) {
         return o1 == o2
         || (o1 != null && o2 != null && isSameDate(o1.getTestDate(),o2.getTestDate())
@@ -85,11 +127,23 @@ public class Equals {
     		|| ( o1 != null && o2 != null && o1.equals(o2)); 
     }
 
+    /**
+     * Compares DrugGenerics on genericId
+     * @param o1
+     * @param o2
+     * @return
+     */
     public static boolean isSameDrugGeneric(DrugGeneric o1, DrugGeneric o2) {
         return o1 == o2
         || (o1 != null && o2 != null && o1.getGenericId().equals(o2.getGenericId()));
     }
 
+    /**
+     * Compares PatientAttributeValues on attribute, value and attributeNominalValue, not on patient 
+     * @param o1
+     * @param o2
+     * @return
+     */
     public static boolean isSamePatientAttributeValue(PatientAttributeValue o1, PatientAttributeValue o2) {
         if(o1 == o2) return true;
         
@@ -113,6 +167,12 @@ public class Equals {
                 && isSameString(v1,v2));
     }
     
+    /**
+     * Compares PatientEventValues on event, value, eventNominalValue, startDate and endDate, not on patient
+     * @param o1
+     * @param o2
+     * @return
+     */
     public static boolean isSamePatientEventValue(PatientEventValue o1, PatientEventValue o2) {
         Date end1 = o1.getEndDate();
         Date end2 = o2.getEndDate();
@@ -125,37 +185,90 @@ public class Equals {
                 && isSameEvent(o1.getEvent(), o2.getEvent()));
     }
 
+    /**
+     * Compares ViralIsolates on sampleId
+     * @param o1
+     * @param o2
+     * @return
+     */
     public static boolean isSameViralIsolate(ViralIsolate o1, ViralIsolate o2) {
         return o1 == o2
         || (o1 != null && o2 != null && o1.getSampleId().equals(o2.getSampleId()));
     }
 
+    /**
+     * Compares Therapies on startDate
+     * @param o1
+     * @param o2
+     * @return
+     */
     public static boolean isSameTherapy(Therapy o1, Therapy o2) {
         return o1 == o2
         || (o1 != null && o2 != null && o1.getStartDate().equals(o2.getStartDate()));
+    /**
+     * Compares TherapyMotivations on value
+     * @param o1
+     * @param o2
+     * @return
+     */
+    public static boolean isSameTherapyMotivation(TherapyMotivation o1, TherapyMotivation o2){
+    	return o1 == o2
+    		|| (o1 != null && o2 != null && isSameString(o1.getValue(), o2.getValue()));
+    }
+    
     }
 
+    /**
+     * Compares NtSequences on label
+     * @param o1
+     * @param o2
+     * @return
+     */
     public static boolean isSameNtSequence(NtSequence o1, NtSequence o2) {
         return o1 == o2
         || (o1 != null && o2 != null && o1.getLabel().equals(o2.getLabel()));
     }
 
+    /**
+     * Compares AaSequences on protein
+     * @param o1
+     * @param o2
+     * @return
+     */
     public static boolean isSameAaSequence(AaSequence o1, AaSequence o2) {
         return o1 == o2
         || (o1 != null && o2 != null && isSameProtein(o1.getProtein(), o2.getProtein()));
     }
 
+    /**
+     * Compares AaInsertions on insertionPosition and insertionOrder
+     * @param o1
+     * @param o2
+     * @return
+     */
     public static boolean isSameAaInsertion(AaInsertion o1, AaInsertion o2) {
         return o1 == o2
         || (o1 != null && o2 != null && o1.getId().getInsertionPosition() == o2.getId().getInsertionPosition()
                 && o1.getId().getInsertionOrder() == o2.getId().getInsertionOrder());
     }
 
+    /**
+     * Compares TherapyCommercials on drugCommercial
+     * @param o1
+     * @param o2
+     * @return
+     */
     public static boolean isSameTherapyCommercial(TherapyCommercial o1, TherapyCommercial o2) {
         return o1 == o2
         || (o1 != null && o2 != null && isSameDrugCommercial(o1.getId().getDrugCommercial(),o2.getId().getDrugCommercial()));
     }
     
+    /**
+     * Compares TherapyCommercials on drugCommercial, dayDosageUnits, frequency, blind and placebo
+     * @param o1
+     * @param o2
+     * @return
+     */
     public static boolean isSameTherapyCommercialEx(TherapyCommercial o1, TherapyCommercial o2) {
         return (o1 == null && o2 == null)
         || (isSameTherapyCommercial(o1, o2)
@@ -165,11 +278,23 @@ public class Equals {
       		  && o1.isPlacebo() == o2.isPlacebo());
     }
 
+    /**
+     * Compares TherapyGenerics on drugGeneric
+     * @param o1
+     * @param o2
+     * @return
+     */
     public static boolean isSameTherapyGeneric(TherapyGeneric o1, TherapyGeneric o2) {
         return o1 == o2
         || (o1 != null && o2 != null && isSameDrugGeneric(o1.getId().getDrugGeneric(),o2.getId().getDrugGeneric()));
     }
     
+    /**
+     * Compares TherapyGenerics on drugGeneric, dayDosageMg, frequency, blind and placebo
+     * @param o1
+     * @param o2
+     * @return
+     */
     public static boolean isSameTherapyGenericEx(TherapyGeneric o1, TherapyGeneric o2) {
         return (o1 == null && o2 == null)
           || (isSameTherapyGeneric(o1, o2)
@@ -184,36 +309,78 @@ public class Equals {
     		|| (o1 != null && o2 != null && o1.equals(o2));
     }
 
+    /**
+     * Compares AaMutations on mutationPosition
+     * @param o1
+     * @param o2
+     * @return
+     */
     public static boolean isSameAaMutation(AaMutation o1, AaMutation o2) {
         return o1 == o2
         || (o1 != null && o2 != null && o1.getId().getMutationPosition() == o2.getId().getMutationPosition());
     }
 
+    /**
+     * Compares Attributes on name
+     * @param o1
+     * @param o2
+     * @return
+     */
     public static boolean isSameAttribute(Attribute o1, Attribute o2) {
         return o1 == o2
         || (o1 != null && o2 != null && o1.getName().equals(o2.getName()));
     }
     
+    /**
+     * Compares Events on name
+     * @param o1
+     * @param o2
+     * @return
+     */
     public static boolean isSameEvent(Event o1, Event o2) {
         return o1 == o2
         || (o1 != null && o2 != null && o1.getName().equals(o2.getName()));
     }
 
+    /**
+     * Compares AttributeNominalValues on value, not on attribute
+     * @param o1
+     * @param o2
+     * @return
+     */
     public static boolean isSameAttributeNominalValue(AttributeNominalValue o1, AttributeNominalValue o2) {
         return o1 == o2
         || (o1 != null && o2 != null && o1.getValue().equals(o2.getValue()));
     }
 
+    /**
+     * Compares ValueTypes on description
+     * @param o1
+     * @param o2
+     * @return
+     */
     public static boolean isSameValueType(ValueType o1, ValueType o2) {
         return o1 == o2
         || (o1 != null && o2 != null && o1.getDescription().equals(o2.getDescription()));
     }
 
+    /**
+     * Compares TestObjects on description
+     * @param o1
+     * @param o2
+     * @return
+     */
     public static boolean isSameTestObject(TestObject o1, TestObject o2) {
         return o1 == o2
         || (o1 != null && o2 != null && o1.getDescription().equals(o2.getDescription()));
     }
     
+    /**
+     * Compares AttributeGroups on groupName
+     * @param o1
+     * @param o2
+     * @return
+     */
     public static boolean isSameAttributeGroup(AttributeGroup o1, AttributeGroup o2) {
         return o1 == o2
         || (o1 != null && o2 != null && o1.getGroupName().equals(o2.getGroupName()));
@@ -233,18 +400,36 @@ public class Equals {
             || (s1 != null && s1.equals(s2));
     }
 
+    /**
+     * Compares Proteins on abbreviation and openReadingFrame 
+     * @param protein
+     * @param protein2
+     * @return
+     */
 	public static boolean isSameProtein(Protein protein, Protein protein2) {
 		return protein == protein2 
 		|| (protein != null && protein2 != null && protein.getAbbreviation().equals(protein2.getAbbreviation())
 		        && isSameOpenReadingFrame(protein.getOpenReadingFrame(), protein2.getOpenReadingFrame()));
 	}
 	
+	/**
+	 * Compares OpenReadingFrames on name and genome
+	 * @param orf
+	 * @param orf2
+	 * @return
+	 */
 	public static boolean isSameOpenReadingFrame(OpenReadingFrame orf, OpenReadingFrame orf2) {
 	    return orf == orf2 
         || (orf != null && orf2 != null && orf.getName().equals(orf2.getName())
                 && isSameGenome(orf.getGenome(),orf2.getGenome()));
 	}
 
+	/**
+	 * Compares DrugCommercials on name
+	 * @param drugCommercial
+	 * @param drugCommercial2
+	 * @return
+	 */
 	public static boolean isSameDrugCommercial(DrugCommercial drugCommercial,
 			DrugCommercial drugCommercial2) {
 		return drugCommercial == drugCommercial2 
