6e6ed946ffc497e0f8d5e69a4c3da6aac1964143
Pieter Libin <pieter.libin@rega.kuleuven.be>
2013-10-30 11:30:52 +0100

    add isEditable() method to this enum

--- regadb-ui/src/net/sf/regadb/ui/framework/forms/InteractionState.java
+++ regadb-ui/src/net/sf/regadb/ui/framework/forms/InteractionState.java
@@ -2,8 +2,30 @@ package net.sf.regadb.ui.framework.forms;
 
 public enum InteractionState
 {
-	Adding,
-	Editing,
-	Viewing,
-    Deleting,
+	Adding {
+        @Override
+        public boolean isEditable() {
+            return true;
+        }
+    },
+	Editing {
+        @Override
+        public boolean isEditable() {
+            return true;
+        }
+    },
+	Viewing {
+        @Override
+        public boolean isEditable() {
+            return false;
+        }
+    },
+    Deleting {
+        @Override
+        public boolean isEditable() {
+            return false;
+        }
+    };
+    
+    public abstract boolean isEditable();
 }
