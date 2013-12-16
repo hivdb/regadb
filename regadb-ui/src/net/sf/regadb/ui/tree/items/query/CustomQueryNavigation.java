b9dc4f7dce9b4381a3a9feaa8ac715d940987f2b
Pieter Libin <pieter.libin@rega.kuleuven.be>
2013-12-16 13:53:03 +0100

    add HCV GHB query form

--- regadb-ui/src/net/sf/regadb/ui/tree/items/query/CustomQueryNavigation.java
+++ regadb-ui/src/net/sf/regadb/ui/tree/items/query/CustomQueryNavigation.java
@@ -1,10 +1,12 @@
 package net.sf.regadb.ui.tree.items.query;
 
 import net.sf.regadb.ui.form.query.custom.NadirQuery;
+import net.sf.regadb.ui.form.query.custom.ghb_hcv.GhbHcvQueryForm;
 import net.sf.regadb.ui.framework.forms.IForm;
 import net.sf.regadb.ui.framework.tree.TreeMenuNode;
 import net.sf.regadb.ui.tree.DefaultNavigationNode;
 import net.sf.regadb.ui.tree.FormNavigationNode;
+import net.sf.regadb.util.settings.RegaDBSettings;
 import eu.webtoolkit.jwt.WString;
 
 public class CustomQueryNavigation extends DefaultNavigationNode {
@@ -18,6 +20,15 @@ public class CustomQueryNavigation extends DefaultNavigationNode {
 				return new NadirQuery();
 			}
 		};
+		
+		if (RegaDBSettings.getInstance().getInstituteConfig().getGhbHcvExportFormConfig() != null) {
+			new FormNavigationNode(WString.tr("form.query.custom.ghb-hcv.name"), this, true) {
+				@Override
+				public IForm createForm() {
+					return new GhbHcvQueryForm();
+				}
+			};
+		}
 	}
 
 }
